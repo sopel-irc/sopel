05f4476a850c9c698531c4f79e78d03f61f1b8b9
Kays <eureka@divinitycoding.net>
2010-12-04 17:21:59 -0500

    Added timeout to and cleaned up roulette.

--- modules/roulette.py
+++ modules/roulette.py
@@ -1,36 +1,66 @@
 #!/usr/bin/env python
 """ roulette.py """
 import random
-
+from datetime import datetime, timedelta
 random.seed()
 
 # edit this setting for roulette counter. Larger, the number, the harder the game.
-MAX_RANGE = 5
+ROULETTE_SETTINGS = {
+    # the bigger the MAX_RANGE, the harder/longer the game will be
+    'MAX_RANGE' : 5,
+
+    # game timeout in minutes (default is 1 minute)
+    'INACTIVE_TIMEOUT' : 1,
+}
 
 # edit this setting for text displays
-ROULETTE_TICK = '*TICK*'
-ROULETTE_KICK_REASON = '*SNIPED! YOU LOSE!*'
+ROULETTE_STRINGS = {
+    'TICK' : '*TICK*',
+    'KICK_REASON' : '*SNIPED! YOU LOSE!*',
+    'GAME_END' : 'Game stopped.',
+    'GAME_END_FAIL' : "%s: Please wait %s seconds to stop Roulette.",
+}
 
 ## do not edit below this line unless you know what you're doing
+ROULETTE_TMP = {
+    'LAST-PLAYER' : None,
+    'NUMBER' : None,
+    'TIMEOUT' :timedelta(minutes=INACTIVE_TIMEOUT),
+    'LAST-ACTIVITY' : None,
+}
 
-lastRouletter = None
-rouletteBAM = None
-def roulette (phenny, input):
-    global lastRouletter, rouletteBAM, MAX_RANGE, ROULETTE_TICK, ROULETTE_KICK
-    if lastRouletter == input.nick:
+def roulette (jenney, input):
+    global ROULETTE_SETTINGS, ROULETTE_STRINGS, ROULETTE_TMP
+    if ROULETTE_TMP['NUMBER'] is None:
+        ROULETTE_TMP['NUMBER'] = random.randint(0,ROULETTE_SETTINGS['MAX_RANGE'])
+        jenney.say(ROULETTE_STRINGS['TICK'])
         return
-    lastRouletter = input.nick
-    if rouletteBAM is None:
-        rouletteBAM = random.randint(0,MAX_RANGE)
-        phenny.say(ROULETTE_TICK)
-        return  # can't lose on round 1 >_>
-    if rouletteBAM == random.randint(0,MAX_RANGE):
-        phenny.write(['KICK', '%s %s :%s' % (input.sender, input.nick, ROULETTE_KICK_REASON)])
-        lastRouletter = None
-        rouletteBAM = None
+    if ROULETTE_TMP['LAST-PLAYER'] == input.nick:
+        return
+    ROULETTE_TMP['LAST-ACTIVITY'] = datetime.now()
+    ROULETTE_TMP['LAST-PLAYER'] = input.nick
+    if ROULETTE_TMP['NUMBER'] == random.randint(0,ROULETTE_SETTINGS['MAX_RANGE']):
+        jenney.write(['KICK', '%s %s :%s' % (input.sender, input.nick, ROULETTE_STRINGS['KICK_REASON'])])
+        ROULETTE_TMP['LAST-PLAYER'] = None
+        ROULETTE_TMP['NUMBER'] = None
+        ROULETTE_TMP['LAST-ACTIVITY'] = None
     else:
-        phenny.say(ROULETTE_TICK)
+        jenney.say(ROULETTE_STRINGS['TICK'])
 roulette.commands = ['roulette']
+roulette.priority = 'low'
+
+def rouletteStop (jenney, input):
+    global ROULETTE_TMP, ROULETTE_STRINGS
+    if ROULETTE_TMP['LAST-PLAYER'] is None:
+        return
+    if datetime.now() - ROULETTE_TMP['LAST-ACTIVITY'] > ROULETTE_TMP['TIMEOUT']:
+        jenney.say(ROULETTE_STRINGS['GAME_END'])
+        ROULETTE_TMP['LAST-ACTIVITY'] = None
+        ROULETTE_TMP['LAST-PLAYER'] = None
+        ROULETTE_TMP['NUMBER'] = None
+    else:
+        jenney.say(ROULETTE_STRINGS['GAME_END_FAIL'] % (input.nick, ROULETTE_TMP['TIMEOUT'].seconds - (datetime.now() - ROULETTE_TMP['LAST-ACTIVITY']).seconds))
+rouletteStop.commands = ['roulette-stop']
 roulette.priority = 'low'
 
 if __name__ == '__main__': 
