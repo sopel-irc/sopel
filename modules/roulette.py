7778ae339f7f0b443e19e18f4c8dd8a1f0578b19
Kays <eureka@divinitycoding.net>
2010-12-04 06:29:56 -0500

    Cleaned up the file and make better use of variables so people can edit it.

--- modules/roulette.py
+++ modules/roulette.py
@@ -1,26 +1,33 @@
 #!/usr/bin/env python
-"""
-roulette.py
-"""
+""" roulette.py """
 import random
+
+# edit this setting for roulette counter. Larger, the number, the harder the game.
+MAX_RANGE = 5
+
+# edit this setting for text displays
+ROULETTE_TICK = '*TICK*'
+ROULETTE_KICK_REASON = '*SNIPED! YOU LOSE!*'
+
+## do not edit below this line unless you know what you're doing
 
 lastRouletter = None
 rouletteBAM = None
 def roulette (phenny, input):
-    global lastRouletter, rouletteBAM
+    global lastRouletter, rouletteBAM, MAX_RANGE, ROULETTE_TICK, ROULETTE_KICK
     if lastRouletter == input.nick:
         return
     lastRouletter = input.nick
     if rouletteBAM is None:
-        rouletteBAM = random.randint(0,6)
-        phenny.say('*TICK*')
+        rouletteBAM = random.randint(0,MAX_RANGE)
+        phenny.say(ROULETTE_TICK)
         return  # can't lose on round 1 >_>
-    if rouletteBAM == random.randint(0,6):
-        phenny.write(['KICK', '%s %s :%s' % (input.sender, input.nick, '*SNIPED! YOU LOSE!*')])
+    if rouletteBAM == random.randint(0,MAX_RANGE):
+        phenny.write(['KICK', '%s %s :%s' % (input.sender, input.nick, ROULETTE_KICK_REASON)])
         lastRouletter = None
         rouletteBAM = None
     else:
-        phenny.say('*TICK*')
+        phenny.say(ROULETTE_TICK)
 roulette.commands = ['roulette']
 roulette.priority = 'low'
 
