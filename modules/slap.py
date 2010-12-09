a9d6c31ed224a12ef843a41a2a7bc1bc15c660ca
Kays <eureka@divinitycoding.net>
2010-12-09 01:50:48 -0500

    fixed error if second param is missing

--- modules/slap.py
+++ modules/slap.py
@@ -9,7 +9,8 @@ import random
 
 def slap(jenney, input):
 	 """.slap <target> - Slaps <target>"""
-	 verb = random.choice(('slaps', 'kicks', 'destroys', 'annihilates', 'punches', 'teabags', 'roundhouse kicks', 'rusty hooks', 'pwns', 'owns' ))
+	 if len(input.group().split()) < 2: return
+	 verb = random.choice(('slaps', 'kicks', 'destroys', 'annihilates', 'punches', 'teabags', 'roundhouse kicks', 'rusty hooks', 'pwns', 'owns'))
 	 jenney.say(str(input.nick) + " " + verb + " " + input.group(2))
 slap.commands = ['slap', 'slaps']
 slap.priority = 'medium'
