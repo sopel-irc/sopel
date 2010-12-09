f8433a78598546d61b24915d071699c58d0fd8cb
= <eureka@divinitycoding.net>
2010-12-09 12:23:25 -0500

    Removed input.admins from bot.py and used the bot wrapper. Updated slap.py. Thanks to myano for the heads up.

--- modules/slap.py
+++ modules/slap.py
@@ -12,11 +12,11 @@ def slap(jenney, input):
 	 text = input.group().split()
 	 if len(text) < 2 or text[1].startswith('#'): return
 	 if text[1] == jenney.nick:
-		 if (input.nick not in input.admins or not input.owner):
+		 if (input.nick not in jenney.config.admins or not input.owner):
 			 text[1] = input.nick
 		 else: text[1] = 'herself'
 	 if text[1] in input.admins:
-		 if (input.nick not in input.admins or not input.owner):
+		 if (input.nick not in jenney.config.admins or not input.owner):
 			 text[1] = input.nick
 	 verb = random.choice(('slaps', 'kicks', 'destroys', 'annihilates', 'punches', 'teabags', 'roundhouse kicks', 'rusty hooks', 'pwns', 'owns'))
 	 jenney.write(['PRIVMSG', input.sender, ' :\x01ACTION', verb, text[1], '\x01'])
