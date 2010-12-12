ace6959570ebf91bca8c6fbf7defede15f26eb98
= <eureka@divinitycoding.net>
2010-12-12 09:14:02 -0500

    Fixed bug in calling an undef member data

--- modules/slap.py
+++ modules/slap.py
@@ -8,20 +8,20 @@ Jenney (About): http://inamidst.com/phenny/
 import random
 
 def slap(jenney, input):
-	 """.slap <target> - Slaps <target>"""
-	 text = input.group().split()
-	 if len(text) < 2 or text[1].startswith('#'): return
-	 if text[1] == jenney.nick:
-		 if (input.nick not in jenney.config.admins or not input.owner):
-			 text[1] = input.nick
-		 else: text[1] = 'herself'
-	 if text[1] in jenney.config.admins:
-		 if (input.nick not in jenney.config.admins or not input.owner):
-			 text[1] = input.nick
-	 verb = random.choice(('slaps', 'kicks', 'destroys', 'annihilates', 'punches', 'teabags', 'roundhouse kicks', 'rusty hooks', 'pwns', 'owns'))
-	 jenney.write(['PRIVMSG', input.sender, ' :\x01ACTION', verb, text[1], '\x01'])
+    """.slap <target> - Slaps <target>"""
+    text = input.group().split()
+    if len(text) < 2 or text[1].startswith('#'): return
+    if text[1] == jenney.nick:
+        if (input.nick not in jenney.config.admins):
+            text[1] = input.nick
+        else: text[1] = 'herself'
+    if text[1] in jenney.config.admins:
+        if (input.nick not in jenney.config.admins):
+            text[1] = input.nick
+    verb = random.choice(('slaps', 'kicks', 'destroys', 'annihilates', 'punches', 'teabags', 'roundhouse kicks', 'rusty hooks', 'pwns', 'owns'))
+    jenney.write(['PRIVMSG', input.sender, ' :\x01ACTION', verb, text[1], '\x01'])
 slap.commands = ['slap', 'slaps']
 slap.priority = 'medium'
 
 if __name__ == '__main__': 
-	print __doc__.strip()
+    print __doc__.strip()
