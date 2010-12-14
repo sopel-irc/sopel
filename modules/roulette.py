09ec621d37b9240cc93a7e00d54937f68c71cbc0
Kays <eureka@divinitycoding.net>
2010-12-14 02:10:23 -0500

    fixed bug where the starter can spin twice in a row

--- modules/roulette.py
+++ modules/roulette.py
@@ -33,6 +33,7 @@ def roulette (jenni, input):
     global ROULETTE_SETTINGS, ROULETTE_STRINGS, ROULETTE_TMP
     if ROULETTE_TMP['NUMBER'] is None:
         ROULETTE_TMP['NUMBER'] = random.randint(0,ROULETTE_SETTINGS['MAX_RANGE'])
+        ROULETTE_TMP['LAST-PLAYER'] = input.nick
         jenni.say(ROULETTE_STRINGS['TICK'])
         return
     if ROULETTE_TMP['LAST-PLAYER'] == input.nick:
