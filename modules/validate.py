b48c280452c8ae24c259f75645662630472dd8b7
Michael Yanovich <yanovich.1@osu.edu>
2011-03-22 13:20:09 -0400

    fixed indentations

--- modules/validate.py
+++ modules/validate.py
@@ -10,33 +10,33 @@ http://inamidst.com/phenny/
 import web
 
 def val(jenni, input): 
-	"""Check a webpage using the W3C Markup Validator."""
-	uri = input.group(2)
+    """Check a webpage using the W3C Markup Validator."""
+    uri = input.group(2)
     if uri:
-    	if not uri.startswith('http://'): 
-    		uri = 'http://' + uri
+        if not uri.startswith('http://'): 
+            uri = 'http://' + uri
 
-	path = '/check?uri=%s;output=xml' % web.urllib.quote(uri)
-	info = web.head('http://validator.w3.org' + path)
+    path = '/check?uri=%s;output=xml' % web.urllib.quote(uri)
+    info = web.head('http://validator.w3.org' + path)
 
-	result = uri + ' is '
+    result = uri + ' is '
 
-	if isinstance(info, list): 
-		return jenni.say('Got HTTP response %s' % info[1])
+    if isinstance(info, list): 
+        return jenni.say('Got HTTP response %s' % info[1])
 
-	if info.has_key('X-W3C-Validator-Status'): 
-		result += str(info['X-W3C-Validator-Status'])
-		if info['X-W3C-Validator-Status'] != 'Valid': 
-			if info.has_key('X-W3C-Validator-Errors'): 
-				n = int(info['X-W3C-Validator-Errors'].split(' ')[0])
-				if n != 1: 
-					result += ' (%s errors)' % n
-				else: result += ' (%s error)' % n
-	else: result += 'Unvalidatable: no X-W3C-Validator-Status'
+    if info.has_key('X-W3C-Validator-Status'): 
+        result += str(info['X-W3C-Validator-Status'])
+        if info['X-W3C-Validator-Status'] != 'Valid': 
+            if info.has_key('X-W3C-Validator-Errors'): 
+                n = int(info['X-W3C-Validator-Errors'].split(' ')[0])
+                if n != 1: 
+                    result += ' (%s errors)' % n
+                else: result += ' (%s error)' % n
+    else: result += 'Unvalidatable: no X-W3C-Validator-Status'
 
-	jenni.reply(result)
+    jenni.reply(result)
 val.rule = (['val'], r'(?i)(\S+)')
 val.example = '.val http://www.w3.org/'
 
 if __name__ == '__main__': 
-	print __doc__.strip()
+    print __doc__.strip()
