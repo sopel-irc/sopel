680642eb1b2784ef799bf42b6262cce375558de5
Michael Yanovich <yanovich.1@osu.edu>
2011-04-11 20:29:45 -0400

    added a ham-radio module

---
+++ modules/ham.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+ham.py - Ham Radio Module
+Author: Michael Yanovich - http://yanovich.net/
+This contains a collection of lookups and calls for ham radio enthusiasts.
+"""
+
+import re
+import web
+
+re_look = re.compile('<FONT FACE="Arial, Helvetica, sans-serif" SIZE=4>(.*)<BR>')
+
+def lookup(jenni, input):
+    cs = input.group(2)
+    link = "http://www.qth.com/callsign.php?cs=" + unicode(cs)
+    page = web.get(link)
+    name = re_look.findall(page)[0]
+    jenni.say(name)
+lookup.commands = ['cs']
+
+if __name__ == '__main__':
+    print __doc__.strip()
+
