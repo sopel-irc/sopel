94c67c037fab9ed3765c8ed2e650c0ea429ae18f
Michael Yanovich <michael@yanovich.net>
2011-11-15 08:17:11 -0500

    added ability to look up weather alerts via ZIP codes

--- modules/warnings.py
+++ modules/warnings.py
@@ -69,7 +69,11 @@ states = {
 }
 
 county_list = "http://alerts.weather.gov/cap/{0}.php?x=3"
-nomsg = "There are no active watches, warnings or advisories"
+alerts = "http://alerts.weather.gov/cap/wwaatmget.php?x={0}"
+zip_code_lookup = "http://www.zip-codes.com/zip-code/{0}/zip-code-{0}.asp"
+nomsg = "There are no active watches, warnings or advisories, for {0}."
+re_fips = re.compile(r'(?i)title="FIPS: (.*)">')
+re_state = re.compile(r'(?i)Welcome to\s(.*\,\s[A-Z][A-Z])')
 
 def nws_lookup(jenni, input):
     """ Look up weather watches, warnings, and advisories. """
@@ -77,6 +81,7 @@ def nws_lookup(jenni, input):
     if not text: return
     bits = text.split(",")
     if len(bits) == 2:
+        ## county given
         url_part1 = "http://alerts.weather.gov"
         state = bits[1].lstrip().rstrip().lower()
         county = bits[0].lstrip().rstrip().lower()
@@ -90,15 +95,38 @@ def nws_lookup(jenni, input):
             if mystr in line.lower():
                 url_part2 = line[9:36]
                 break
+        if not url_part2:
+            jenni.reply("Could not find county.")
+            return
         master_url = url_part1 + url_part2
+        location = text
+    elif len(bits) == 1:
+        ## zip code
+        if bits[0]:
+            urlz = zip_code_lookup.format(bits[0])
+            pagez = web.get(urlz)
+            fips = re_fips.findall(pagez)
+            if fips:
+                state = re_state.findall(pagez)
+                if not state:
+                    jenni.reply("Could not match ZIP code to a state")
+                    return
+                location = state[0]
+                state = location[-2:]
+                fips = unicode(state) + "C" + unicode(fips[0])
+                master_url = alerts.format(fips)
+    else:
+        jenni.reply("Invalid input. Please enter a ZIP code or a county and state pairing, such as 'Franklin, Ohio'")
+        return
 
-        feed = feedparser.parse(master_url)
-        for item in feed.entries:
-            if nomsg == item["title"]:
-                jenni.reply(nomsg)
-            else:
-                jenni.reply(unicode(item["title"]))
-                jenni.reply(unicode(item["summary"]))
+    feed = feedparser.parse(master_url)
+    for item in feed.entries:
+        if nomsg[:51] == item["title"]:
+            jenni.reply(nomsg.format(location))
+            break
+        else:
+            jenni.reply(unicode(item["title"]))
+            jenni.reply(unicode(item["summary"]))
 nws_lookup.commands = ['nws']
 
 if __name__ == '__main__':
