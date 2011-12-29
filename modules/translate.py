dd824c10ab92cb96c3c29c27a62f48928c814ecf
Michael Yanovich <michael@yanovich.net>
2011-12-28 22:18:51 -0500

    merged in sbp's update of translate

--- modules/translate.py
+++ modules/translate.py
@@ -3,94 +3,80 @@
 """
 translate.py - Jenni Translation Module
 Copyright 2008, Sean B. Palmer, inamidst.com
-Copyright 2011, Michael Yanovich, yanovich.net
 Licensed under the Eiffel Forum License 2.
 
-More info:
- * Jenni: https://github.com/myano/jenni/
- * Phenny: http://inamidst.com/phenny/
+http://inamidst.com/phenny/
 """
 
-import re
+import re, urllib
 import web
 
+def translate(text, input='auto', output='en'):
+    import urllib2, json
+    opener = urllib2.build_opener()
+    opener.addheaders = [(
+        'User-Agent', 'Mozilla/5.0' +
+        '(X11; U; Linux i686)' +
+        'Gecko/20071127 Firefox/2.0.0.11'
+    )]
 
-def translate(phrase, fromlang, tolang):
-    url = "http://translate.google.com/translate_a/t?client=t&text={0}"
-    url = url.format(phrase)
+    input, output = urllib.quote(input), urllib.quote(output)
+    text = urllib.quote(text)
 
-    if tolang:
-        tl = (tolang).encode('utf-8')
-    else:
-        tl = ('en').encode('utf-8')
+    result = opener.open('http://translate.google.com/translate_a/t?'
+        ('client=t&hl=en&sl=%s&tl=%s&multires=1' % (input, output)) +
+        ('&otf=1&ssel=0&tsel=0&uptl=en&sc=1&text=%s' % text)).read()
 
-    url += "&tl=" + tl
+    while ',,' in result:
+        result = result.replace(',,', ',null,')
+    data = json.loads(result)
 
-    if fromlang:
-        sl = (fromlang).encode('utf-8')
-        url += "&sl=" + sl
+    try: language = data[-2][0][0]
+    except: language = '?'
 
-    results = web.get(url)
-
-    re_sl = re.compile(r'\,"([a-z]{2})"\,\,')
-    sl_possible = re_sl.findall(results)
-    if sl_possible:
-        sl = sl_possible[0]
-    results = results.split('"')
-    phrase = results[1]
-
-    return (phrase, sl, tl)
-
+    return ''.join(x[0] for x in data[0]), language
 
 def tr(jenni, context):
     """Translates a phrase, with an optional language hint."""
-
     input, output, phrase = context.groups()
 
-    if input and not output:
-        output = input
-        input = False
-
     phrase = phrase.encode('utf-8')
+
     if (len(phrase) > 350) and (not context.admin):
         return jenni.reply('Phrase must be under 350 characters.')
 
-    good_response = '"{0}" ({1} to {2}, translate.google.com)'
-    bad_response = "The {0} to {1} translation failed, sorry!"
-    dl = "Error: Source language and translation language can not be the same!"
+    input = input or 'auto'
+    input = input.encode('utf-8')
+    output = (output or 'en').encode('utf-8')
 
-    result = translate(phrase, input, output)
+    if input != output:
+        msg, input = translate(phrase, input, output)
+        if isinstance(msg, str):
+            msg = msg.decode('utf-8')
+        if msg:
+            msg = web.decode(msg) # msg.replace('&#39;', "'")
+            msg = '"%s" (%s to %s, translate.google.com)' % (msg, input, output)
+        else: msg = 'The %s to %s translation failed, sorry!' % (input, output)
 
-    if result[1] == result[2]:
-        jenni.reply(dl)
-        return
-
-    if phrase == result[0]:
-        response = (bad_response).format(result[1], result[2])
-    else:
-        response = (good_response).format(result[0], result[1], result[2])
-    jenni.reply(response)
+        jenni.reply(msg)
+    else: jenni.reply('Language guessing failed, so try suggesting one!')
 
 tr.rule = ('$nick', ur'(?:([a-z]{2}) +)?(?:([a-z]{2}) +)?["“](.+?)["”]\? *$')
 tr.example = '$nickname: "mon chien"? or $nickname: fr "mon chien"?'
 tr.priority = 'low'
 
-
 def mangle(jenni, input):
-    if not input.group(2):
-        jenni.reply("What do you want me to mangle?")
-        return
     phrase = input.group(2).encode('utf-8')
     for lang in ['fr', 'de', 'es', 'it', 'ja']:
         backup = phrase
-        phrase = translate(phrase, 'en', lang)[0]
+        phrase = translate(phrase, 'en', lang)
         if not phrase:
             phrase = backup
             break
         __import__('time').sleep(0.5)
 
         backup = phrase
-        phrase = translate(phrase, lang, 'en')[0]
+        phrase = translate(phrase, lang, 'en')
         if not phrase:
             phrase = backup
             break
@@ -101,3 +87,4 @@ mangle.commands = ['mangle']
 
 if __name__ == '__main__':
     print __doc__.strip()
+
