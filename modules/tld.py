ebcfa1dac34bc1f3201f41912f3e14a58a16f76e
Michael Yanovich <michael@yanovich.net>
2011-12-30 11:21:30 -0500

    updated regexp to provide more information and fix some tlds

--- modules/tld.py
+++ modules/tld.py
@@ -17,34 +17,47 @@ r_tag = re.compile(r'<(?!!)[^>]+>')
 
 def gettld(jenni, input):
     page = web.get(uri)
-    #page = urllib2.urlopen(uri).read()
-    search = r'(?i)<td><a href="\S+" title="\S+">\.{0}</a></td>\n<td>(\S+)</td>\n<td>(.*)</td>\n'
+    print page
+    search = r'(?i)<td><a href="\S+" title="\S+">\.{0}</a></td>\n(<td><a href=".*</a></td>\n)?<td>([A-Za-z0-9].*?)</td>\n<td>(.*)</td>\n<td[^>]*>(.*?)</td>\n<td[^>]*>(.*?)</td>\n'
     search = search.format(input.group(2))
     re_country = re.compile(search)
     matches = re_country.findall(page)
+    if not matches:
+        search = r'(?i)<td><a href="\S+" title="(\S+)">\.{0}</a></td>\n<td><a href=".*">(.*)</a></td>\n<td>([A-Za-z0-9].*?)</td>\n<td[^>]*>(.*?)</td>\n<td[^>]*>(.*?)</td>\n'
+        search = search.format(input.group(2))
+        re_country = re.compile(search)
+        matches = re_country.findall(page)
     if matches:
-        desc = r_tag.sub("", matches[0][1])
+        matches = list(matches[0])
+        i = 0
+        while i < len(matches):
+            matches[i] = r_tag.sub("", matches[i])
+            i += 1
+        desc = matches[2]
         if len(desc) > 400:
             desc = desc[:400] + "..."
-        reply = "%s -- %s" % (matches[0][0], desc)
+        reply = "%s -- %s. IDN: %s, DNSSEC: %s" % (matches[1], desc,
+                matches[3], matches[4])
         jenni.reply(reply)
     else:
-        search = r'<td><a href="\S+" title="\S+">.{0}</a></td>\n<td><span class="flagicon"><img.*?">(.*?)</a></td>\n<td[^>]*>(.*?)</td>\n<td[^>]*>(.*?)</td>\n<td[^>]*>(.*?)</td>\n<td[^>]*>(.*?)</td>\n<td[^>]*>(.*?)</td>\n'
+        search = r'<td><a href="\S+" title="\S+">.{0}</a></td>\n<td><span class="flagicon"><img.*?\">(.*?)</a></td>\n<td[^>]*>(.*?)</td>\n<td[^>]*>(.*?)</td>\n<td[^>]*>(.*?)</td>\n<td[^>]*>(.*?)</td>\n<td[^>]*>(.*?)</td>\n'
         search = search.format(unicode(input.group(2)))
         re_country = re.compile(search)
         matches = re_country.findall(page)
-        if len(matches) > 0:
+        if matches:
             matches = matches[0]
             dict_val = dict()
             dict_val["country"], dict_val["expl"], dict_val["notes"], dict_val["idn"], dict_val["dnssec"], dict_val["sld"] = matches
             for key in dict_val:
                 if dict_val[key] == "&#160;":
                     dict_val[key] = "N/A"
+                dict_val[key] = r_tag.sub('', dict_val[key])
+            if len(dict_val["notes"]) > 400:
+                dict_val["notes"] = dict_val["notes"][:400] + "..."
             reply = "%s (%s, %s). IDN: %s, DNSSEC: %s, SLD: %s" % (dict_val["country"], dict_val["expl"], dict_val["notes"], dict_val["idn"], dict_val["dnssec"], dict_val["sld"])
-            jenni.reply(reply)
         else:
             reply = "No matches found for TLD: {0}".format(unicode(input.group(2)))
-            jenni.reply(reply)
+        jenni.reply(reply)
 gettld.commands = ['tld']
 gettld.thread = False
 
