d31038aafbeedc36e677f63e43628daaa9d13405
Edward Powell <powell.518@gmail.com>
2012-01-01 14:23:16 -0500

    Started module for YourPants tools

---
+++ modules/yp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+yp.py - YourPants tools for Jenni
+Copyright 2012 Edward Powell, http://embolalia.net
+Licensed under the Eiffel Forum License 2.
+
+More info:
+ * Jenni: https://github.com/myano/jenni/
+ * Phenny: http://inamidst.com/phenny/
+"""
+
+def profile(jenni, input):
+    jenni.say('http://yourpants.org/members/'+ input.group(2) +'/profile')
+profile.commands = ['yp','profile']
