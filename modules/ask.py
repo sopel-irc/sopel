9cb0420b4b9c3fd2d3fd712b2a1bba6977c93123
Michael Yanovich <michael@yanovich.net>
2012-01-07 04:06:15 -0500

    removed the wolframalpha call in .ask

--- modules/ask.py
+++ modules/ask.py
@@ -9,27 +9,26 @@ More info:
  * Phenny: http://inamidst.com/phenny/
 """
 
-import random, string, calc
+import random
 
 random.seed()
 
+
 def ask(jenni, input):
     """.ask <item1> or <item2> or <item3> - Randomly picks from a set of items seperated by ' or '."""
+
     choices = input.group(2)
+
     if choices == None:
         jenni.reply("There is no spoon! Please try a valid question.")
     elif choices.lower() == "what is the answer to life, the universe, and everything?":
         jenni.reply("42")
-    #if the question posed is not a yes/no question or 'or' question, ask wolframalpha via oblique
-    elif 'what ' in choices.lower() and not ' or ' in choices.lower():
-      	calc.wa(jenni,input)
     else:
         list_choices = choices.split(" or ")
         if len(list_choices) == 1:
             jenni.say(str(input.nick) + ": " + str(random.choice(('yes', 'no'))))
         else:
             jenni.say(str(input.nick) + ": " + str(random.choice(list_choices)))
-
 ask.commands = ['ask']
 ask.priority = 'medium'
 ask.example = '.ask today or tomorrow or next week'
