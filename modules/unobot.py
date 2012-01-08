7312f77f13da5b3351294380167646950165b039
Edward Powell <powell.518@gmail.com>
2012-01-08 18:49:45 -0500

    changed uno channel and score location

--- modules/unobot.py
+++ modules/unobot.py
@@ -36,8 +36,8 @@ from datetime import datetime, timedelta
 random.seed()
 
 # Remember to change these 3 lines or nothing will work
-CHANNEL = '##uno'
-SCOREFILE = "/home/yano/dev/jenni/unoscores.txt"
+CHANNEL = '#uno'
+SCOREFILE = "/usr/share/.jenni/unoscores.txt"
 # Only the owner (starter of the game) can call .unostop to stop the game.
 # But this calls for a way to allow others to stop it after the game has been idle for a while.
 # After this set time, anyone can stop the game via .unostop
