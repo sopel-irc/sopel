5c225fa747c555dae50ff93e42fbda0e2a47aff6
Edward Powell <powell.518@gmail.com>
2012-01-10 21:02:52 -0500

    My failure to make any progress using urllib

--- modules/git.py
+++ modules/git.py
@@ -11,13 +11,26 @@ More info:
 This module will parse any command , spitting out a random http://www.fmylife.com/ quote.
 """
 
-import re, urllib2
+import re, urllib
 
 def issue(jenni, input):
+    ghat = input.gitToken
+    r = urllib.urlopen('https://api.github.com?access_token='+ghat)
+    print r.getcode()
+    
+    print input.group(1)
+    print input.group(3)
+    params = urllib.urlencode({"title": input.group(1), "body": input.group(3)})
+    r = urllib.urlopen('https://api.github.com/repos/embolalia/jenni/issues',
+                       params)
+    code = r.getcode()
+    print code
+    
+    
     #Grab info from rscript (code re-used from ytinfo)
 
     #this is the curl command that should be executed.
-    curl = 'curl -u \''+input.gitUser+':'+input.gitPassword+'\' -H "Content-Type: application/json" -X POST -d \'{"title":"'+input.group(1)+'", "body":"'+input.group(3)+'"}\' https://api.github.com/repos/embolalia/jenni/issues'
+    #curl = 'curl -u \''+input.gitUser+':'+input.gitPassword+'\' -H "Content-Type: application/json" -X POST -d \'{"title":"'+input.group(1)+'", "body":"'+input.group(3)+'"}\' https://api.github.com/repos/embolalia/jenni/issues'
 
 issue.rule = '\.issue ([^~]+)( ~ )([^.]+)'
 
