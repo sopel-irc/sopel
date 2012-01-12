fd73adb9ea1b6d8f9b28731720e632ee7dc43709
Matt Meinwald <matt@meinwald.name>
2012-01-11 23:28:09 -0500

    Bugfixes, removed cruft, and bolding self s/// /me
    I broke the regex in the last revision, but now it works (hooray for non-capturing groups).
    
    Also removed unused/commented out code.
    
    Putting corrections in bold when they are s/// on ones own /me command.

--- modules/find.py
+++ modules/find.py
@@ -20,8 +20,6 @@ try:
 except IOError:
     search_dict = dict()
 
-exp = re.compile(r"(?<!\\)/")
-
 # Create a temporary log of the most recent thing anyone says.
 def collectlines(jenni, input):
     # don't log things in PM
@@ -100,75 +98,13 @@ def findandreplace(jenni, input):
 
     # output
     phrase = input.nick + (input.group(1) and ' thinks ' + rnick or '') + (me and ' ' or " \x02meant\x02 to say: ") + new_phrase
+    if me and not input.group(1): phrase = '\x02' + phrase + '\x02'
     jenni.say(phrase)
 
 # Matches optional whitespace + 's' + optional whitespace + separator character
-#findandreplace.rule = r'(?u)\s*s\s*([^\s\w])(.*)' 
-findandreplace.rule = r'(?u)([^\s:]+)?[\s:]\s*s\s*([^\s\w])(.*)' # May work for both this and "meant" (requires input.group(i+1))
+findandreplace.rule = r'(?u)(?:([^\s:]+)[\s:])?\s*s\s*([^\s\w])(.*)' # May work for both this and "meant" (requires input.group(i+1))
 findandreplace.priority = 'high'
 
-#def meant (jenni, input):
-#    # don't bother in PM
-#    if not input.sender.startswith('#'): return
-#
-#    global search_dict
-#    global search_file
-#    global exp
-#
-#    text = unicode(input.group())
-#    pos = text.find(" ")
-#    user = text[:pos - 1]
-#    matching = text[pos + 1:]
-#
-#    if not matching.startswith("s/"):
-#        return
-#
-#    list_pattern = exp.split(matching)
-#    try:
-#        pattern = list_pattern[1]
-#    except:
-#        return
-#
-#    # Make sure the list exists
-#    try:
-#        replacement = list_pattern[2]
-#    except:
-#        return
-#
-#    # If someone does it for a user that hasn't said anything
-#    try:
-#        current_list = search_dict[input.sender][user]
-#    except:
-#        return
-#    phrase = unicode(current_list[-1])
-#
-#    if matching.endswith("/g"):
-#        new_phrase = freplace(current_list, pattern, replacement, phrase, 0)
-#    else:
-#        new_phrase = freplace(current_list, pattern, replacement, phrase, 1)
-#
-#    # Prevents abuse; apparently there is an RFC spec about how servers handle
-#    # messages that contain more than 512 characters.
-#    if new_phrase:
-#        if len(new_phrase) > 512:
-#            new_phrase[511:]
-#
-#    # Save the new "edited" message.
-#    list = search_dict[input.sender][user]
-#    list.append(new_phrase)
-#    search_dict[input.sender][user] = list
-#    search_file = open("find.txt","w")
-#    pickle.dump(search_dict, search_file)
-#    search_file.close()
-#
-#    # output
-#    if new_phrase:
-#        #new_phrase = new_phrase.replace("\\", "\\\\")
-#        phrase = "%s thinks %s \x02meant:\x02 %s" % (input.nick, user, new_phrase)
-#        jenni.say(phrase)
-#
-#meant.rule = r'\S+(\S|\:)\s.*'
-#meant.priority = 'high'
 
 if __name__ == '__main__':
     print __doc__.strip()
