be21650bc2d341d06742955a09cc8f722abe92f4
Dimitri Molenaars <Tyrope@TyRope.nl>
2012-01-17 01:27:50 +0100

    Disabling .translate and .mangle since their broken-ness seem to be long term Issue #3 and Issue #6 are relevant.
    Signed-off-by: Dimitri Molenaars <Tyrope@TyRope.nl>

--- modules/translate.py
+++ modules/translate.py
@@ -37,52 +37,54 @@ def translate(text, input='auto', output='en'):
     return ''.join(x[0] for x in data[0]), language
 
 def tr(jenni, context):
-    """Translates a phrase, with an optional language hint."""
-    input, output, phrase = context.groups()
-
-    phrase = phrase.encode('utf-8')
-
-    if (len(phrase) > 350) and (not context.admin):
-        return jenni.reply('Phrase must be under 350 characters.')
-
-    input = input or 'auto'
-    input = input.encode('utf-8')
-    output = (output or 'en').encode('utf-8')
-
-    if input != output:
-        msg, input = translate(phrase, input, output)
-        if isinstance(msg, str):
-            msg = msg.decode('utf-8')
-        if msg:
-            msg = web.decode(msg) # msg.replace('&#39;', "'")
-            msg = '"%s" (%s to %s, translate.google.com)' % (msg, input, output)
-        else: msg = 'The %s to %s translation failed, sorry!' % (input, output)
-
-        jenni.reply(msg)
-    else: jenni.reply('Language guessing failed, so try suggesting one!')
+#     """Translates a phrase, with an optional language hint."""
+#     input, output, phrase = context.groups()
+#
+#     phrase = phrase.encode('utf-8')
+#
+#     if (len(phrase) > 350) and (not context.admin):
+#         return jenni.reply('Phrase must be under 350 characters.')
+#
+#     input = input or 'auto'
+#     input = input.encode('utf-8')
+#     output = (output or 'en').encode('utf-8')
+#
+#     if input != output:
+#         msg, input = translate(phrase, input, output)
+#         if isinstance(msg, str):
+#             msg = msg.decode('utf-8')
+#         if msg:
+#             msg = web.decode(msg) # msg.replace('&#39;', "'")
+#             msg = '"%s" (%s to %s, translate.google.com)' % (msg, input, output)
+#         else: msg = 'The %s to %s translation failed, sorry!' % (input, output)
+#
+#         jenni.reply(msg)
+#     else: jenni.reply('Language guessing failed, so try suggesting one!')
+    jenni.reply('This function is broken and disabled. see https://github.com/embolalia/jenni/issues/3 for more details.')
 
 tr.rule = ('$nick', ur'(?:([a-z]{2}) +)?(?:([a-z]{2}) +)?["“](.+?)["”]\? *$')
 tr.example = '$nickname: "mon chien"? or $nickname: fr "mon chien"?'
 tr.priority = 'low'
 
 def mangle(jenni, input):
-    phrase = input.group(2).encode('utf-8')
-    for lang in ['fr', 'de', 'es', 'it', 'ja']:
-        backup = phrase
-        phrase = translate(phrase, 'en', lang)
-        if not phrase:
-            phrase = backup
-            break
-        __import__('time').sleep(0.5)
-
-        backup = phrase
-        phrase = translate(phrase, lang, 'en')
-        if not phrase:
-            phrase = backup
-            break
-        __import__('time').sleep(0.5)
-
-    jenni.reply(phrase or 'ERRORS SRY')
+#    phrase = input.group(2).encode('utf-8')
+#    for lang in ['fr', 'de', 'es', 'it', 'ja']:
+#        backup = phrase
+#        phrase = translate(phrase, 'en', lang)
+#        if not phrase:
+#            phrase = backup
+#            break
+#        __import__('time').sleep(0.5)
+#
+#        backup = phrase
+#        phrase = translate(phrase, lang, 'en')
+#        if not phrase:
+#            phrase = backup
+#            break
+#        __import__('time').sleep(0.5)
+#
+#    jenni.reply(phrase or 'ERRORS SRY')
+    jenni.reply('This function is broken and disabled. see https://github.com/embolalia/jenni/issues/6 for more details.')
 mangle.commands = ['mangle']
 
 if __name__ == '__main__':
