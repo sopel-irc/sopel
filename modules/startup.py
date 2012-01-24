04031cee3fc0462a5c34a0c19eed1f21a3e7d618
Michael Yanovich <michael@yanovich.net>
2012-01-24 17:56:44 -0500

    merged in update from sbp, "Added clsn's ping-pong code"

--- modules/startup.py
+++ modules/startup.py
@@ -4,8 +4,55 @@ startup.py - Jenni Startup Module
 Copyright 2008, Sean B. Palmer, inamidst.com
 Licensed under the Eiffel Forum License 2.
 
-http://inamidst.com/phenny/
+More info:
+ * Jenni: https://github.com/myano/jenni/
+ * Phenny: http://inamidst.com/phenny/
 """
+
+import threading, time
+
+def setup(jenni):
+    # by clsn
+    jenni.data = {}
+    refresh_delay = 300.0
+
+    if hasattr(jenni.config, 'refresh_delay'):
+        try: refresh_delay = float(jenni.config.refresh_delay)
+        except: pass
+
+    def close():
+        print "Nobody PONGed our PING, restarting"
+        jenni.handle_close()
+
+    def pingloop():
+        timer = threading.Timer(refresh_delay, close, ())
+        jenni.data['startup.setup.timer'] = timer
+        jenni.data['startup.setup.timer'].start()
+        # print "PING!"
+        jenni.write(('PING', jenni.config.host))
+    jenni.data['startup.setup.pingloop'] = pingloop
+
+    def pong(jenni, input):
+        try:
+            # print "PONG!"
+            jenni.data['startup.setup.timer'].cancel()
+            time.sleep(refresh_delay + 60.0)
+            pingloop()
+        except: pass
+    pong.event = 'PONG'
+    pong.thread = True
+    pong.rule = r'.*'
+    jenni.variables['pong'] = pong
+
+    # Need to wrap handle_connect to start the loop.
+    inner_handle_connect = jenni.handle_connect
+
+    def outer_handle_connect():
+        inner_handle_connect()
+        if jenni.data.get('startup.setup.pingloop'):
+            jenni.data['startup.setup.pingloop']()
+
+    jenni.handle_connect = outer_handle_connect
 
 def startup(jenni, input):
     if hasattr(jenni.config, 'serverpass'):
