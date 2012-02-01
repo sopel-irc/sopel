a8d2fd210d76d51ed9202287ebb2ee0162fe7d05
Edward Powell <powell.518@gmail.com>
2012-02-01 16:05:42 -0500

    Update moved to tweepy

--- modules/twit.py
+++ modules/twit.py
@@ -7,10 +7,17 @@ Licensed under the Eiffel Forum License 2.
 
 http://inamidst.com/phenny/
 
-For this module to work, you need to create 2 variables in your config file ( ~/.jenni/default.py ). The first one "twitter_username" with the username you have registered on twitter, and "twitter_password" with the password for that twitter account.
+For this module to work, you must register the application with Twitter, and
+create the variables consumer_key, consumer_secret, access_token, and
+access_token_secret in your config file (~/.jenni/default.py).
+
+#For this module to work, you need to create 2 variables in your config file ( ~/.jenni/default.py ). The first one 
+#"twitter_username" with the username you have registered on twitter, and "twitter_password" with the password for that twitter 
+#account.
 """
 import simplejson
 import twitter
+import tweepy
 import sched, time
 import re
 
@@ -27,71 +34,76 @@ def format_thousands(integer):
     return re.sub(r'(\d{3})(?=\d)', r'\1,', str(integer)[::-1])[::-1]
 
 def gettweet(jenni, input):
-	try:
-		twituser = input.group(2)
-		twituser = str(twituser)
-		statuses = api.GetUserTimeline(twituser)
-		recent = [s.text for s in statuses][0]
-		#jenni.say("<" + twituser + "> " + unicode(recent))
-		if twituser[0] != '@': twituser = '@' + twituser
-		jenni.say(twituser + ": " + unicode(recent))
-	except:
-		jenni.reply("You have inputted an invalid user.")
+    try:
+        twituser = input.group(2)
+        twituser = str(twituser)
+        statuses = api.GetUserTimeline(twituser)
+        recent = [s.text for s in statuses][0]
+        #jenni.say("<" + twituser + "> " + unicode(recent))
+        if twituser[0] != '@': twituser = '@' + twituser
+        jenni.say(twituser + ": " + unicode(recent))
+    except:
+        jenni.reply("You have inputted an invalid user.")
 gettweet.commands = ['twit']
 gettweet.priority = 'medium'
 gettweet.example = '.twit aplusk'
 
 def f_info(jenni, input):
-	try:
-		twituser = input.group(2)
-		twituser = str(twituser)
-		info = api.GetUser(twituser)
-		friendcount = format_thousands(info.friends_count)
-		name = info.name
-		id = info.id
-		favourites = info.favourites_count
-		followers = format_thousands(info.followers_count)
-		location = info.location
-		description = info.description
-		jenni.reply("<" + str(twituser) + "> " + str(name) + ". " + "ID: " + str(id) + ". Friend Count: " + friendcount + ". Followers: " + followers + ". Favourites: " + str(favourites) + ". Location: " + str(location) + ". Description: " + str(description))
-	except:
-		jenni.reply("You have inputted an invalid user.")
+    try:
+        twituser = input.group(2)
+        twituser = str(twituser)
+        info = api.GetUser(twituser)
+        friendcount = format_thousands(info.friends_count)
+        name = info.name
+        id = info.id
+        favourites = info.favourites_count
+        followers = format_thousands(info.followers_count)
+        location = info.location
+        description = info.description
+        jenni.reply("<" + str(twituser) + "> " + str(name) + ". " + "ID: " + str(id) + ". Friend Count: " + friendcount + ". Followers: " + followers + ". Favourites: " + str(favourites) + ". Location: " + str(location) + ". Description: " + str(description))
+    except:
+        jenni.reply("You have inputted an invalid user.")
 f_info.commands = ['twitinfo']
 f_info.priority = 'medium'
 f_info.example = '.twitinfo aplsuk'
 
 def f_update(jenni, input):
-	try:
-		api2 = twitter.Api(username=str(input.twitter_username), password=str(input.twitter_password))
-		update = str(input.group(2)) + " ^" + input.nick
-		if len(update) <= 140:
-			api2.PostUpdates(update)
-			jenni.reply("Successfully posted to twitter.com/jenni_osu")
-		else:
-			toofar = len(update) - 140
-			jenni.reply("Please shorten the length of your message by: " + str(toofar) + " characters.")
-	except:
-		jenni.reply("There was a problem posting to jenni's Twitter page.")
+    auth = tweepy.OAuthHandler(jenni.config.consumer_key, jenni.config.consumer_secret)
+    auth.set_access_token(jenni.config.access_token, jenni.config.access_token_secret)
+    api = tweepy.API(auth)
+    
+    print api.me().name
+    
+    update = str(input.group(2)) + " ^" + input.nick
+    if len(update) <= 140:
+        api.update_status(update)
+        jenni.reply("Successfully posted to twitter.com/jenni_osu")
+    else:
+        toofar = len(update) - 140
+        jenni.reply("Please shorten the length of your message by: " + str(toofar) + " characters.")
 f_update.commands = ['tweet']
 f_update.priority = 'medium'
 f_update.example = '.twitup Hello World!'
 
 def f_reply(jenni, input):
-	api3 = twitter.Api(username=str(input.twitter_username), password=str(input.twitter_password))
-	incoming = str(input.group(2))
-	incoming = incoming.split()
-	statusid = incoming[0]
-	if statusid.isdigit():
-		update = incoming[1:]
-		if len(update) <= 140:
-			statusid = int(statusid)
-			api3.PostUpdate(str(" ".join(update)), in_reply_to_status_id=10503164300)
-			jenni.reply("Successfully posted to twitter.com/jenni_osu")
-		else:
-			toofar = len(update) - 140
-			jenni.reply("Please shorten the length of your message by: " + str(toofar) + " characters.")
-	else:
-		jenni.reply("Please provide a status ID.")
+    auth = tweepy.OAuthHandler(jenni.config.consumer_key, jenni.config.consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    
+    incoming = str(input.group(2))
+    incoming = incoming.split()
+    statusid = incoming[0]
+    if statusid.isdigit():
+        update = incoming[1:]
+        if len(update) <= 140:
+            statusid = int(statusid)
+            #api3.PostUpdate(str(" ".join(update)), in_reply_to_status_id=10503164300)
+            jenni.reply("Successfully posted to twitter.com/jenni_osu")
+        else:
+            toofar = len(update) - 140
+            jenni.reply("Please shorten the length of your message by: " + str(toofar) + " characters.")
+    else:
+        jenni.reply("Please provide a status ID.")
 #f_reply.commands = ['reply']
 f_reply.priority = 'medium'
 f_reply.example = '.reply 892379487 I like that idea!'
@@ -148,4 +160,4 @@ def tweetwatcher(jenni, input):
 tweetwatcher.commands = ['tweetwatcher']
 
 if __name__ == '__main__':
-	print __doc__.strip()
+    print __doc__.strip()
