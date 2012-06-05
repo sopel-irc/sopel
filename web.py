a7006ae0c8a7b58662a67e82fcee949f41cc029f
Elad Alfassa <elad@fedoraproject.org>
2012-06-05 20:47:54 +0300

    Rework web.py for easier useage (1 out of 2 commits to reduce code duplication)y

--- web.py
+++ web.py
@@ -1,42 +1,46 @@
 #!/usr/bin/env python
+# -*- coding: utf8 -*-
 """
 web.py - Web Facilities
-Author: Sean B. Palmer, inamidst.com
-About: http://inamidst.com/phenny/
+Copyright © 2008, Sean B. Palmer, inamidst.com
+Copyright © 2009, Michael Yanovich <yanovich.1@osu.edu>
+Copyright © 2012, Dimitri Molenaars, Tyrope.nl.
+Copyright © 2012, Elad Alfassa, <elad@fedoraproject.org>
+Licensed under the Eiffel Forum License 2.
+
+More info:
+ * Jenni: https://github.com/myano/jenni/
+ * Phenny: http://inamidst.com/phenny/
+
 """
 
-import re, urllib
+import re, urllib2
 from htmlentitydefs import name2codepoint
 
-class Grab(urllib.URLopener):
-    def __init__(self, *args):
-        self.version = 'Mozilla/5.0 (Jenni)'
-        urllib.URLopener.__init__(self, *args)
-    def http_error_default(self, url, fp, errcode, errmsg, headers):
-        return urllib.addinfourl(fp, [headers, errcode], "http:" + url)
-urllib._urlopener = Grab()
-
-def get(uri):
+#HTTP GET
+def get(uri, timeout=20):
     if not uri.startswith('http'):
         return
-    u = urllib.urlopen(uri)
+    u = get_urllib_object(uri, timeout)
     bytes = u.read()
     u.close()
     return bytes
 
-def head(uri):
+# Get HTTP headers
+def head(uri, timeout=20):
     if not uri.startswith('http'):
         return
-    u = urllib.urlopen(uri)
+    u = get_urllib_object(uri, timeout)
     info = u.info()
     u.close()
     return info
 
+# HTTP POST
 def post(uri, query):
     if not uri.startswith('http'):
         return
-    data = urllib.urlencode(query)
-    u = urllib.urlopen(uri, data)
+    data = urllib2.urlencode(query)
+    u = urllib2.urlopen(uri, data)
     bytes = u.read()
     u.close()
     return bytes
@@ -55,6 +59,38 @@ def entity(match):
 
 def decode(html):
     return r_entity.sub(entity, html)
+
+#For internal use in web.py, (modules can use this if they need a urllib object they can execute read() on)
+#Both handles redirects and makes sure input URI is UTF-8
+def get_urllib_object(uri, timeout):
+    redirects = 0
+    try:
+        uri = uri.encode("utf-8")
+    except:
+        pass
+    while True:
+        req = urllib2.Request(uri, headers={'Accept':'*/*', 'User-Agent':'Mozilla/5.0 (Jenni)'})
+        try: u = urllib2.urlopen(req, None, timeout)
+        except:
+            raise
+        info = u.info()
+        if not isinstance(info, list):
+            status = '200'
+        else:
+            status = str(info[1])
+            try: info = info[0]
+            except: pass
+        if status.startswith('3'):
+            uri = urlparse.urljoin(uri, info['Location'])
+        else: break
+        redirects += 1
+        if redirects >= 50:
+            return "Too many re-directs."
+    return u
+
+#Identical to urllib2.quote
+def quote(string):
+    return urllib2.quote(string)
 
 r_string = re.compile(r'("(\\.|[^"\\])*")')
 r_json = re.compile(r'^[,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]+$')
