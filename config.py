sopel-irc/sopel

commit a70c244a3e71f12bb26e64d0cdad20462df83384

Fix an error message in config.py

config.py
@@ -244,7 +244,6 @@ class Config(object):
         If you'd like to include modules from other directories, enter them one at a
         time, and hit enter again when done.""")
         self.add_list('extra', c, 'Directory:')
-        self.extra.append(os.getcwd() + '/modules/')
         
     def _settings(self):
         try:
@@ -269,7 +268,11 @@ class Config(object):
         self.modules_chunk = ''
         # This segment largely copied from bot.py
         filenames = []
-        if hasattr(self, 'enable'):
+        if not hasattr(self, 'enable'):
+            for fn in os.listdir(os.path.join(home, 'modules')):
+                if fn.endswith('.py') and not fn.startswith('_'):
+                    filenames.append(os.path.join(home, 'modules', fn))
+        else:
             for fn in self.enable:
                 filenames.append(os.path.join(home, 'modules', fn + '.py'))
 

commit 0789e165169aab6785f7b65aaffb1c8822772467

Actually load modules even if we have one module disabled.
This fixes a regression caused by commit a70c244a3e71f12bb26e64d0cdad20462df83384
I'd like to use this commit message to further explain about the issue I just fixed:
Yesterday at #Embo, we were wondering why modules were loading twice. Then we discovered modules/ was listed twice, once under config.extra and once by itself in the module loading function.

What we didn't understand is why when we used module excludes the issue suddenly disappered. Now I figured it out.
When using excludes, self.enable does not exist. When you do use excludes, self.enable is []. That means, using hasattr is wrong here, because even with self.enable is empty (ie. not using whitelist and not using excludes) it will be True.

This commit fixes this issue. Relevant issue is #67

config.py
@@ -273,7 +273,7 @@ class Config(object):
         self.modules_chunk = ''
         # This segment largely copied from bot.py
         filenames = []
-        if not hasattr(self, 'enable'):
+        if self.enable:
             for fn in os.listdir(os.path.join(home, 'modules')):
                 if fn.endswith('.py') and not fn.startswith('_'):
                     filenames.append(os.path.join(home, 'modules', fn))