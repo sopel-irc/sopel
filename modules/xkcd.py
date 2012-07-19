5c611f0fa329be93767e1a1ebc274f94855914a0
firerogue <lramati@verizon.net>
2012-07-19 16:30:18 -0300

    added contact info

--- modules/xkcd.py
+++ modules/xkcd.py
@@ -2,7 +2,7 @@
 """
 xkcd.py - XKCD Module
 Copyright 2010, Michael Yanovich (yanovich.net), and Morgan Goose
-Copyright 2012, FireRogue
+Copyright 2012, FireRogue (FireRogue517@gmail.com)
 Licensed under the Eiffel Forum License 2.
 
 More info:
