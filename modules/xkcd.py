757b2b758ae724022decd5cc5eeedf3d9d9dc071
firerogue <lramati@verizon.net>
2012-07-19 03:35:23 -0300

    UNTESTED: dont pull/push! replaced reply with say for consistency, modified program flow, and added comic title to output

--- modules/xkcd.py
+++ modules/xkcd.py
@@ -12,38 +12,44 @@ More info:
 
 import random
 from search import google_search
+from url import find_title
+import urllib2
+from lxml import etree
 
 """.xkcd - Finds an xkcd comic strip. Takes one of 3 inputs:
-    If no input is provided it gives a random comic.
+    If no input is provided it will return a random comic.
     If a numeric input is provided it will return that comic
     If non-numeric input is provided it will return the first google result for those keywords on the xkcd.com site"""
     
 def xkcd(jenni, input):
-    import urllib2
-    from lxml import etree
-    if not input.group(2):
-        body = urllib2.urlopen("http://xkcd.com/rss.xml").readlines()[1]
-        parsed = etree.fromstring(body)
-        newest = etree.tostring(parsed.findall("channel/item/link")[0])
-        max_int = int(newest.split("/")[-3])
-        website = "http://xkcd.com/%d/" % random.randint(0,max_int+1)
-        jenni.say(website)
+    # get latest comic for rand function and numeric input
+    body = urllib2.urlopen("http://xkcd.com/rss.xml").readlines()[1]
+    parsed = etree.fromstring(body)
+    newest = etree.tostring(parsed.findall("channel/item/link")[0])
+    max_int = int(newest.split("/")[-3])
+
+    # if no input is given (pre - Lior's edits code)
+    if not input.group(2): website = "http://xkcd.com/%d/" % random.randint(0,max_int+1)
     else:
         query = input.group(2)
+
+        # numeric input!
         if (query.strip().isdigit()): 
             random.seed()
-            website = "http://xkcd.com/" + query.strip()
-            jenni.say(website)
+            website = "http://xkcd.com/" + query.strip() + '/'
+        
+        # non-numeric input! code lifted from search.g
         else:
            try:
                 query = query.encode('utf-8')
            except:
                pass
-           uri = google_search("site:xkcd.com "+ query)
-           if uri:
-               jenni.say(uri)
-           elif uri is False: jenni.say("Problem getting data from Google.")
-           else: jenni.say("No results found for '%s'." % query) 
+           website = google_search("site:xkcd.com "+ query)
+    if website: # format and say result
+        website += ' [' + find_title(website)[0] + ']'
+        jenni.say(website)
+    elif website is False: jenni.say("Problem getting data from Google.")
+    else: jenni.say("No results found for '%s'." % query) 
 xkcd.commands = ['xkcd']
 xkcd.priority = 'low'
 
