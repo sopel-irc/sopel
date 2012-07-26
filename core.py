a3f815d227c0bc9f7e7f15456250b69e9bf78476
Elad Alfassa <elad@fedoraproject.org>
2012-07-26 21:59:11 +0300

    We're making progress, slowly

--- core.py
+++ core.py
@@ -6,17 +6,19 @@ from twisted.internet import reactor, protocol
 
 class IRCBot(irc.IRCClient):
     def __init__(self, config):
-        irc.IRCClient.__init__(self)
+        self.config = config
         self.realname = config.name
         self.nickname = config.nick
-        self.username = config.username
-        self.password = config.password
+        #self.username = config.username
+        #self.password = config.password
         self.lineRate = 0.05
 
     def connectionMade(self):
         #Connected, prepare client and bot system, then report connection made to console.
         irc.IRCClient.connectionMade(self)
-        self.willie = Willie(self.nickname, self)
+        print 'connectionmade'
+        self.willie = Willie(self)
+        self.msg("elad", "hello")
         print("connected to localhost")
 
     def connectionLost(self, reason):
@@ -25,13 +27,15 @@ class IRCBot(irc.IRCClient):
         print("disconnected from localhost:"+str(reason).split(":")[3])
 
     def signedOn(self):
-        self.msg("NickServ", "id password") #we need to load this from config
+        for channel in self.config.channels:
+            self.join(channel)
+        #self.msg("NickServ", "id password") we need to load this from config
         self.mode(self.nickname, True, "B")
 
     def privmsg(self, user, channel, messages):
         if(channel == self.nickname):
             #new PM!
-            pass #for now
+            print messages
 
     def userJoined(self, user, channel):
         pass #Do stuff
@@ -71,17 +75,21 @@ def enumerate_modules(config):
     return filenames
 
 class IRCBotFactory(protocol.ClientFactory):
+    def __init__(self, config):
+        self.config = config
     protocol = IRCBot
     def clientConnectionLost(self, connector, reason):
         connector.connect()
     def clientConnectionFailed(self, connector, reason):
         print "connection failed:", reason
         reactor.stop()
+    def buildProtocol(self,addr):
+        return IRCBot(self.config)
 
-if __name__ == '__main__':
-    print "Booting HelpBot"
-    f = IRCBotFactory()
-    reactor.connectTCP("localhost", 6667, f) #We need to load port and host from config
+def start(config):  
+    f = IRCBotFactory(config)
+    reactor.connectTCP(config.host, config.port, f) #We need to load port and host from config
+    print 'Starting reactor'
     reactor.run()
 
 
