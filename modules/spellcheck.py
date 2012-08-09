bc7439da4cb04aa1082b32c889895b190026d5d9
FireRogue <firerogue517@gmail.com>
2012-08-09 14:36:16 -0400

    made spellcheck identify which dictionary is matched and suggest words from both

--- modules/spellcheck.py
+++ modules/spellcheck.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
-# -*- coding: utf8 -*-
 """
 spellcheck.py - Jenni spell check Module
 Copyright © 2012, Elad Alfassa, <elad@fedoraproject.org>
+Copyright © 2012, Lior Ramati
 Licensed under the Eiffel Forum License 2.
 
 This module relies on pyenchant, on Fedora and Red Hat based system, it can be found in the package python-enchant
@@ -18,13 +17,20 @@ def spellcheck(jenni, input):
     dictionary = enchant.Dict("en_US")
     dictionary_uk = enchant.Dict("en_GB")
     # I don't want to make anyone angry, so I check both American and British English.
-    if dictionary_uk.check(word):
+    if dictionary_uk.check(word) and dictionary.check(word):
         jenni.say(word+" is spelled correctly")
+    if dictionary_uk.check(word):
+        jenni.say(word+" is spelled correctly (British)")
     elif dictionary.check(word):
         jenni.say(word+" is spelled correctly (American)")
     else:
         msg = word+" is not spelled correctly. Maybe you want one of these spellings:"
+        sugWords = []
         for suggested_word in dictionary.suggest(word):
+                sugWords.append(suggested_word)
+        for suggested_word in dictionary_uk.suggest(word):
+                sugWords.append(suggested_word)
+        for suggested_word in set(sorted(sugWords)): # removes duplicates
             msg = msg + " '"+suggested_word+"',"
         jenni.say(msg)
 spellcheck.commands = ['spellcheck', 'spell']
