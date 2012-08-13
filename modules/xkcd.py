f2599744fcc2f44d7444fbb70932d7a35963f619
FireRogue <firerogue517@gmail.com>
2012-08-13 12:36:40 -0400

    [xkcd] replaces threads from comic forum w/ comic

--- modules/xkcd.py
+++ modules/xkcd.py
@@ -15,6 +15,7 @@ from search import google_search
 from url import find_title
 import urllib2
 from lxml import etree
+import re
 
 """.xkcd - Finds an xkcd comic strip. Takes one of 3 inputs:
 If no input is provided it will return a random comic
@@ -29,29 +30,32 @@ def xkcd(jenni, input):
     max_int = int(newest.split("/")[-3])
 
     # if no input is given (pre - lior's edits code)
-    if not input.group(2):
+    if not input.group(2): # get rand comic
         random.seed()
         website = "http://xkcd.com/%d/" % random.randint(0,max_int+1)
     else:
         query = input.group(2).strip()
 
-        # numeric input!
+        # numeric input! get that comic number if it exists
         if (query.isdigit()):
             if (int(query) > max_int):
                 jenni.say("Sorry, comic #" + query + " hasn't been posted yet. The last comic was #%d" % max_int)
                 return
-            else: website = "http://xkcd.com/" + query + '/'
+            else: website = "http://xkcd.com/" + query
         
         # non-numeric input! code lifted from search.g
         else:
-            if (query.lower() == "latest" or query.lower() == "newest"):
+            if (query.lower() == "latest" or query.lower() == "newest"): # special commands
                 website = "https://xkcd.com/"
-            else:
+            else: # just google
                 try:
                     query = query.encode('utf-8')
                 except:
                     pass
                 website = google_search("site:xkcd.com "+ query)
+                regex = re.compile(r'([0-9].*):') # regex for comic specific forum threads
+                if (re.match(regex, website)):
+                    website = "http://xkcd.com/" + re.match(regex, website).groups()[0]
     if website: # format and say result
         website += ' [' + find_title(website)[6:] + ']'
         jenni.say(website)
