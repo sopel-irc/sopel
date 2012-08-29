bb282e974224262763104b61122b599e461fb3e3
Elad Alfassa <elad@fedoraproject.org>
2012-08-29 21:19:44 +0300

    [whois] strip input, make sure that even if there are execptions we did not predict, vars will be reset

--- modules/whois.py
+++ modules/whois.py
@@ -17,37 +17,38 @@ nick, host, rl, chans, idle, signon = None, None, None, None, None, None
 
 def sendwhois(willie, trigger):
     global whois, got318, nick, host, rl, chans, idle, signon
-    whois = True
-    willie.write(['WHOIS'], trigger.group(2)) #A whois needs a 2nd parameter to get the signon time... name+" "+name doesn't work.
+    try:
+        whois = True
+        willie.write(['WHOIS'], trigger.group(2).strip()) #A whois needs a 2nd parameter to get the signon time... name+" "+name doesn't work.
 
-    while not got318: #Wait until event 318 (End of /WHOIS list.)
-        sleep(0.5)
-    if nick is None:
-        willie.say('[WHOIS] No such user')
+        while not got318: #Wait until event 318 (End of /WHOIS list.)
+            sleep(0.5)
+        if nick is None:
+            willie.say('[WHOIS] No such user')
+            whois, got318 = False, False
+            nick, host, rl, chans, idle, signon = None, None, None, None, None, None
+            return
+        msg1 = '[WHOIS] Nick: ' + str(nick) + ' Host: ' + str(host) + \
+               ' Real name: ' + str(rl)
+
+        #hide channels that are +s (prefixed with a ?)
+        if chans is not None:
+            channels = chans.split(' ')
+            for chan in channels:
+                if len(chan):
+                    if chan[0] == "?":
+                        channels.remove(chan)
+            chans = ' '.join(channels)
+            msg2 = str(nick) + ' is on '+str(len(channels)-1)+' channels: ' + str(chans)
+        else:
+            msg2 = str(nick) + ' is not in any channel!'
+
+        willie.say(msg1)
+        willie.say(msg2)
+    finally:
+        #reset variables.
         whois, got318 = False, False
         nick, host, rl, chans, idle, signon = None, None, None, None, None, None
-        return
-    msg1 = '[WHOIS] Nick: ' + str(nick) + ' Host: ' + str(host) + \
-           ' Real name: ' + str(rl)
-
-    #hide channels that are +s (prefixed with a ?)
-    if chans is not None:
-        channels = chans.split(' ')
-        for chan in channels:
-            if len(chan):
-                if chan[0] == "?":
-                    channels.remove(chan)
-        chans = ' '.join(channels)
-        msg2 = str(nick) + ' is on '+str(len(channels)-1)+' channels: ' + str(chans)
-    else:
-        msg2 = str(nick) + ' is not in any channel!'
-
-    willie.say(msg1)
-    willie.say(msg2)
-
-    #reset variables.
-    whois, got318 = False, False
-    nick, host, rl, chans, idle, signon = None, None, None, None, None, None
 sendwhois.commands = ['whois']
 
 def whois311(willie, trigger):
