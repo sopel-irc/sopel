ba7aba599a5c3bc3e36105afc3345c90d768cfb1
Dimitri Molenaars <Tyrope@TyRope.nl>
2012-09-05 21:57:31 +0200

    [dice]Adding some commentary. [radio]putting parameters in alphabetical order. OCD, I know.
    Signed-off-by: Dimitri Molenaars <Tyrope@TyRope.nl>

--- modules/radio.py
+++ modules/radio.py
@@ -87,7 +87,7 @@ def radio(willie, trigger):
     try:
         args = trigger.group(2).lower().split(' ')
     except AttributeError:
-        willie.say('Usage: .radio (on|off|song|now|next|soon|stats)')
+        willie.say('Usage: .radio (next|now|off|on|song|soon|stats)')
         return
     if args[0] == 'on':
         if not trigger.isop:
