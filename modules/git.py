b3665409a652709ffe3f853e95390c93728072de
Dimitri Molenaars <Tyrope@TyRope.nl>
2012-09-06 02:25:31 +0200

    [git]Uploading broken .issue command, see issue #108 and #12

--- modules/git.py
+++ modules/git.py
@@ -4,35 +4,56 @@ git.py - Jenni Github Module
 Copyright 2012, Dimitri Molenaars http://tyrope.nl/
 Licensed under the Eiffel Forum License 2.
 
-More info:
- * Jenni: https://github.com/myano/jenni/
- * Phenny: http://inamidst.com/phenny/
-
-This module will parse any command , spitting out a random http://www.fmylife.com/ quote.
+http://willie.dftba.net/
 """
 
-import re, urllib
+from datetime import datetime
+import json, re, web
 
-def issue(jenni, input):
-    ghat = input.gitToken
-    r = urllib.urlopen('https://api.github.com?access_token='+ghat)
-    print r.getcode()
-    
-    print input.group(1)
-    print input.group(3)
-    params = urllib.urlencode({"title": input.group(1), "body": input.group(3)})
-    r = urllib.urlopen('https://api.github.com/repos/embolalia/jenni/issues',
-                       params)
-    code = r.getcode()
-    print code
-    
-    
-    #Grab info from rscript (code re-used from ytinfo)
+def checkConfig(willie):
+    if not hasattr(willie.config, 'git_Oath_token'):
+        return False
+    if not hasattr(willie.config, 'git_repo'):
+        return False
+    else:
+        return [willie.config.git_Oath_token, willie.config.git_repo]
 
-    #this is the curl command that should be executed.
-    #curl = 'curl -u \''+input.gitUser+':'+input.gitPassword+'\' -H "Content-Type: application/json" -X POST -d \'{"title":"'+input.group(1)+'", "body":"'+input.group(3)+'"}\' https://api.github.com/repos/embolalia/jenni/issues'
+def configure(config):
+    chunk = ''
+    if config.option('Configuring git module', True):
+        config.interactive_add('git_Oath_token', 'Github API Oauth2 token', '')
+        config.interactive_add('git_repo', 'Github repository', 'embolalia/willie')
+        chunk = ("\ngit_Oath_token = '%s'\ngit_repo = '%s'\n"
+                 % (config.git_Oath_token,config.git_repo))
+    return chunk
 
-issue.rule = '\.issue ([^~]+)( ~ )([^.]+)'
+def issue(willie, trigger):
+    """Create a GitHub issue, also known as a bug report. Syntax: .issue Title of the bug report"""
+    return willie.say('broken feature, requires a fix in web.py, gitted for testcases in local copies.')
+    #check input
+    if not trigger.group(2):
+        return willie.say('Please title the issue')
+
+    #Is the Oauth token and repo available?
+    gitAPI = checkConfig(willie)
+    if gitAPI == False:
+        return willie.say('Git module not configured, make sure git_Oath_token and git_repo are defined')
+
+    #parse input
+    now = ' '.join(str(datetime.utcnow()).split(' ')).split('.')[0]+' UTC'
+    body = 'submitted by: %s\nfrom channel: %s\nat %s' % (trigger.nick, trigger.sender, now)
+    data = '{"title": "%s","body": %s,"labels": ["IRC"]}' % (trigger.group(2), body)
+    #submit
+    try:
+        raw = web.post('https://api.github.com/repos/'+gitAPI[1]+'/issues?access_token='+gitAPI[0], data)
+    except Exception as e:
+        print ('%s %s %s' % (type(e), e.args, e))
+        return willie.say('Exception while reaching the git API.')
+
+    print raw
+    return willie.say('Issue posted')
+issue.commands = ['issue']
+issue.priority = 'medium'
 
 if __name__ == '__main__':
     print __doc__.strip()
