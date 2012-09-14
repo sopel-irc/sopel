2f8d1b80abf0f1fcd91c115edb5672b932214687
Edward Powell <powell.518@gmail.com>
2012-09-14 15:46:37 -0400

    [reddit] Improve error handling on non-existant users, and fix linking when commanded

--- modules/reddit-info.py
+++ modules/reddit-info.py
@@ -8,16 +8,16 @@ This module provides special tools for reddit, namely showing detailed info abou
 
 import reddit, re
 
-def setup(jenni):
+def setup(willie):
     regex = re.compile('http(?:s)?://(www\.)?reddit\.com/(r/.*?/comments/[\w-]+|u(ser)?/[\w-]+)')
-    if not hasattr(jenni.config, 'url_exclude'):
-        jenni.config.set_attr('url_exclude', [regex])
+    if not hasattr(willie.config, 'url_exclude'):
+        willie.config.set_attr('url_exclude', [regex])
     else:
-        jenni.config.url_exclude.append(regex)
+        willie.config.url_exclude.append(regex)
 
-def rpost_info(jenni, input):
-    r = reddit.Reddit(user_agent='phenny / jenni IRC bot - see dft.ba/-williesource for more')
-    s = r.get_submission(url=input.group(1))
+def rpost_info(willie, trigger):
+    r = reddit.Reddit(user_agent='phenny / willie IRC bot - see dft.ba/-williesource for more')
+    s = r.get_submission(url=trigger.group(1))
     
     message = '[REDDIT] '+s.title
     if s.is_self: message = message + ' (self.' + s.subreddit.display_name + ')'
@@ -29,25 +29,32 @@ def rpost_info(jenni, input):
                       +str(s.ups)+'|05'+str(s.downs)+') | '+str(s.num_comments)\
                       +' comments | Posted by '+s.author.name
     #TODO add creation time with s.created
-    jenni.say(message)
+    willie.say(message)
 rpost_info.rule = '.*(http(?:s)?://(www\.)?reddit\.com/r/.*?/comments/[\w-]+).*'
 
-def redditor_info(jenni, input):
-    r = reddit.Reddit(user_agent='phenny / jenni IRC bot - see dft.ba/-williesource for more')
-    u = r.get_redditor(input.group(2))#Handling auto-expansion and by command in the same function.
+def redditor_info(willie, trigger):
+    commanded = re.match(willie.config.prefix+'.*', trigger)
+    r = reddit.Reddit(user_agent='phenny / willie IRC bot - see dft.ba/-williesource for more')
+    try:
+        u = r.get_redditor(trigger.group(2))
+    except:
+        if commanded:
+            willie.say('No such Redditor.')
+        return
+        #Fail silently if it wasn't an explicit command.
     
     message = '[REDDITOR] '+u.name
-    if input.group(1) == '.redditor': message = message + ' | http://reddit.com/u/'+u.name
+    if commanded: message = message + ' | http://reddit.com/u/'+u.name
     if u.is_gold: message = message + ' | 08Gold'
     if u.is_mod: message = message + ' | 05Mod'
     message = message + ' | Link: '+str(u.link_karma)+ ' | Comment: '+str(u.comment_karma)
     
     #TODO detect cake day with u.created
-    jenni.say(message)
+    willie.say(message)
 #If you change this, you'll have to change some things above.
 redditor_info.commands = ['redditor']
 
-def auto_redditor_info(jenni, input):
-    redditor_info(jenni, input)
+def auto_redditor_info(willie, trigger):
+    redditor_info(willie, trigger)
 #If you change the groups here, you'll have to change some things above.
 auto_redditor_info.rule = '.*http(?:s)?://(?:www\.)?reddit\.com/u(ser)?/([\w-]+).*'
