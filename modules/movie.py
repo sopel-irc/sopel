sopel-irc/sopel

commit e95f8f9f662360c6b5250c710a1c95e34020157c

Clean up json imports now that 2.7 is required per issue #100

modules/movie.py
@@ -7,13 +7,7 @@ Licensed under the Eiffel Forum License 2.
 
 This module relies on imdbapi.com
 """
-try:
-    import json
-except ImportError:
-    import simplejson as json
-except ImportError:
-    print("Either update to python 2.6+ or install simplejson")
-import web
+import json
 
 def imdb(jenni, input):
     if not input.group(2):

modules/dftba.py
@@ -1,17 +1,13 @@
 """
-dftba.py - jenni DFT.BA Module
+dftba.py - Willie DFT.BA Module
 Author: Edward Powell, embolalia.net
-About: http://inamidst.com/phenny
+
+http://willie.dftba.net
 
 This module allows for retrieving stats, shortening and lengthening dft.ba urls.
 """
 import urllib
-try:
-    import json
-except ImportError:
-    import simplejson as json
-except ImportError:
-    print("Either update to python 2.6+ or install simplejson")
+import json
 
 def shorten(jenni, input):
     """Shorten a URL with DFT.BA"""

modules/twit.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 """
-twitter.py - jenni Twitter Module
+twitter.py - Wilie Twitter Module
 Copyright 2008-10, Michael Yanovich, opensource.osu.edu/~yanovich/wiki/
 Tweetwatch features copyright 2011, Edward Powell, embolalia.net
 Licensed under the Eiffel Forum License 2.
 
-http://inamidst.com/phenny/
+http://willie.dftba.net
 
 For this module to work, you must register the application with Twitter, and
 create the variables consumer_key, consumer_secret, access_token, and
@@ -15,12 +15,6 @@ access_token_secret in your config file (~/.jenni/default.py).
 #"twitter_username" with the username you have registered on twitter, and "twitter_password" with the password for that twitter 
 #account.
 """
-try:
-    import json
-except ImportError:
-    import simplejson as json
-except ImportError:
-    print("Either update to python 2.6+ or install simplejson")
 import tweepy
 import sched, time
 import re

commit 83c93c1ad3c442e4a536854288a0a0315404e2b5

[movie] Fix regression caused by commit e95f8f9f662360c6b5250c710a1c95e34020157c
Next time you mess with imports, actually check if things work
Also, removed the "is too slow at the moment" error so if there's a real exception we'll be able to see it.

modules/movie.py
@@ -8,6 +8,7 @@ Licensed under the Eiffel Forum License 2.
 This module relies on imdbapi.com
 """
 import json
+import web
 
 def imdb(willie, trigger):
     """
@@ -18,10 +19,7 @@ def imdb(willie, trigger):
     word=trigger.group(2).rstrip()
     word=word.replace(" ", "+")
     uri="http://www.imdbapi.com/?t="+word
-    try: u = web.get_urllib_object(uri, 30)
-    except:
-        willie.say('IMDBAPI is too slow at the moment :(')
-        return 'err'
+    u = web.get_urllib_object(uri, 30)
     data = json.load(u) #data is a Dict containing all the information we need
     u.close()
     if data['Response'] == 'False':