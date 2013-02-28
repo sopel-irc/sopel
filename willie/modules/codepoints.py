2af743618260b540bc37cb5c5c070e7e3a2abc2e
Edward Powell <powell.518@gmail.com>
2013-02-28 17:57:07 -0500

    [codepoints] pep8ify per issue #125

--- willie/modules/codepoints.py
+++ willie/modules/codepoints.py
@@ -5,22 +5,26 @@ Licensed under the Eiffel Forum License 2.
 
 http://willie.dfbta.net
 """
-
-import re, unicodedata
+import re
+import unicodedata
 from itertools import islice
+
 
 def about(u, cp=None, name=None):
     if cp is None:
         cp = ord(u)
     if name is None:
-        try: name = unicodedata.name(u)
+        try:
+            name = unicodedata.name(u)
         except ValueError:
             return 'U+%04X (No name found)' % cp
 
     if not unicodedata.combining(u):
         template = 'U+%04X %s (%s)'
-    else: template = 'U+%04X %s (\xe2\x97\x8c%s)'
+    else:
+        template = 'U+%04X %s (\xe2\x97\x8c%s)'
     return template % (cp, name, u.encode('utf-8'))
+
 
 def codepoint_simple(arg):
     arg = arg.upper()
@@ -30,8 +34,10 @@ def codepoint_simple(arg):
     results = []
     for cp in xrange(0xFFFF):
         u = unichr(cp)
-        try: name = unicodedata.name(u)
-        except ValueError: continue
+        try:
+            name = unicodedata.name(u)
+        except ValueError:
+            continue
 
         if r_label.search(name):
             results.append((len(name), u, cp, name))
@@ -39,8 +45,10 @@ def codepoint_simple(arg):
         r_label = re.compile('\\b' + arg.replace(' ', '.*\\b'))
         for cp in xrange(0xFFFF):
             u = unichr(cp)
-            try: name = unicodedata.name(u)
-            except ValueError: continue
+            try:
+                name = unicodedata.name(u)
+            except ValueError:
+                continue
 
             if r_label.search(name):
                 results.append((len(name), u, cp, name))
@@ -51,10 +59,13 @@ def codepoint_simple(arg):
     length, u, cp, name = sorted(results)[0]
     return about(u, cp, name)
 
+
 def codepoint_extended(arg):
     arg = arg.upper()
-    try: r_search = re.compile(arg)
-    except: raise ValueError('Broken regexp: %r' % arg)
+    try:
+        r_search = re.compile(arg)
+    except:
+        raise ValueError('Broken regexp: %r' % arg)
 
     for cp in xrange(1, 0x10FFFF):
         u = unichr(cp)
@@ -63,22 +74,25 @@ def codepoint_extended(arg):
         if r_search.search(name):
             yield about(u, cp, name)
 
+
 def u(willie, trigger):
     """Look up unicode information."""
     arg = trigger.bytes[3:]
     if not arg:
         return willie.reply('You gave me zero length trigger.')
     elif not arg.strip(' '):
-        if len(arg) > 1: return willie.reply('%s SPACEs (U+0020)' % len(arg))
+        if len(arg) > 1:
+            return willie.reply('%s SPACEs (U+0020)' % len(arg))
         return willie.reply('1 SPACE (U+0020)')
 
     # @@ space
     if set(arg.upper()) - set(
-        'ABCDEFGHIJKLMNOPQRSTUVWYXYZ0123456789- .?+*{}[]\\/^$'):
+            'ABCDEFGHIJKLMNOPQRSTUVWYXYZ0123456789- .?+*{}[]\\/^$'):
         printable = False
     elif len(arg) > 1:
         printable = True
-    else: printable = False
+    else:
+        printable = False
 
     if printable:
         extended = False
@@ -88,9 +102,12 @@ def u(willie, trigger):
                 break
 
         if len(arg) == 4:
-            try: u = unichr(int(arg, 16))
-            except ValueError: pass
-            else: return willie.say(about(u))
+            try:
+                u = unichr(int(arg, 16))
+            except ValueError:
+                pass
+            else:
+                return willie.say(about(u))
 
         if extended:
             # look up a codepoint with regexp
@@ -107,7 +124,8 @@ def u(willie, trigger):
             result = codepoint_simple(arg)
             if result is not None:
                 willie.say(result)
-            else: willie.reply("Sorry, no results for %r." % arg)
+            else:
+                willie.reply("Sorry, no results for %r." % arg)
     else:
         text = arg.decode('utf-8')
         # look up less than three podecoints
@@ -117,9 +135,11 @@ def u(willie, trigger):
         # look up more than three podecoints
         elif len(text) <= 10:
             willie.reply(' '.join('U+%04X' % ord(c) for c in text))
-        else: willie.reply('Sorry, your trigger is too long!')
+        else:
+            willie.reply('Sorry, your trigger is too long!')
 u.commands = ['u']
 u.example = '.u 203D'
+
 
 def bytes(willie, trigger):
     """Show the input as pretty printed bytes."""
