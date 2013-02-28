d18dd392e08ca012a89adcae23062ea95e86895e
Edward Powell <powell.518@gmail.com>
2013-02-28 17:35:13 -0500

    [calc] pep8ify per issue #125

--- willie/modules/calc.py
+++ willie/modules/calc.py
@@ -13,10 +13,11 @@ from socket import timeout
 import string
 import HTMLParser
 
+
 def calculate(trigger):
     q = trigger.encode('utf-8')
-    q = q.replace('\xcf\x95', 'phi') # utf-8 U+03D5
-    q = q.replace('\xcf\x80', 'pi') # utf-8 U+03C0
+    q = q.replace('\xcf\x95', 'phi')  # utf-8 U+03D5
+    q = q.replace('\xcf\x80', 'pi')  # utf-8 U+03C0
     uri = 'http://www.google.com/ig/calculator?q='
     bytes = web.get(uri + web.quote(q))
     parts = bytes.split('",')
@@ -30,7 +31,9 @@ def calculate(trigger):
         answer = answer.replace('</sup>', ')')
         answer = web.decode(answer)
         return answer
-    else: return 'Sorry, no result.'
+    else:
+        return 'Sorry, no result.'
+
 
 def c(willie, trigger):
     """Google calculator."""
@@ -41,6 +44,7 @@ def c(willie, trigger):
 c.commands = ['c', 'calc']
 c.example = '.c 5 + 3'
 
+
 def py(willie, trigger):
     """Evaluate a Python expression."""
     query = trigger.group(2).encode('utf-8')
@@ -48,9 +52,11 @@ def py(willie, trigger):
     answer = web.get(uri + web.quote(query))
     if answer:
         willie.say(answer)
-    else: willie.reply('Sorry, no result.')
+    else:
+        willie.reply('Sorry, no result.')
 py.commands = ['py']
 py.example = '.py len([1,2,3])'
+
 
 def wa(willie, trigger):
     """Wolfram Alpha calculator"""
@@ -65,22 +71,25 @@ def wa(willie, trigger):
     if answer:
         answer = answer.decode('string_escape')
         answer = HTMLParser.HTMLParser().unescape(answer)
-        #This might not work if there are more than one instance of escaped unicode chars
-        #But so far I haven't seen any examples of such output examples from Wolfram Alpha
+        #This might not work if there are more than one instance of escaped
+        #unicode chars But so far I haven't seen any examples of such output
+        #examples from Wolfram Alpha
         match = re.search('\\\:([0-9A-Fa-f]{4})', answer)
         if match is not None:
             char_code = match.group(1)
             char = unichr(int(char_code, 16))
-            answer = answer.replace('\:'+char_code, char)
+            answer = answer.replace('\:' + char_code, char)
         waOutputArray = string.split(answer, ";")
         if(len(waOutputArray) < 2):
-            willie.say('[WOLFRAM ERROR]'+answer)
+            willie.say('[WOLFRAM ERROR]' + answer)
         else:
-            
-            willie.say('[WOLFRAM] ' + waOutputArray[0]+" = "+waOutputArray[1])
+
+            willie.say('[WOLFRAM] ' + waOutputArray[0] + " = "
+                       + waOutputArray[1])
         waOutputArray = []
-    else: willie.reply('Sorry, no result.')
-wa.commands = ['wa','wolfram']
+    else:
+        willie.reply('Sorry, no result.')
+wa.commands = ['wa', 'wolfram']
 wa.example = '.wa circumference of the sun * pi'
 wa.commands = ['wa']
 
