9ef01a23816736f87eaac989977a346dda9af6cf
Edward Powell <powell.518@gmail.com>
2013-02-28 17:24:47 -0500

    [ai] PEP8ify per issue #125

--- willie/modules/ai.py
+++ willie/modules/ai.py
@@ -5,11 +5,12 @@ Licensed under the Eiffel Forum License 2.
 
 http://willie.dftba.net
 """
-
-import random, time
+import random
+import time
 
 random.seed()
 limit = 3
+
 
 def goodbye(willie, trigger):
     byemsg = random.choice(('Bye', 'Goodbye', 'Seeya', 'Auf Wiedersehen', 'Au revoir', 'Ttyl'))
@@ -21,7 +22,7 @@ goodbye.rate = 30
 
 
 def ty(willie, trigger):
-    human = random.uniform(0,9)
+    human = random.uniform(0, 9)
     time.sleep(human)
     mystr = trigger.group()
     mystr = str(mystr)
@@ -31,18 +32,21 @@ ty.rule = '(?i).*(thank).*(you).*(willie|$nickname).*$'
 ty.priority = 'high'
 ty.rate = 30
 
+
 def ty2(willie, trigger):
-    ty(willie,trigger)
+    ty(willie, trigger)
 ty2.rule = '(?i)$nickname\:\s+(thank).*(you).*'
 ty2.rate = 30
+
 
 def ty4(willie, trigger):
     ty(willie, trigger)
 ty4.rule = '(?i).*(thanks).*(willie|$nickname).*'
 ty4.rate = 40
 
-def yesno(willie,trigger):
-    rand = random.uniform(0,5)
+
+def yesno(willie, trigger):
+    rand = random.uniform(0, 5)
     text = trigger.group()
     text = text.split(":")
     text = text[1].split()
@@ -54,7 +58,8 @@ def yesno(willie,trigger):
 yesno.rule = '(willie|$nickname)\:\s+(yes|no)$'
 yesno.rate = 15
 
-def ping_reply (willie,trigger):
+
+def ping_reply(willie, trigger):
     text = trigger.group().split(":")
     text = text[1].split()
     if text[0] == 'PING' or text[0] == 'ping':
@@ -62,30 +67,35 @@ def ping_reply (willie,trigger):
 ping_reply.rule = '(?i)($nickname|willie)\:\s+(ping)\s*'
 ping_reply.rate = 30
 
-def love (willie, trigger):
+
+def love(willie, trigger):
     willie.reply("I love you too.")
 love.rule = '(?i)i.*love.*(willie|$nickname).*'
 love.rate = 30
 
-def love2 (willie, trigger):
+
+def love2(willie, trigger):
     willie.reply("I love you too.")
 love2.rule = '(?i)(willie|$nickname)\:\si.*love.*'
 love2.rate = 30
 
-def love3 (willie, trigger):
+
+def love3(willie, trigger):
     willie.reply("I love you too.")
 love3.rule = '(?i)(willie|$nickname)\,\si.*love.*'
 love3.rate = 30
+
 
 def f_lol(willie, trigger):
     randnum = random.random()
     if 0 < randnum < limit:
         respond = ['haha', 'lol', 'rofl']
-        randtime = random.uniform(0,9)
+        randtime = random.uniform(0, 9)
         time.sleep(randtime)
         willie.say(random.choice(respond))
 f_lol.rule = '(haha!?|lol!?)$'
 f_lol.priority = 'high'
+
 
 def f_bye(willie, trigger):
     respond = ['bye!', 'bye', 'see ya', 'see ya!']
@@ -93,22 +103,25 @@ def f_bye(willie, trigger):
 f_bye.rule = '(g2g!?|bye!?)$'
 f_bye.priority = 'high'
 
+
 def f_heh(willie, trigger):
     randnum = random.random()
     if 0 < randnum < limit:
         respond = ['hm']
-        randtime = random.uniform(0,7)
+        randtime = random.uniform(0, 7)
         time.sleep(randtime)
         willie.say(random.choice(respond))
 f_heh.rule = '(heh!?)$'
 f_heh.priority = 'high'
 
+
 def f_really(willie, trigger):
-    randtime = random.uniform(10,45)
+    randtime = random.uniform(10, 45)
     time.sleep(randtime)
     willie.say(str(trigger.nick) + ": " + "Yes, really.")
 f_really.rule = r'(?i)$nickname\:\s+(really!?)'
 f_really.priority = 'high'
+
 
 def wb(willie, trigger):
     willie.reply("Thank you!")
