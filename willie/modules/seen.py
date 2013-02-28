e8bdca456a9f749ec15c9f4dee29484fce50f51d
Edward Powell <powell.518@gmail.com>
2013-02-27 20:44:59 -0500

    [seen] Make seen show time in requesting user's timezone
    Issue #192

--- willie/modules/seen.py
+++ willie/modules/seen.py
@@ -9,21 +9,34 @@ http://willie.dftba.net
 """
 
 import time
-from willie.tools import Ddict
+import datetime
+import pytz
+from willie.tools import Ddict, Nick
 
 seen_dict=Ddict(dict)
+
+def get_tz(willie, nick):
+    tz = 'asdf'
+    if willie.db and nick in willie.db.preferences:
+            tz = willie.db.preferences.get(nick, 'tz')
+    if tz not in pytz.all_timezones_set:
+        tz = 'UTC'
+    return tz.strip()
 
 def seen(willie, trigger):
     """Reports when and where the user was last seen."""
     if not trigger.group(2):
         willie.say(".seen <nick> - Reports when <nick> was last seen.")
         return
-    nick = trigger.group(2).lower()
+    nick = Nick(trigger.group(2).strip())
     if seen_dict.has_key(nick):
         timestamp = seen_dict[nick]['timestamp']
         channel = seen_dict[nick]['channel']
         message = seen_dict[nick]['message']
-        timestamp = time.strftime('%Y-%m-%d %H:%M:%S UTC', time.gmtime(timestamp))
+
+        tz = get_tz(willie, nick)
+        saw = datetime.datetime.fromtimestamp(timestamp, pytz.timezone(tz))
+        timestamp = saw.strftime('%Y-%m-%d %H:%M:%S %Z')
 
         msg = "I last saw %s at %s on %s, saying %s" % (nick, timestamp, channel, message)
         willie.say(str(trigger.nick) + ': ' + msg)
@@ -33,7 +46,7 @@ seen.commands = ['seen']
 
 def note(willie, trigger):
     if trigger.sender.startswith('#'):
-        nick = trigger.nick.lower()
+        nick = Nick(trigger.nick)
         seen_dict[nick]['timestamp'] = time.time()
         seen_dict[nick]['channel'] = trigger.sender
         seen_dict[nick]['message'] = trigger
