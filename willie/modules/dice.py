5eaece0e969eddf2dfa5b4f5954697f9657fc0fe
Dimitri Molenaars <Tyrope@TyRope.nl>
2013-03-04 18:38:25 +0100

    [dice]some optimization.
    Signed-off-by: Dimitri Molenaars <Tyrope@TyRope.nl>

--- willie/modules/dice.py
+++ willie/modules/dice.py
@@ -1,6 +1,6 @@
 """
 dice.py - Dice Module
-Copyright 2010-2012, Dimitri "Tyrope" Molenaars, TyRope.nl
+Copyright 2010-2013, Dimitri "Tyrope" Molenaars, TyRope.nl
 Licensed under the Eiffel Forum License 2.
 
 http://willie.dftba.net/
@@ -21,7 +21,7 @@ def dice(willie, trigger):
     no_dice = True
     if not trigger.group(2):
         return willie.reply('You have to specify the dice you wanna roll.')
-    arr = trigger.group(2).lower().strip(' ')
+    arr = trigger.group(2).lower().replace(' ','')
     arr = arr.replace('-', ' - ').replace('+', ' + ').replace('/', ' / ')
     arr = arr.replace('*', ' * ').replace('(', ' ( ').replace(')', ' ) ')
     arr = arr.replace('^', ' ^ ').replace('()', '').split(' ')
@@ -35,19 +35,14 @@ def dice(willie, trigger):
             if result.group(3) is not None:
                 #check for invalid droplowest
                 dropLowest = int(result.group(3)[1:])
-                if(result.group(2).lower().startswith('d')):
-                    if(dropLowest != 0):
-                        willie.reply('You\'re trying to drop too many dice.')
-                        return
-                elif(dropLowest
-                     >= int('0' + result.group(2).lower().split('d')[0])):
+                if(dropLowest >= int(result.group(2).split('d')[0] or 1)):
                     willie.reply('You\'re trying to drop too many dice.')
                     return
             else:
                 dropLowest = 0
             #dicerolling
             value, drops = '(', ''
-            dice = rollDice(result.group(2).lower())
+            dice = rollDice(result.group(2))
             for i in range(0, len(dice)):
                 if i < dropLowest:
                     if drops == '':
@@ -69,28 +64,25 @@ def dice(willie, trigger):
     #repeat next segment
 
     #we're replacing, splitting and joining to exclude []'s from the math.
-    result = calculate(''.join(full_string.replace('[', '#').replace(']', '#').split('#')[::2]))
+    result = calculate(''.join(
+                full_string.replace('[', '#').replace(']', '#').split('#')[::2]))
     if result == 'Sorry, no result.':
         willie.reply('Calculation failed, did you try something weird?')
     elif(no_dice):
         willie.reply('For pure math, you can use .c '
                      + trigger.group(2) + ' = ' + result)
     else:
-        willie.reply('You roll ' + trigger.group(2) + ' (' + full_string + '): '
+        willie.reply('You roll ' + trigger.group(2) + ': ' + full_string + ' = '
                      + result)
 dice.commands = ['roll', 'dice', 'd']
 dice.priority = 'medium'
 
 
 def rollDice(diceroll):
-    if(diceroll.startswith('d')):
-        rolls = 1
-        size = int(diceroll[1:])
-    else:
-        rolls = int(diceroll.split('d')[0])
-        size = int(diceroll.split('d')[1])
+    rolls = int(diceroll.split('d')[0] or 1)
+    size = int(diceroll.split('d')[1])
     result = []  # dice results.
-
+    
     for i in range(1, rolls + 1):
         #roll 10 dice, pick a random dice to use, add string to result.
         result.append((randint(1, size), randint(1, size), randint(1, size),
