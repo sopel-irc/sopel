eb44c794cbc54a3be8582ca2dfc547f30b8d0b4b
tcki <teckie@game-proz.com>
2013-04-16 22:21:03 +0000

    rss debug argument, auth config, tweaks

--- willie/modules/rss.py
+++ willie/modules/rss.py
@@ -13,18 +13,31 @@ import socket
 import sys
 import time
 
+COOKIE = ''
+DEBUG = False
+INTERVAL = 10
+STOP = False
+
+def checkConfig(willie):
+    if not willie.config.has_option('rss', 'auth_url') or not willie.config.has_option('rss', 'auth_user') or not willie.config.has_option('rss', 'auth_pass'):
+        return False
+    else:
+        return [willie.config.rss.auth_url, willie.config.rss.auth_user, willie.config.rss.auth_pass]
+
+def configure(config):
+    chunk = ''
+    if config.option('Configuring RSS module', False):
+        config.interactive_add('rss', 'auth_url', 'RSS authentication page', '')
+        config.interactive_add('rss', 'auth_user', 'RSS authentication user', '')
+        config.interactive_add('rss', 'auth_pass', 'RSS authentication password', '')
+    return chunk
 
 def manage_rss(willie, trigger):
-    """ .rss [list|add|del|start|stop|auth] <#channel> <title> <url> """
+    """ .rss [list|add|del|start|stop|auth|debug] <#channel> <title> <url> """
     if not trigger.admin:
         return
-    DEBUG = False
-    INTERVAL = 10
-    STOP = False
-    COOKIE = ''
+    global COOKIE, DEBUG, INTERVAL, STOP
     text = trigger.group().split()
-    if DEBUG:
-        willie.reply(unicode(text))
     conn = willie.db.connect()
     db = conn.cursor()
     db.execute('CREATE TABLE IF NOT EXISTS feeds(channel text,title text,url text)')
@@ -71,7 +84,7 @@ def manage_rss(willie, trigger):
                 STOP = False
                 break
             if DEBUG:
-                willie.reply('Sync...')
+                willie.reply('Synchronizing...')
             db.execute('SELECT * FROM feeds')
             for row in db:
                 rows += 1
@@ -81,8 +94,10 @@ def manage_rss(willie, trigger):
                 'url': row[2]
                 }
                 try:
-                    fp = feedparser.parse(feed['url'])
-                    #extra_headers={'Cookie': COOKIE}
+                    if COOKIE != '':
+                        fp = feedparser.parse(feed['url'], extra_headers={'Cookie': COOKIE})
+                    else:
+                        fp = feedparser.parse(feed['url'])
                 except IOError, E:
                     willie.reply('Invalid entry: ' + str(E))
                 entry = fp.entries[0]
@@ -102,22 +117,32 @@ def manage_rss(willie, trigger):
                     if DEBUG:
                         willie.reply('Old entry: ' + message)
             if rows == 0:
-                STOP = True
                 willie.reply('No feeds in database.')
+                STOP = True
             if DEBUG:
-                willie.reply('Done.')
+                willie.reply('Synchronized.')
             time.sleep(INTERVAL)
         if DEBUG:
             willie.reply('Stopped.')
     elif text[1] == 'stop':
-        STOP = True
         willie.reply('Stopping...')
+        STOP = True
+    elif text[1] == 'debug':
+        if DEBUG:
+            willie.reply('Stopping debug.')
+            DEBUG = False
+        else:
+            willie.reply('Starting debug.')
+            DEBUG = True
     elif text[1] == 'auth':
-        response = mechanize.urlopen(mechanize.Request('http://alphachat.net/forums'))
+        if DEBUG:
+            willie.reply('Authenticating as %s on %s...' % (cfg[1], cfg[0]))
+        cfg = checkConfig(willie)
+        response = mechanize.urlopen(mechanize.Request(cfg[0]))
         forms = mechanize.ParseResponse(response, backwards_compat=False)
         response.close()
-        forms[0]['login'] = 'tckibot'
-        forms[0]['password'] = '******'
+        forms[0]['login'] = cfg[1]
+        forms[0]['password'] = cfg[2]
         response2 = mechanize.urlopen(forms[0].click())
         for name, value in response2.info().items():
             if DEBUG:
