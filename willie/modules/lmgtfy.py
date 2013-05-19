b77161097314f2f792a238139c0c72aa26378057
Dimitri Molenaars <Tyrope@TyRope.nl>
2013-05-19 20:40:56 +0200

    [lmgtfy]Rename def (CC #258)
    Signed-off-by: Dimitri Molenaars <Tyrope@TyRope.nl>

--- willie/modules/lmgtfy.py
+++ willie/modules/lmgtfy.py
@@ -6,14 +6,14 @@ Licensed under the Eiffel Forum License 2.
 http://willie.dftba.net/
 """
 
-def issue(willie, trigger):
+def googleit(willie, trigger):
     """Let me just... google that for you."""
     #No input
     if not trigger.group(2):
         return willie.say('http://google.com/')
     willie.say('http://lmgtfy.com/?q='+trigger.group(2).replace(' ','+'))
-issue.commands = ['lmgtfy','lmgify','gify','gtfy']
-issue.priority = 'medium'
+googleit.commands = ['lmgtfy','lmgify','gify','gtfy']
+googleit.priority = 'medium'
 
 if __name__ == '__main__':
     print __doc__.strip()
