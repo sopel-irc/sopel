66c3d660f8a7ae6ab6a3cc7b5c5df3a130857cbe
Dimitri Molenaars <Tyrope@TyRope.nl>
2013-05-27 16:55:21 +0200

    [Tests]Merged test.py and core.py, added some TODOs

--- tests/core.py
+++ tests/core.py
@@ -1,6 +1,5 @@
-import sys
-import test, config
-
+import config, sys
+from time import sleep
 try:
     from twisted.words.protocols import irc
     from twisted.internet import reactor, protocol
@@ -33,13 +32,73 @@ class trigger():
         except:
             self.host = ''
 
+class Tests():
+    """ Test handler """
+    activeTest = None
+    willieReply = None
+
+    # List of tests to be executed,
+    # Test syntax:
+    # ['Name', (msg|action, target, message), (msg|action|raw, target, expected output (regex-enabled))]
+    # To use trigger values, call as %trigger.value%
+    tests = [
+        #TODO: Automatically generate this list
+        ['GreetPriv', ('msg', config.Willie_nick, config.Willie_nick+'!'), ('msg', config.Nickname, config.Nickname+'!')],
+        ['GreetPub', ('msg', config.Channel, config.Willie_nick+'!'), ('msg', config.Channel, config.Nickname+'!')],
+    ]
+
+    def executeTests(self, willie):
+        for test in self.tests:
+            self.activeTest = test[0]
+            print "Test: "+test[0]
+            if test[1][0] == 'msg':
+                willie.say(test[1][1], test[1][2])
+            elif test[1][0] == 'action':
+                willie.describe(test[1][1], test[1][2])
+            #Wait for result.
+            while not self.willieReply:
+                #Bad, BAAAAAAAAAAAAAAD...
+                sleep(5)
+
+            if self.willieReply != test[2]:
+                print "  FAILED"
+                print "  Expected output: "+str(test[2])
+                print "  Received:        "+str(test[2])
+            else: print "  SUCCESS"
+            self.willieReply = None
+        print "Testing complete."
+        willie.quit("Testing complete.")
+        #TODO exit Willie
+        sys.exit(0)
+
+    def onJoin(self, willie, trigger):
+        if trigger.nick == config.Willie_nick:
+            if(self.activeTest == None): #We're not testing yet
+                self.executeTests(willie) #Start testing
+
+    def onMsg(self, willie, trigger):
+        if trigger.nick != config.Willie_nick:
+            return #Message didn't originate from Willie
+        if self.activeTest == None:
+            return #No active tests
+        else:
+            self.willieReply = ('msg', trigger.msg)
+
+    def onAction(self, willie, trigger):
+        if trigger.nick != config.Willie_nick:
+            return #Message didn't originate from Willie
+        if self.activeTest == None:
+            return #No active tests
+        else:
+            self.willieReply = ('action', trigger.msg)
+
 class WillieTest(irc.IRCClient):
     """ IRC data handler """
     nickname = config.Nickname
     username = config.Username
     password = config.Server_Password
     realname = 'Willie test unit'
-    testUnit = test.test()
+    testUnit = Tests()
 
     def connectionLost(self, reason):
         print "Disconnected: "+str(reason).split(":")[3]
@@ -48,21 +107,21 @@ class WillieTest(irc.IRCClient):
         self.join(config.Channel)
         if config.NS_Password != '':
             self.msg('NickServ', 'IDENTIFY '+config.NS_Password)
-        self.msg(config.Channel, 'Testing unit online.')
+        #TODO: Launch Willie
 
     def userJoined(self, user, channel):
         trig = trigger(channel, user)
-        self.testUnit.onJoin(trig)
+        self.testUnit.onJoin(self, trig)
 
     def privmsg(self, user, channel, message):
         trig = trigger(channel, user)
         trig.msg = message
-        self.testUnit.onMsg(trig)
+        self.testUnit.onMsg(self, trig)
 
     def action(self, user, channel, data):
         trig = trigger(channel, user)
         trig.msg = data
-        self.testUnit.onAction(trig)
+        self.testUnit.onAction(self, trig)
 
 class testFactory(protocol.ClientFactory):
     """ TCP Connection handler """
