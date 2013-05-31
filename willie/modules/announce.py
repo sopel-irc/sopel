ee0b4c5c9fd25951c306000fd01caa8482dc939f
Elad Alfassa <elad@fedoraproject.org>
2013-06-01 00:46:28 +0300

    [announce] Update to 4.0 standard
    Issue #276
    Also removed useless print

--- willie/modules/announce.py
+++ willie/modules/announce.py
@@ -5,15 +5,16 @@ Copyright © 2013, Elad Alfassa, <elad@fedoraproject.org>
 Licensed under the Eiffel Forum License 2.
 
 """
-def announce(willie, trigger):
+from willie.module import command, example
+
+@command('announce')
+@example('.announce Some important message here')
+def announce(bot, trigger):
     """
     Send an announcement to all channels the bot is in
     """
     if not trigger.admin:
-        willie.reply('Sorry, I can\'t let you do that')
+        bot.reply('Sorry, I can\'t let you do that')
         return
-    print willie.channels
-    for channel in willie.channels:
-        willie.msg(channel, '[ANNOUNCMENT] %s' % trigger.group(2))
-announce.commands = ['announce']
-announce.example = '.announce Some important message here'
+    for channel in bot.channels:
+        bot.msg(channel, '[ANNOUNCMENT] %s' % trigger.group(2))
