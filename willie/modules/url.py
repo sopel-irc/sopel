f2512c05f2f1be2858843bc4b3fe7e6916dec868
Edward Powell <powell.518@gmail.com>
2013-06-08 17:35:23 -0400

    [url] Handle when page doesn't give Content-Type at all

--- willie/modules/url.py
+++ willie/modules/url.py
@@ -189,7 +189,7 @@ def find_title(url):
     # We want to try for UTF-8 if we can
     content = web.get(url, headers={'Accept-Charset': 'utf-8'})
     headers = web.head(url, headers={'Accept-Charset': 'utf-8'})
-    content_type = headers.get('Content-Type')
+    content_type = headers.get('Content-Type') or ''
     encoding_match = re.match('.*?charset *= *(\S+)', content_type)
     # If they gave us something else instead, try that
     if encoding_match:
