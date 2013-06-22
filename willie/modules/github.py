aab782f562a3f90d191dc265499457ebafffbf93
Edward Powell <powell.518@gmail.com>
2013-06-22 18:02:01 -0400

    [github, meetbot, url, youtube] 4.0 standards per #276

--- willie/modules/github.py
+++ willie/modules/github.py
@@ -10,13 +10,15 @@ from datetime import datetime
 from urllib2 import HTTPError
 import json
 import re
-import willie.web as web
+from willie import web
+from willie.module import commands
 
-def checkConfig(willie):
-    if not willie.config.has_option('github', 'oauth_token') or not willie.config.has_option('github', 'repo'):
+def checkConfig(bot):
+    if not bot.config.has_option('github', 'oauth_token') or not bot.config.has_option('github', 'repo'):
         return False
     else:
-        return [willie.config.github.oauth_token, willie.config.github.repo]
+        return [bot.config.github.oauth_token, bot.config.github.repo]
+
 
 def configure(config):
     """
@@ -31,57 +33,59 @@ def configure(config):
         config.interactive_add('github', 'repo', 'Github repository', 'embolalia/willie')
     return chunk
 
-def issue(willie, trigger):
+
+@commands('makeissue', 'makebug')
+def issue(bot, trigger):
     """Create a GitHub issue, also known as a bug report. Syntax: .makeissue Title of the bug report"""
     #check input
     if not trigger.group(2):
-        return willie.say('Please title the issue')
+        return bot.say('Please title the issue')
 
     #Is the Oauth token and repo available?
-    gitAPI = checkConfig(willie)
+    gitAPI = checkConfig(bot)
     if gitAPI == False:
-        return willie.say('Git module not configured, make sure github.oauth_token and github.repo are defined')
+        return bot.say('Git module not configured, make sure github.oauth_token and github.repo are defined')
 
     #parse input
-    now = ' '.join(str(datetime.utcnow()).split(' ')).split('.')[0]+' UTC'
+    now = ' '.join(str(datetime.utcnow()).split(' ')).split('.')[0] + ' UTC'
     body = 'Submitted by: %s\nFrom channel: %s\nAt %s' % (trigger.nick, trigger.sender, now)
-    data = {"title":trigger.group(2).encode('utf-8'), "body":body, "labels": ["IRC"]}
+    data = {"title": trigger.group(2).encode('utf-8'), "body": body, "labels": ["IRC"]}
     #submit
     try:
-        raw = web.post('https://api.github.com/repos/'+gitAPI[1]+'/issues?access_token='+gitAPI[0], json.dumps(data))
+        raw = web.post('https://api.github.com/repos/' + gitAPI[1] + '/issues?access_token=' + gitAPI[0], json.dumps(data))
     except HTTPError:
-        return willie.say('The GitHub API returned an error.')
-    
-    data = json.loads(raw)
-    willie.say('Issue #%s posted. %s' % (data['number'], data['html_url']))
-    willie.debug('GitHub','Issue #%s created in %s' % (data['number'],trigger.sender),'warning')
-issue.commands = ['makeissue','makebug']
-issue.priority = 'medium'
+        return bot.say('The GitHub API returned an error.')
 
-def findIssue(willie, trigger):
+    data = json.loads(raw)
+    bot.say('Issue #%s posted. %s' % (data['number'], data['html_url']))
+    bot.debug('GitHub', 'Issue #%s created in %s' % (data['number'], trigger.sender), 'warning')
+
+
+@commands('findissue', 'findbug')
+def findIssue(bot, trigger):
     """Search for a GitHub issue by keyword or ID. usage: .findissue search keywords/ID (optional) You can specify the first keyword as "CLOSED" to search closed issues."""
     if not trigger.group(2):
-        return willie.reply('What are you searching for?')
+        return bot.reply('What are you searching for?')
 
     #Is the Oauth token and repo available?
-    gitAPI = checkConfig(willie)
+    gitAPI = checkConfig(bot)
     if gitAPI == False:
-        return willie.say('Git module not configured, make sure github.oauth_token and github.repo are defined')
+        return bot.say('Git module not configured, make sure github.oauth_token and github.repo are defined')
     firstParam = trigger.group(2).split(' ')[0]
     if firstParam.isdigit():
         URL = 'https://api.github.com/repos/%s/issues/%s' % (gitAPI[1], trigger.group(2))
     elif firstParam == 'CLOSED':
-        if '%20'.join(trigger.group(2).split(' ')[1:]) not in ('','\x02','\x03'):
-            URL = 'https://api.github.com/legacy/issues/search/'+gitAPI[1]+'/closed/'+'%20'.join(trigger.group(2).split(' ')[1:])
+        if '%20'.join(trigger.group(2).split(' ')[1:]) not in ('', '\x02', '\x03'):
+            URL = 'https://api.github.com/legacy/issues/search/' + gitAPI[1] + '/closed/' + '%20'.join(trigger.group(2).split(' ')[1:])
         else:
-            return willie.reply('What are you searching for?')
+            return bot.reply('What are you searching for?')
     else:
         URL = 'https://api.github.com/legacy/issues/search/%s/open/%s' % (gitAPI[1], trigger.group(2))
 
     try:
         raw = web.get(URL)
     except HTTPError:
-        return willie.say('The GitHub API returned an error.')
+        return bot.say('The GitHub API returned an error.')
 
     try:
         if firstParam.isdigit():
@@ -89,16 +93,10 @@ def findIssue(willie, trigger):
         else:
             data = json.loads(raw)['issues'][-1]
     except (KeyError, IndexError):
-        return willie.say('No search results.')
+        return bot.say('No search results.')
     if len(data['body'].split('\n')) > 1:
-        body = data['body'].split('\n')[0]+'...'
+        body = data['body'].split('\n')[0] + '...'
     else:
         body = data['body'].split('\n')[0]
-    willie.reply('[#%s]\x02title:\x02 %s \x02|\x02 %s' % (data['number'],data['title'],body))
-    willie.say(data['html_url'])
-findIssue.commands = ['findissue','findbug']
-findIssue.priority = 'medium'
-
-if __name__ == '__main__':
-    print __doc__.strip()
-
+    bot.reply('[#%s]\x02title:\x02 %s \x02|\x02 %s' % (data['number'], data['title'], body))
+    bot.say(data['html_url'])
--- willie/modules/meetbot.py
+++ willie/modules/meetbot.py
@@ -10,8 +10,10 @@ import time
 import os
 import urllib2
 from willie.modules.url import find_title
+from willie.module import example, commands, rule, priority
 from willie.tools import Ddict
 import codecs
+
 
 def configure(config):
     """
@@ -24,7 +26,7 @@ def configure(config):
         config.interactive_add('meetbot', 'meeting_log_path', "Path to meeting logs storage directory (should be an absolute path, accessible on a webserver)")
         config.interactive_add('meetbot', 'meeting_log_baseurl', "Base URL for the meeting logs directory (eg. http://example.com/logs)")
 
-""" 
+"""
 meetings_dict is a 2D dict.
 
 Each meeting should have:
@@ -38,9 +40,10 @@ current subject
 Using channel as the meeting ID as there can't be more than one meeting in a channel at the same time.
 """
 
-meetings_dict=Ddict(dict) #Saves metadata about currently running meetings
-meeting_log_path = '' #To be defined on meeting start as part of sanity checks, used by logging functions so we don't have to pass them willie
-meeting_log_baseurl = '' #To be defined on meeting start as part of sanity checks, used by logging functions so we don't have to pass them willie
+meetings_dict = Ddict(dict)  # Saves metadata about currently running meetings
+meeting_log_path = ''  # To be defined on meeting start as part of sanity checks, used by logging functions so we don't have to pass them bot
+meeting_log_baseurl = ''  # To be defined on meeting start as part of sanity checks, used by logging functions so we don't have to pass them bot
+
 
 #Get the logfile name for the meeting in the requested channel
 #Used by all logging functions
@@ -57,6 +60,7 @@ def figure_logfile_name(channel):
     filename = timestring + '_' + name
     return filename
 
+
 #Start HTML log
 def logHTML_start(channel):
     logfile = codecs.open(meeting_log_path + channel + '/' + figure_logfile_name(channel) + '.html', 'a', encoding='utf-8')
@@ -65,12 +69,14 @@ def logHTML_start(channel):
     logfile.write('<!doctype html>\n<html>\n<head>\n<meta charset="utf-8">\n<title>%TITLE%</title>\n</head>\n<body>\n<h1>%TITLE%</h1>\n'.replace('%TITLE%', title))
     logfile.write('<h4>Meeting started by %s</h4><ul>\n' % meetings_dict[channel]['head'])
     logfile.close()
-    
+
+
 #Write a list item in the HTML log
 def logHTML_listitem(item, channel):
     logfile = codecs.open(meeting_log_path + channel + '/' + figure_logfile_name(channel) + '.html', 'a', encoding='utf-8')
-    logfile.write('<li>'+item+'</li>\n')
+    logfile.write('<li>' + item + '</li>\n')
     logfile.close()
+
 
 #End the HTML log
 def logHTML_end(channel):
@@ -82,12 +88,14 @@ def logHTML_end(channel):
     logfile.write('\n</body>\n</html>')
     logfile.close()
 
+
 #Write a string to the plain text log
 def logplain(item, channel):
     current_time = time.strftime('%H:%M:%S', time.gmtime())
     logfile = codecs.open(meeting_log_path + channel + '/' + figure_logfile_name(channel) + '.log', 'a', encoding='utf-8')
-    logfile.write('['+ current_time + '] '+item+'\r\n')
+    logfile.write('[' + current_time + '] ' + item + '\r\n')
     logfile.close()
+
 
 #Check if a meeting is currently running
 def ismeetingrunning(channel):
@@ -99,30 +107,34 @@ def ismeetingrunning(channel):
     except:
         return False
 
+
 #Check if nick is a chair or head of the meeting
-def ischair(nick,channel):
+def ischair(nick, channel):
     try:
         if nick.lower() == meetings_dict[channel]['head'] or nick.lower() in meetings_dict[channel]['chairs']:
             return True
         else:
-            return False;
+            return False
     except:
-        return False;
+        return False
+
 
 #Start meeting (also preforms all required sanity checks)
-def startmeeting(willie, trigger):
+@commands('startmeeting')
+@example('.startmeeting title or .startmeeting')
+def startmeeting(bot, trigger):
     """
     Start a meeting.
     https://github.com/embolalia/willie/wiki/Using-the-meetbot-module
     """
     if ismeetingrunning(trigger.sender):
-        willie.say('Can\'t do that, there is already a meeting in progress here!')
+        bot.say('Can\'t do that, there is already a meeting in progress here!')
         return
     if not trigger.sender.startswith('#'):
-        willie.say('Can only start meetings in channels')
+        bot.say('Can only start meetings in channels')
         return
-    if not willie.config.has_section('meetbot'):
-        willie.say('Meetbot not configured, make sure meeting_log_path and meeting_log_baseurl are defined')
+    if not bot.config.has_section('meetbot'):
+        bot.say('Meetbot not configured, make sure meeting_log_path and meeting_log_baseurl are defined')
         return
     #Start the meeting
     meetings_dict[trigger.sender]['start'] = time.time()
@@ -132,162 +144,162 @@ def startmeeting(willie, trigger):
         meetings_dict[trigger.sender]['title'] = trigger.group(2)
     meetings_dict[trigger.sender]['head'] = trigger.nick.lower()
     meetings_dict[trigger.sender]['running'] = True
-    
+
     global meeting_log_path
-    meeting_log_path = willie.config.meetbot.meeting_log_path
+    meeting_log_path = bot.config.meetbot.meeting_log_path
     if not meeting_log_path.endswith('/'):
         meeting_log_path = meeting_log_path + '/'
     global meeting_log_baseurl
-    meeting_log_baseurl = willie.config.meetbot.meeting_log_baseurl
+    meeting_log_baseurl = bot.config.meetbot.meeting_log_baseurl
     if not meeting_log_baseurl.endswith('/'):
         meeting_log_baseurl = meeting_log_baseurl + '/'
     if not os.path.isdir(meeting_log_path + trigger.sender):
         try:
             os.makedirs(meeting_log_path + trigger.sender)
         except Exception as e:
-            willie.say("Can't create log directory for this channel, meeting not started!")
+            bot.say("Can't create log directory for this channel, meeting not started!")
             meetings_dict[trigger.sender] = Ddict(dict)
             raise
             return
     #Okay, meeting started!
     logplain('Meeting started by ' + trigger.nick.lower(), trigger.sender)
     logHTML_start(trigger.sender)
-    willie.say('Meeting started! use .action, .agreed, .info, .chairs and .subject to control the meeting. to end the meeting, type .endmeeting')
+    bot.say('Meeting started! use .action, .agreed, .info, .chairs and .subject to control the meeting. to end the meeting, type .endmeeting')
 
-
-startmeeting.commands = ['startmeeting']
-startmeeting.example = '.startmeeting title or .startmeeting'
 
 #Change the current subject (will appear as <h3> in the HTML log)
-def meetingsubject(willie, trigger):
+@commands('subject')
+@example('.subject roll call')
+def meetingsubject(bot, trigger):
     """
     Change the meeting subject.
     https://github.com/embolalia/willie/wiki/Using-the-meetbot-module
     """
     if not ismeetingrunning(trigger.sender):
-        willie.say('Can\'t do that, start meeting first')
+        bot.say('Can\'t do that, start meeting first')
         return
     if not trigger.group(2):
-        willie.say('what is the subject?')
+        bot.say('what is the subject?')
         return
     if not ischair(trigger.nick, trigger.sender):
-        willie.say('Only meeting head or chairs can do that')
+        bot.say('Only meeting head or chairs can do that')
         return
     meetings_dict[trigger.sender]['current_subject'] = trigger.group(2)
     logfile = open(meeting_log_path + trigger.sender + '/' + figure_logfile_name(trigger.sender) + '.html', 'a')
-    logfile.write('</ul><h3>'+trigger.group(2)+'</h3><ul>')
+    logfile.write('</ul><h3>' + trigger.group(2) + '</h3><ul>')
     logfile.close()
-    logplain('Current subject: ' + trigger.group(2) +', (set by ' + trigger.nick +')', trigger.sender)
-    willie.say('Current subject: ' + trigger.group(2))
-meetingsubject.commands = ['subject']
-meetingsubject.example = '.subject roll call'
+    logplain('Current subject: ' + trigger.group(2) + ', (set by ' + trigger.nick + ')', trigger.sender)
+    bot.say('Current subject: ' + trigger.group(2))
+
 
 #End the meeting
-def endmeeting(willie, trigger):
+@commands('.endmeeting')
+@example('.endmeeting')
+def endmeeting(bot, trigger):
     """
     End a meeting.
     https://github.com/embolalia/willie/wiki/Using-the-meetbot-module
     """
     if not ismeetingrunning(trigger.sender):
-        willie.say('Can\'t do that, start meeting first')
+        bot.say('Can\'t do that, start meeting first')
         return
     if not ischair(trigger.nick, trigger.sender):
-        willie.say('Only meeting head or chairs can do that')
+        bot.say('Only meeting head or chairs can do that')
         return
-    meeting_length = time.time() - meetings_dict[trigger.sender]['start'] 
+    meeting_length = time.time() - meetings_dict[trigger.sender]['start']
     #TODO: Humanize time output
-    willie.say("Meeting ended! total meeting length %d seconds" % meeting_length)
+    bot.say("Meeting ended! total meeting length %d seconds" % meeting_length)
     logHTML_end(trigger.sender)
     htmllog_url = meeting_log_baseurl + urllib2.quote(trigger.sender + '/' + figure_logfile_name(trigger.sender) + '.html')
     logplain('Meeting ended by %s, total meeting length %d seconds' % (trigger.nick, meeting_length), trigger.sender)
-    willie.say('Meeting minutes: ' + htmllog_url)
+    bot.say('Meeting minutes: ' + htmllog_url)
     meetings_dict[trigger.sender] = Ddict(dict)
 
-endmeeting.commands = ['endmeeting']
-endmeeting.example = '.endmeeting'
 
 #Set meeting chairs (people who can control the meeting)
-def chairs(willie, trigger):
+@commands('chairs')
+@example('.chairs Tyrope Jason elad')
+def chairs(bot, trigger):
     """
     Set the meeting chairs.
     https://github.com/embolalia/willie/wiki/Using-the-meetbot-module
     """
     if not ismeetingrunning(trigger.sender):
-        willie.say('Can\'t do that, start meeting first')
+        bot.say('Can\'t do that, start meeting first')
         return
     if not trigger.group(2):
-        willie.say('Who are the chairs?')
+        bot.say('Who are the chairs?')
         return
     if trigger.nick.lower() == meetings_dict[trigger.sender]['head']:
         meetings_dict[trigger.sender]['chairs'] = trigger.group(2).lower().split(' ')
         chairs_readable = trigger.group(2).lower().replace(' ', ', ')
         logplain('Meeting chairs are: ' + chairs_readable, trigger.sender)
-        logHTML_listitem('<span style="font-weight: bold">Meeting chairs are: </span>'+chairs_readable, trigger.sender)
-        willie.say('Meeting chairs are: ' + chairs_readable)
+        logHTML_listitem('<span style="font-weight: bold">Meeting chairs are: </span>' + chairs_readable, trigger.sender)
+        bot.say('Meeting chairs are: ' + chairs_readable)
     else:
-        willie.say("Only meeting head can set chairs")
+        bot.say("Only meeting head can set chairs")
 
-chairs.commands = ['chairs']
-chairs.example = '.chairs Tyrope Jason elad'
 
 #Log action item in the HTML log
-def meetingaction(willie, trigger):
+@commands('action')
+@example('.action elad will develop a meetbot')
+def meetingaction(bot, trigger):
     """
     Log an action in the meeting log
     https://github.com/embolalia/willie/wiki/Using-the-meetbot-module
     """
     if not ismeetingrunning(trigger.sender):
-        willie.say('Can\'t do that, start meeting first')
+        bot.say('Can\'t do that, start meeting first')
         return
     if not trigger.group(2):
-        willie.say('try .action someone will do something')
+        bot.say('try .action someone will do something')
         return
     if not ischair(trigger.nick, trigger.sender):
-        willie.say('Only meeting head or chairs can do that')
+        bot.say('Only meeting head or chairs can do that')
         return
     logplain('ACTION: ' + trigger.group(2), trigger.sender)
-    logHTML_listitem('<span style="font-weight: bold">Action: </span>'+trigger.group(2), trigger.sender)
-    willie.say('ACTION: ' + trigger.group(2))
-    
-meetingaction.commands = ['action']
-meetingaction.example = '.action elad will develop a meetbot'
+    logHTML_listitem('<span style="font-weight: bold">Action: </span>' + trigger.group(2), trigger.sender)
+    bot.say('ACTION: ' + trigger.group(2))
+
 
 #Log agreed item in the HTML log
-def meetingagreed(willie, trigger):
+@commands('agreed')
+@example('.agreed Bowties are cool')
+def meetingagreed(bot, trigger):
     """
     Log an agreement in the meeting log.
     https://github.com/embolalia/willie/wiki/Using-the-meetbot-module
     """
     if not ismeetingrunning(trigger.sender):
-        willie.say('Can\'t do that, start meeting first')
+        bot.say('Can\'t do that, start meeting first')
         return
     if not trigger.group(2):
-        willie.say('try .action someone will do something')
+        bot.say('try .action someone will do something')
         return
     if not ischair(trigger.nick, trigger.sender):
-        willie.say('Only meeting head or chairs can do that')
+        bot.say('Only meeting head or chairs can do that')
         return
     logplain('AGREED: ' + trigger.group(2), trigger.sender)
-    logHTML_listitem('<span style="font-weight: bold">Agreed: </span>'+trigger.group(2), trigger.sender)
-    willie.say('AGREED: ' + trigger.group(2))
-    
-meetingagreed.commands = ['agreed']
-meetingagreed.example = '.agreed bowties are not cool'
+    logHTML_listitem('<span style="font-weight: bold">Agreed: </span>' + trigger.group(2), trigger.sender)
+    bot.say('AGREED: ' + trigger.group(2))
+
 
 #Log link item in the HTML log
-def meetinglink(willie, trigger):
+@commands('link')
+@example('.link http://example.com')
+def meetinglink(bot, trigger):
     """
     Log a link in the meeing log.
     https://github.com/embolalia/willie/wiki/Using-the-meetbot-module
     """
     if not ismeetingrunning(trigger.sender):
-        willie.say('Can\'t do that, start meeting first')
+        bot.say('Can\'t do that, start meeting first')
         return
     if not trigger.group(2):
-        willie.say('try .action someone will do something')
+        bot.say('try .action someone will do something')
         return
     if not ischair(trigger.nick, trigger.sender):
-        willie.say('Only meeting head or chairs can do that')
+        bot.say('Only meeting head or chairs can do that')
         return
     link = trigger.group(2)
     if not link.startswith("http"):
@@ -298,44 +310,38 @@ def meetinglink(willie, trigger):
         title = ''
     logplain('LINK: %s [%s]' % (link, title), trigger.sender)
     logHTML_listitem('<a href="%s">%s</a>' % (link, title), trigger.sender)
-    willie.say('LINK: ' + link)
-
-meetinglink.commands = ['link']
-meetinglink.example = '.link http://example.com'
+    bot.say('LINK: ' + link)
 
 
 #Log informational item in the HTML log
-def meetinginfo(willie, trigger):
+@commands('info')
+@example('.info all board members present')
+def meetinginfo(bot, trigger):
     """
     Log an informational item in the meeting log
     https://github.com/embolalia/willie/wiki/Using-the-meetbot-module
     """
     if not ismeetingrunning(trigger.sender):
-        willie.say('Can\'t do that, start meeting first')
+        bot.say('Can\'t do that, start meeting first')
         return
     if not trigger.group(2):
-        willie.say('try .info some informative thing')
+        bot.say('try .info some informative thing')
         return
     if not ischair(trigger.nick, trigger.sender):
-        willie.say('Only meeting head or chairs can do that')
+        bot.say('Only meeting head or chairs can do that')
         return
     logplain('INFO: ' + trigger.group(2), trigger.sender)
     logHTML_listitem(trigger.group(2), trigger.sender)
-    willie.say('INFO: ' + trigger.group(2))
-meetinginfo.commands = ['info']
-meetinginfo.example = '.info all board members present'
+    bot.say('INFO: ' + trigger.group(2))
+
 
 #called for every single message
 #Will log to plain text only
-def log_meeting(willie, trigger): 
+@rule('(.*)')
+@priority('low')
+def log_meeting(bot, trigger):
     if not ismeetingrunning(trigger.sender):
         return
     if trigger.startswith('.endmeeting') or trigger.startswith('.chairs') or trigger.startswith('.action') or trigger.startswith('.info') or trigger.startswith('.startmeeting') or trigger.startswith('.agreed') or trigger.startswith('.link') or trigger.startswith('.subject'):
         return
-    logplain('<'+trigger.nick+'> '+trigger, trigger.sender)
-
-log_meeting.rule = r'(.*)'
-log_meeting.priority = 'low'
-
-if __name__ == '__main__':
-    print __doc__.strip()
+    logplain('<' + trigger.nick + '> ' + trigger, trigger.sender)
