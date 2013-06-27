8f8ad01d5e5801d8158c8a32e8d5a3f2136e7a72
Ari Koivula <ari@koivu.la>
2013-06-28 02:39:14 +0300

    Add a script for running pytest with the correct sys.path.

---
+++ pytest_run.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+"""This is a script for running pytest from the command line.
+
+This script exists so that the project directory gets added to sys.path, which
+prevents us from accidentally testing the globally installed willie version.
+
+pytest_run.py
+Copyright 2013, Ari Koivula, <ari@koivu.la>
+Licensed under the Eiffel Forum License 2.
+
+http://willie.dfbta.net
+"""
+
+if __name__ == "__main__":
+    import sys
+    import pytest
+    returncode = pytest.main()
+    sys.exit(returncode)
