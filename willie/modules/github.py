af5e9352e72291e8a1092876f0f66678594c6c7d
Edward Powell <powell.518@gmail.com>
2013-07-27 17:39:19 -0400

    [github] Enable attaching a traceback to an issue.
    Close #148

--- willie/modules/github.py
+++ willie/modules/github.py
@@ -11,6 +11,7 @@ from urllib2 import HTTPError
 import json
 from willie import web
 from willie.module import commands
+import os
 
 
 def checkConfig(bot):
@@ -59,6 +60,65 @@ def issue(bot, trigger):
     data = json.loads(raw)
     bot.say('Issue #%s posted. %s' % (data['number'], data['html_url']))
     bot.debug('GitHub', 'Issue #%s created in %s' % (data['number'], trigger.sender), 'warning')
+
+
+@commands('addtrace', 'addtraceback')
+def add_traceback(bot, trigger):
+    """Add a traceback to a GitHub issue.
+
+    This pulls the traceback from the exceptions log file. To use, put .addtrace
+    followed by the issue number to add the comment to, then the signature of
+    the error (the message shown to the channel when the error occured). This
+    command will only work for errors from unhandled exceptions."""
+    # Make sure the API is set up
+    gitAPI = checkConfig(bot)
+    if gitAPI == False:
+        return bot.say('Git module not configured, make sure github.oauth_token and github.repo are defined')
+
+    # Make sure the input is valid
+    args = trigger.group(2).split(None, 1)
+    if len(args) != 2:
+        bot.say(validate)
+        return
+    number, trace = args
+
+    # Make sure the given issue number exists
+    issue_data = web.get('https://api.github.com/repos/%s/issues/%s' % (gitAPI[1], number))
+    issue_data = json.loads(issue_data)
+    if 'message' in issue_data and issue_data['message'] == 'Not Found':
+        return bot.say("That issue doesn't exist.")
+
+    # Find the relevant lines from the log file
+    post = ''
+    logfile = os.path.join(bot.config.logdir, 'exceptions.log')
+    with open(logfile) as log:
+        in_trace = False
+        for data in log:
+            if data == 'Signature: ' + trace + '\n':
+                post = data
+                in_trace = True
+            elif data == '----------------------------------------\n':
+                in_trace = False
+            elif in_trace:
+                post += data
+
+    # Give an error if we didn't find the traceback
+    if not post:
+        return bot.say("I don't remember getting that error. Please post it "
+                       "yourself at https://github.com/%s/issues/%s"
+                       % (gitAPI[1], number))
+
+    # Make the comment
+    try:
+        raw = web.post('https://api.github.com/repos/' + gitAPI[1] + '/issues/'
+                       + number + '/comments?access_token=' + gitAPI[0],
+                       json.dumps({'body': post}))
+    except OSError:#HTTPError:
+        return bot.say('The GitHub API returned an error.')
+
+    data = json.loads(raw)
+    bot.say('Added traceback to issue #%s. %s' % (number, data['html_url']))
+    bot.debug('GitHub', 'Traceback added to #%s in %s.' % (number, trigger.sender), 'warning')
 
 
 @commands('findissue', 'findbug')
