7a0cb284465c558257d8ac92f14bc94d91aa8d9a
Edward Powell <git@embolalia.com>
2013-11-29 13:41:28 -0500

    [willie.py] PEP8ify, #125

--- willie.py
+++ willie.py
@@ -57,28 +57,35 @@ def main(argv=None):
     try:
         parser = optparse.OptionParser('%prog [options]')
         parser.add_option('-c', '--config', metavar='filename',
-            help='use a specific configuration file')
+                          help='use a specific configuration file')
         parser.add_option("-d", '--fork', action="store_true",
-            dest="deamonize", help="Deamonize willie")
+                          dest="deamonize", help="Deamonize willie")
         parser.add_option("-q", '--quit', action="store_true", dest="quit",
-            help="Gracefully quit Willie")
+                          help="Gracefully quit Willie")
         parser.add_option("-k", '--kill', action="store_true", dest="kill",
-            help="Kill Willie")
-        parser.add_option('--exit-on-error', action="store_true", dest="exit_on_error",
-            help="Exit immediately on every error instead of trying to recover")
+                          help="Kill Willie")
+        parser.add_option('--exit-on-error', action="store_true",
+                          dest="exit_on_error", help=(
+                              "Exit immediately on every error instead of "
+                              "trying to recover"))
         parser.add_option("-l", '--list', action="store_true",
-            dest="list_configs", help="List all config files found")
+                          dest="list_configs",
+                          help="List all config files found")
         parser.add_option("-m", '--migrate', action="store_true",
-            dest="migrate_configs",
-            help="Migrate config files to the new format")
+                          dest="migrate_configs",
+                          help="Migrate config files to the new format")
         parser.add_option('--quiet', action="store_true", dest="quiet",
-            help="Supress all output")
+                          help="Supress all output")
         parser.add_option('-w', '--configure-all', action='store_true',
-            dest='wizard', help='Run the configuration wizard.')
+                          dest='wizard', help='Run the configuration wizard.')
         parser.add_option('--configure-modules', action='store_true',
-            dest='mod_wizard', help='Run the configuration wizard, but only for the module configuration options.')
+                          dest='mod_wizard', help=(
+                              'Run the configuration wizard, but only for the '
+                              'module configuration options.'))
         parser.add_option('--configure-database', action='store_true',
-            dest='db_wizard', help='Run the configuration wizard, but only for the database configuration options.')
+                          dest='db_wizard', help=(
+                              'Run the configuration wizard, but only for the '
+                              'database configuration options.'))
         opts, args = parser.parse_args(argv)
 
         if opts.wizard:
