355cbcf93642c14e6780d42b0ea3cc3602f3381c
Edward Powell <git@embolalia.com>
2013-11-29 13:57:06 -0500

    [setup.py] Fix PEP8, #125

--- setup.py
+++ setup.py
@@ -15,19 +15,20 @@ def do_setup():
         tmp_main_script = os.path.join(tmp_dir, 'willie')
         shutil.copy('willie.py', tmp_main_script)
 
-        setup(name='willie',
-              version=__version__,
-              description='Simple and extendible IRC bot',
-              author='Edward Powell',
-              author_email='powell.518@gmail.com',
-              url='http://willie.dftba.net/',
-              long_description="""Willie is a simple, lightweight, open source, easy-to-use IRC Utility bot, written in Python. It's designed to be easy to use, easy to run, and easy to make new features for. """,
-              packages=['willie', 'willie.modules'],
-              scripts=[tmp_main_script],
-              license='Eiffel Forum License, version 2',
-              platforms='Linux x86, x86-64',
-              requires=['feedparser', 'pytz', 'lxml', 'praw', 'enchant', 'pygeoip']
-             )
+        setup(
+            name='willie',
+            version=__version__,
+            description='Simple and extendible IRC bot',
+            author='Edward Powell',
+            author_email='powell.518@gmail.com',
+            url='http://willie.dftba.net/',
+            long_description="""Willie is a simple, lightweight, open source, easy-to-use IRC Utility bot, written in Python. It's designed to be easy to use, easy to run, and easy to make new features for. """,
+            packages=['willie', 'willie.modules'],
+            scripts=[tmp_main_script],
+            license='Eiffel Forum License, version 2',
+            platforms='Linux x86, x86-64',
+            requires=['feedparser', 'pytz', 'lxml', 'praw', 'enchant', 'pygeoip']
+        )
     finally:
         try:
             shutil.rmtree(tmp_dir)
