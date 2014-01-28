a1fd1dc4811dce45f466e8a2a556607a65988471
Edward Powell <git@embolalia.com>
2014-01-27 21:34:47 -0500

    [core] Use coding and unicode_literals consistently
    Also add them to CONTRIBUTING. Both are now official style guidelines of
    the project. Anyone who pushes new code without them will be
    defenestrated.

--- willie/module.py
+++ willie/module.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#coding: utf8
 """This module is meant to be imported from willie modules.
 
 It defines the following decorators for defining willie callables:
@@ -19,6 +19,7 @@ Copyright 2013, Lior Ramati <firerogue517@gmail.com>
 
 Licensed under the Eiffel Forum License 2.
 """
+from __future__ import unicode_literals
 
 import willie.test_tools
 import functools
--- willie/tools.py
+++ willie/tools.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+#coding: utf8
 """
 *Availability: 3+*
 ``tools`` contains a number of useful miscellaneous tools and shortcuts for use
