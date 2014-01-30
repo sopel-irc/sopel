sopel-irc/sopel

commit a1fd1dc4811dce45f466e8a2a556607a65988471

[core] Use coding and unicode_literals consistently
Also add them to CONTRIBUTING. Both are now official style guidelines of
the project. Anyone who pushes new code without them will be
defenestrated.

willie/config.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+#coding: utf8
 """
 *Availability: 3+ for all functions; attributes may vary.*
 
@@ -40,6 +40,7 @@ Licensed under the Eiffel Forum License 2.
 
 http://willie.dftba.net
 """
+from __future__ import unicode_literals
 
 import db
 import os

willie/db.py
@@ -1,3 +1,4 @@
+#coding: utf8
 """
 *Availability: 3.1+*
 
@@ -14,6 +15,8 @@ Licensed under the Eiffel Forum License 2.
 
 http://willie.dftba.net
 """
+from __future__ import unicode_literals
+
 import os
 from collections import Iterable
 from tools import deprecated

willie/web.py
@@ -1,4 +1,4 @@
-# -*- coding: utf8 -*-
+#coding: utf8
 """
 *Availability: 3+*
 
@@ -20,6 +20,7 @@ More info:
 http://willie.dftba.net
 
 """
+from __future__ import unicode_literals
 
 import re
 import urllib

willie/test_tools.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+#coding: utf8
 """This module has classes and functions that can help in writing tests.
 
 test_tools.py - Willie misc tools
@@ -7,6 +7,8 @@ Licensed under the Eiffel Forum License 2.
 
 https://willie.dftba.net
 """
+from __future__ import unicode_literals
+
 import sys
 import re
 

CONTRIBUTING
@@ -43,6 +43,10 @@ include your changes.
   the name of the thing you're changing in [brackets] at the beginning of the
   message: the module name for modules, [docs] for documentation files,
   [coretasks] for coretasks.py, [db] for the database feature, and so on.
+* Python files should always have `#coding: utf8` as the first line (or the
+  second, if the first is `#!/usr/bin/env python`), and
+  `from __future__ import unicode_literals` as the first line after the module
+  docstring.
 
 Issue Tags
 ----------

pytest_run.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# coding=utf-8
+#coding: utf8
 """This is a script for running pytest from the command line.
 
 This script exists so that the project directory gets added to sys.path, which
@@ -11,6 +11,7 @@ Licensed under the Eiffel Forum License 2.
 
 http://willie.dfbta.net
 """
+from __future__ import unicode_literals
 
 if __name__ == "__main__":
     import sys

setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf8 -*-
+#coding: utf8
 from __future__ import unicode_literals
 
 from distutils.core import setup

willie.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2.7
-# -*- coding: utf8 -*-
+#coding: utf8
 """
 Willie - An IRC Bot
 Copyright 2008, Sean B. Palmer, inamidst.com

commit 8866f4aa22a962d19d5baa931fcf7ed7570c3497

[core] Fix issues caused by a1fd1dc
Closes #438, closes #436.

willie/config.py
@@ -31,15 +31,10 @@ The configuration function, if used, must be declared with the signature
 ``configure(config)``. To add options, use ``interactive_add``, ``add_list``
 and ``add_option``.
 """
-"""
-Config - A config class and writing/updating utility for Willie
-Copyright 2012, Edward Powell, embolalia.net
-Copyright © 2012, Elad Alfassa <elad@fedoraproject.org>
+#Copyright 2012, Edward Powell, embolalia.net
+#Copyright © 2012, Elad Alfassa <elad@fedoraproject.org>
+#Licensed under the Eiffel Forum License 2.
 
-Licensed under the Eiffel Forum License 2.
-
-http://willie.dftba.net
-"""
 from __future__ import unicode_literals
 
 import db

willie/db.py
@@ -9,12 +9,9 @@ This class defines an interface for a semi-arbitrary database type. It is meant
 to allow module writers to operate without regard to how the end user has
 decided to set up the database.
 """
-"""
-Copyright 2012, Edward D. Powell, embolalia.net
-Licensed under the Eiffel Forum License 2.
+#Copyright 2012, Edward D. Powell, embolalia.net
+#Licensed under the Eiffel Forum License 2.
 
-http://willie.dftba.net
-"""
 from __future__ import unicode_literals
 
 import os
@@ -314,7 +311,7 @@ class Table(object):
 
     def __init__(self, db, name, columns, key):
         #This lets us have a pseudo-table to handle a non-existant table
-        if name is '_none':
+        if name == '_none':
             self.db = db
             self.columns = set()
             self.name = name

willie/web.py
@@ -5,21 +5,13 @@
 The web class contains essential web-related functions for interaction with web
 applications or websites in your modules.  It supports HTTP GET, HTTP POST and
 HTTP HEAD.
-
 """
+#Copyright © 2008, Sean B. Palmer, inamidst.com
+#Copyright © 2009, Michael Yanovich <yanovich.1@osu.edu>
+#Copyright © 2012, Dimitri Molenaars, Tyrope.nl.
+#Copyright © 2012-2013, Elad Alfassa, <elad@fedoraproject.org>
+#Licensed under the Eiffel Forum License 2.
 
-"""
-web.py - Web Facilities
-Copyright © 2008, Sean B. Palmer, inamidst.com
-Copyright © 2009, Michael Yanovich <yanovich.1@osu.edu>
-Copyright © 2012, Dimitri Molenaars, Tyrope.nl.
-Copyright © 2012-2013, Elad Alfassa, <elad@fedoraproject.org>
-Licensed under the Eiffel Forum License 2.
-
-More info:
-http://willie.dftba.net
-
-"""
 from __future__ import unicode_literals
 
 import re

willie/test_tools.py
@@ -124,7 +124,7 @@ def insert_into_module(func, module_name, base_name, prefix):
     module = sys.modules[module_name]
     # Make sure the func method does not overwrite anything.
     for i in xrange(1000):
-        func.__name__ = "%s_%s_%s" % (prefix, base_name, i)
+        func.__name__ = str("%s_%s_%s" % (prefix, base_name, i))
         if not hasattr(module, func.__name__):
             break
     setattr(module, func.__name__, func)