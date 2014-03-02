sopel-irc/sopel

commit 222b1f43c6d17f78b10da287d464d55ecbc5e24d

[core] Better unicode handling on message sending
This commits makes sure that even if a module sent us bytes, or a str
that came out of .encode(), we will always send it properly and never fail
on various AttributeError exceptions (bytes don't have .encode()) or
UnicodeDecoteError exceptions.

willie/irc.py
@@ -155,10 +155,13 @@ class Bot(asynchat.async_chat):
 
     def safe(self, string):
         """Remove newlines from a string."""
+        if sys.version_info.major >=3 and isinstance(string, bytes):
+                string = string.decode('utf8')
+        elif sys.version_info.major < 3:
+            if not isinstance(string, unicode):
+                string = unicode(string, encoding='utf8')
         string = string.replace('\n', '')
         string = string.replace('\r', '')
-        if not isinstance(string, unicode):
-            string = unicode(string, encoding='utf8')
         return string
 
     def write(self, args, text=None):
@@ -441,7 +444,8 @@ class Bot(asynchat.async_chat):
         # messages will be split. Otherwise, we'd have to acocunt for the bot's
         # hostmask, which is hard.
         max_text_length = 400
-        encoded_text = text.encode('utf-8')
+        if isinstance(text, unicode):
+            encoded_text = text.encode('utf-8')
         excess = ''
         if max_messages > 1 and len(encoded_text) > max_text_length:
             last_space = encoded_text.rfind(' ', 0, max_text_length)
@@ -452,7 +456,8 @@ class Bot(asynchat.async_chat):
                 excess = encoded_text[last_space + 1:]
                 encoded_text = encoded_text[:last_space]
             # Back to unicode again, so we don't screw things up later.
-            text = encoded_text.decode('utf-8')
+            if sys.version_info.major < 3:
+                text = encoded_text.decode('utf-8')
         # We'll then send the excess at the end
         try:
             self.sending.acquire()

commit 173afa62388d3fe60cbcab9e643ad224bc7ddfd2

[core]: Fix bug introduced in commit 222b1f4
* If text was already encoded in utf-8, the variable encoded_text was
never set and was needed further down in the execution path.

willie/irc.py
@@ -444,6 +444,7 @@ class Bot(asynchat.async_chat):
         # messages will be split. Otherwise, we'd have to acocunt for the bot's
         # hostmask, which is hard.
         max_text_length = 400
+        encoded_text = text
         if isinstance(text, unicode):
             encoded_text = text.encode('utf-8')
         excess = ''