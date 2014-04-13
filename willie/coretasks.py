f5bbe20962f9ce5a44cd2e4c9cde26c42080d8bd
Daniel Givens <daniel.givens@rackspace.com>
2014-04-13 12:37:38 -0500

    Added option for SASL username for cases when the nick isn't the same as the SASL username

--- willie/coretasks.py
+++ willie/coretasks.py
@@ -403,7 +403,12 @@ def auth_proceed(bot, trigger):
         # How did we get here? I am not good with computer.
         return
     # Is this right?
-    sasl_token = '\0'.join((bot.nick, bot.nick, bot.config.core.sasl_password))
+    if bot.config.core.sasl_username:
+        sasl_username = bot.config.core.sasl_username
+    else:
+        sasl_username = bot.nick
+    sasl_token = '\0'.join((sasl_username, sasl_username,
+                           bot.config.core.sasl_password))
     # Spec says we do a base 64 encode on the SASL stuff
     bot.write(('AUTHENTICATE', base64.b64encode(sasl_token)))
 
