65bf636d16b7ea903fd2467ce087ecb52e6bff28
warddr <git@warddr.eu>
2014-04-28 20:02:44 +0200

    fix conftest.py to ignore the ipython module

--- conftest.py
+++ conftest.py
@@ -1,2 +1,2 @@
 # This file lists files which should be ignored by pytest
-collect_ignore = ["setup.py", "willie.py", "ipython.py"]
+collect_ignore = ["setup.py", "willie.py", "willie/modules/ipython.py"]
