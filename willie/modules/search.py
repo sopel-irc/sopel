2cd939ac2db202bbfc6b72fa0ad380808aeab8ae
Elad Alfassa <elad@fedoraproject.org>
2014-05-02 22:43:45 +0300

    [search] Fix google search in Python 3
    I wonder when it broke

--- willie/modules/search.py
+++ willie/modules/search.py
@@ -13,6 +13,7 @@ import re
 from willie import web
 from willie.module import commands, example
 import json
+import sys
 import time
 
 
@@ -21,6 +22,8 @@ def google_ajax(query):
     uri = 'http://ajax.googleapis.com/ajax/services/search/web'
     args = '?v=1.0&safe=off&q=' + query
     bytes = web.get(uri + args)
+    if sys.version_info.major >= 3:
+        bytes = bytes.decode()
     return json.loads(bytes)
 
 
