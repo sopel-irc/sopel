sopel-irc/sopel

commit 6a0d18ef6cd54662498cda95bea0467aed4efa87

[Many modules] Add unicode_literals import
Also add #coding: utf8 line

This is probably going to force a lot of errors up, but they were
already hiding there anyway.

willie/modules/unicode_info.py
@@ -1,3 +1,4 @@
+#coding: utf8
 """
 codepoints.py - Willie Codepoints Module
 Copyright 2013, Edward Powell, embolalia.net
@@ -6,6 +7,7 @@ Licensed under the Eiffel Forum License 2.
 
 http://willie.dfbta.net
 """
+from __future__ import unicode_literals
 import unicodedata
 from willie.module import commands, example, NOLIMIT
 

willie/modules/__init__.py
@@ -1,0 +1,2 @@
+#coding: utf8
+from __future__ import unicode_literals

willie/modules/admin.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+#coding: utf8
 """
 admin.py - Willie Admin Module
 Copyright 2010-2011, Sean B. Palmer (inamidst.com) and Michael Yanovich
@@ -10,6 +10,7 @@ Licensed under the Eiffel Forum License 2.
 
 http://willie.dftba.net
 """
+from __future__ import unicode_literals
 
 import willie.module
 

willie/modules/adminchannel.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+#coding: utf8
 """
 admin.py - Willie Admin Module
 Copyright 2010-2011, Michael Yanovich, Alek Rollyson, and Edward Powell
@@ -8,6 +8,7 @@ Licensed under the Eiffel Forum License 2.
 http://willie.dftba.net/
 
 """
+from __future__ import unicode_literals
 
 import re
 from willie.module import commands, priority, OP

willie/modules/announce.py
@@ -1,10 +1,12 @@
-# -*- coding: utf8 -*-
+#coding: utf8
 """
 announce.py - Send a message to all channels
 Copyright © 2013, Elad Alfassa, <elad@fedoraproject.org>
 Licensed under the Eiffel Forum License 2.
 
 """
+from __future__ import unicode_literals
+
 from willie.module import commands, example
 
 

willie/modules/bugzilla.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+#coding: utf8
 """
 admin.py - Willie Bugzilla Module
 Copyright © 2013, Edward Powell, embolalia.net
@@ -6,6 +6,8 @@ Licensed under the Eiffel Forum License 2.
 
 http://willie.dftba.net/
 """
+from __future__ import unicode_literals
+
 from lxml import etree
 import re
 from willie import web, tools

willie/modules/calc.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+#coding: utf8
 """
 calc.py - Willie Calculator Module
 Copyright 2008, Sean B. Palmer, inamidst.com

willie/modules/chanlogs.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+#coding: utf8
 """
 chanlogs.py - Willie Channel Logger module
 Copyright 2014, David Baumgold <david@davidbaumgold.com>

commit 0a2467f5262998826c21b13aa6a83934001ca170

[unicode_info] Fix unicode error introduced in 6a0d18ef
This fix only affects Python 2.7, as unicode handling is much better in 3.x

willie/modules/unicode_info.py
@@ -29,7 +29,7 @@ def codepoint(bot, trigger):
     # Get the hex value for the code point, and drop the 0x from the front
     point = str(hex(ord(u'' + arg)))[2:]
     # Make the hex 4 characters long with preceding 0s, and all upper case
-    point = point.rjust(4, '0').upper()
+    point = point.rjust(4, str('0')).upper()
     try:
         name = unicodedata.name(arg)
     except ValueError: