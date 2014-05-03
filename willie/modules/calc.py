sopel-irc/sopel

commit c33f8f1f90d8ead489491edf74fb10fd71692009

[web] Decode bytestring to unicode in get()
I've noticed this began to become a problem for many modules,
so instead of decoding it in the modules, it made much more sense
to decode it in web.get()

Fixes #532

willie/web.py
@@ -59,7 +59,7 @@ def get(uri, timeout=20, headers=None, return_headers=False,
     bytes = u.read(limit_bytes)
     u.close()
     if not return_headers:
-        return bytes
+        return bytes.decode('utf-8')
     else:
         return (bytes, u.info())
 

willie/modules/search.py
@@ -22,8 +22,6 @@ def google_ajax(query):
     uri = 'http://ajax.googleapis.com/ajax/services/search/web'
     args = '?v=1.0&safe=off&q=' + query
     bytes = web.get(uri + args)
-    if sys.version_info.major >= 3:
-        bytes = bytes.decode()
     return json.loads(bytes)
 
 

willie/modules/translate.py
@@ -59,10 +59,6 @@ def translate(text, in_lang='auto', out_lang='en'):
     )
     url = "http://translate.google.com/translate_a/t?{query}".format(query=query_string)
     result = web.get(url, timeout=40, headers=headers)
-    if sys.version_info.major >= 3:
-        result = result.decode()
-    elif isinstance(result, str):
-        result = result.decode('utf-8')
 
     while ',,' in result:
         result = result.replace(',,', ',null,')

commit 0de1f20d722eb5b6d2d13f182c70fe08d92d0a5d

[web] Add a dont_decode argument to get() for modules who wants bytes
This fixes a regression in calc.py Wolfram Alpha feature caused by
c33f8f1f90

I still think c33f8f1f90 was a good thing to do

willie/modules/calc.py
@@ -65,7 +65,8 @@ def wa(bot, trigger):
     query = trigger.group(2)
     uri = 'http://tumbolia.appspot.com/wa/'
     try:
-        answer = web.get(uri + web.quote(query).replace('+', '%2B'), 45)
+        answer = web.get(uri + web.quote(query).replace('+', '%2B'), 45,
+                         dont_decode=True)
     except timeout as e:
         return bot.say('[WOLFRAM ERROR] Request timed out')
     if answer:

willie/web.py
@@ -39,7 +39,7 @@ if not hasattr(ssl, 'match_hostname'):
 
 # HTTP GET
 def get(uri, timeout=20, headers=None, return_headers=False,
-        limit_bytes=None, verify_ssl=True):
+        limit_bytes=None, verify_ssl=True, dont_decode=False):
     """Execute an HTTP GET query on `uri`, and return the result.
 
     `timeout` is an optional argument, which represents how much time we should
@@ -58,8 +58,10 @@ def get(uri, timeout=20, headers=None, return_headers=False,
     u = get_urllib_object(uri, timeout, headers, verify_ssl)
     bytes = u.read(limit_bytes)
     u.close()
+    if not dont_decode:
+        bytes = bytes.decode('utf-8')
     if not return_headers:
-        return bytes.decode('utf-8')
+        return bytes
     else:
         return (bytes, u.info())
 