sopel-irc/sopel

commit c09449f2b756d68289f32a63b3b8b6dd5a99810b

[core] Enable IPv6
This is the same as dee3c95 (#217), but without removing
`self.connect((host, port))`. It seems strange that we all would have
missed it, but so far re-doing this commit seems to be working in all
the ways it's supposed to.

willie/irc.py
@@ -192,13 +192,18 @@ class Bot(asynchat.async_chat):
             self.writing_lock.release()
 
     def run(self, host, port=6667):
-        self.initiate_connect(host, port)
+        try:
+            self.initiate_connect(host, port)
+        except socket.error, e:
+            stderr('Connection error: %s' % e.strerror)
+            self.hasquit = True
 
     def initiate_connect(self, host, port):
         stderr('Connecting to %s:%s...' % (host, port))
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        if self.config.core.bind_host is not None:
-            self.socket.bind((self.config.core.bind_host, 0))
+        source_address = ((self.config.core.bind_host, 0)
+                          if self.config.core.bind_address else None)
+        self.set_socket(socket.create_connection((host, port),
+            source_address=source_address))
         if self.config.core.use_ssl and has_ssl:
             self.send = self._ssl_send
             self.recv = self._ssl_recv

commit 331d7ab88183014ba5a09f139126ba2a2cf98559

[core] Fix regression that caused bind_host to no longer work
Fixes #541
Regression introduced in c09449f2 due to a typo.

willie/irc.py
@@ -224,7 +224,7 @@ class Bot(asynchat.async_chat):
     def initiate_connect(self, host, port):
         stderr('Connecting to %s:%s...' % (host, port))
         source_address = ((self.config.core.bind_host, 0)
-                          if self.config.core.bind_address else None)
+                          if self.config.core.bind_host else None)
         self.set_socket(socket.create_connection((host, port),
                         source_address=source_address))
         if self.config.core.use_ssl and has_ssl: