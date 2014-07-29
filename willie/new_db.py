cdce5dc612b4ddc07b7e88687a3321b97dfec8a4
Edward Powell <git@embolalia.com>
2014-07-28 21:44:24 -0400

    [db] Add provisional docs to new DB functions

--- willie/new_db.py
+++ willie/new_db.py
@@ -7,14 +7,20 @@ class WillieDB(object):
         self.filename = filename
 
     def connect(self):
+        """Return a raw database connection object."""
         return sqlite3.connect(self.filename)
 
     def execute(self, *args, **kwargs):
+        """Execute an arbitrary SQL query against the database.
+
+        Returns a cursor object, on which things like `.fetchall()` can be
+        called per PEP 249."""
         with self.connect() as conn:
             cur = conn.cursor()
             return cur.execute(*args, **kwargs)
 
     def create(self):
+        """Create the basic database structure."""
         self.execute(
             'CREATE TABLE nicknames '
             '(nick_id INTEGER, slug STRING PRIMARY KEY, canonical string)'
@@ -27,6 +33,10 @@ class WillieDB(object):
         self.execute('INSERT INTO nicknames VALUES (?, ?, ?)', [0, '', ''])
 
     def _get_nick_id(self, nick):
+        """Return the internal identifier for a given nick.
+
+        This identifier is unique to a user, and shared across all of that
+        user's aliases."""
         slug = nick.lower()
         nick_id = self.execute('SELECT nick_id from nicknames where slug = ?',
                                [slug]).fetchone()
@@ -40,6 +50,10 @@ class WillieDB(object):
         return nick_id[0]
 
     def alias_nick(self, nick, alias):
+        """Create an alias for a nick.
+
+        Raises ValueError if the alias already exists. If nick does not already
+        exist, it will be added along with the alias."""
         nick = Nick(nick)
         alias = Nick(alias)
         nick_id = self._get_nick_id(nick)
@@ -51,6 +65,7 @@ class WillieDB(object):
             raise ValueError('Alias already exists.')
 
     def set_nick_value(self, nick, key, value):
+        """Sets the value for a given key to be associated with the nick."""
         nick = Nick(nick)
         nick_id = self._get_nick_id(nick)
         # Insert if it's not already set, otherwise update
@@ -63,6 +78,7 @@ class WillieDB(object):
                          [value, nick_id, key])
 
     def get_nick_value(self, nick, key):
+        """Retrieves the value for a given key associated with a nick."""
         nick = Nick(nick)
         result = self.execute(
             'SELECT value FROM nicknames, nick_values WHERE slug = ? AND key = ?',
