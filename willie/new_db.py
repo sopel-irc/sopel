98261391fd01731083664aca4900ffa786b6f718
Edward Powell <git@embolalia.com>
2014-09-07 15:31:08 -0400

    [db] Fill out remaining new DB methods

--- willie/new_db.py
+++ willie/new_db.py
@@ -27,20 +27,32 @@ class WillieDB(object):
         )
         self.execute(
             'CREATE TABLE nick_values '
-            '(nick_id INTEGER, key STRING, value STRING,'
+            '(nick_id INTEGER REFERENCES nicknames(nick_id) '
+            '    ON UPDATE CASCADE ON DELETE CASCADE,'
+            'key STRING, value STRING,'
             'PRIMARY KEY (nick_id, key))'
         )
         self.execute('INSERT INTO nicknames VALUES (?, ?, ?)', [0, '', ''])
+        self.execute(
+            'CREATE TABLE channel_values '
+            '(channel STRING, key STRING, value STRING) '
+            'PRIMARY KEY (channel, key)'
+        )
 
-    def _get_nick_id(self, nick):
+    # NICK FUNCTIONS
+
+    def _get_nick_id(self, nick, create=True):
         """Return the internal identifier for a given nick.
 
         This identifier is unique to a user, and shared across all of that
-        user's aliases."""
+        user's aliases. If create is True, a new ID will be created if one does
+        not already exist"""
         slug = nick.lower()
         nick_id = self.execute('SELECT nick_id from nicknames where slug = ?',
                                [slug]).fetchone()
         if nick_id is None:
+            if not create:
+                raise ValueError('No ID exists for the given nick')
             self.execute(
                 'INSERT INTO nicknames (nick_id, slug, canonical) VALUES '
                 '((SELECT max(nick_id) + 1 from nicknames), ?, ?)',
@@ -68,14 +80,8 @@ class WillieDB(object):
         """Sets the value for a given key to be associated with the nick."""
         nick = Nick(nick)
         nick_id = self._get_nick_id(nick)
-        # Insert if it's not already set, otherwise update
-        if not self.get_nick_value(nick, key):
-            self.execute('INSERT INTO nick_values VALUES (?, ?, ?)',
-                         [nick_id, key, value])
-        else:
-            self.execute('UPDATE nick_values SET value = ? '
-                         'WHERE nick_id = ? AND key = ?',
-                         [value, nick_id, key])
+        self.execute('INSERT OR REPLACE INTO nick_values VALUES (?, ?, ?)',
+                     [nick_id, key, value])
 
     def get_nick_value(self, nick, key):
         """Retrieves the value for a given key associated with a nick."""
@@ -88,22 +94,91 @@ class WillieDB(object):
             result = result[0]
         return result
 
-    def unalias_nick(self, nick, alias):
+    def get_all_nick_values(self, nick):
+        """Returns a dict of all values associated with a nick."""
+        nick = Nick(nick)
+        results = self.execute(
+            'SELECT key, value from nicknames, nick_values WHERE slug = ?',
+            [nick.lower()]).fetchall()
+        results = {key: value for key, value in results}
+        return results
+
+    def unalias_nick(self, alias):
         """Removes an alias.
 
         Raises ValueError if there is not at least one other nick in the group.
+        To delete an entire group, use `delete_group`.
         """
-        pass  # TODO
+        alias = Nick(alias)
+        nick_id = self._get_nick_id(alias, False)
+        count = self.execute('SELECT COUNT(*) FROM nicknames WHERE nick_id = ?',
+                             [nick_id]).fetchone()[0]
+        if count == 0:
+            raise ValueError('Given alias is the only entry in its group.')
+        self.execute('DELETE FROM nicknames WHERE slug = ?', [alias.lower()])
 
-    def merge_nick_groups(self, nicks):
+    def delete_nick_group(self, nick):
+        """Removes a nickname, and all associated aliases and settings.
+        """
+        nick = Nick(nick)
+        nick_id = self._get_nick_id(nick, False)
+        self.execute('DELETE FROM nicknames WHERE id = ?', [nick_id])
+
+    def merge_nick_groups(self, first_nick, second_nick):
         """Merges the nick groups for the specified nicks.
 
-        Takes an iterable of nicks, which may or may not be registered.
-        Unregistered nicks will be registered. Keys which are set for only one
-        of the given nicks will be preserved. Where multiple nicks have values
-        for a given key, the value set for an arbitrary nick will be chosen
-        (#TODO unless deterministic is just as easy to implement)."""
-        pass  # TODO
+        Takes two nicks, which may or may not be registered.  Unregistered
+        nicks will be registered. Keys which are set for only one of the given
+        nicks will be preserved. Where multiple nicks have values for a given
+        key, the value set for the first nick will be used.
 
-# TODO settings for channels, and a method to provide a Trigger and get the
-# nick setting if set, else the channel setting.
+        Note that merging of data only applies to the native key-value store.
+        If modules define their own tables which rely on the nick table, they
+        will need to have their merging done separately."""
+        first_id = self._get_nick_id(Nick(first_nick))
+        second_id = self._get_nick_id(Nick(second_nick))
+        data = self.execute(
+            'UPDATE OR IGNORE nick_values SET nick_id = ? WHERE nick_id = ?',
+            [first_id, second_id])
+        self.execute('DELETE FROM nick_values WHERE nick_id = ?', [second_id])
+        self.execute('UPDATE nick_values SET nick_id = ? WHERE nick_id = ?',
+                     [first_id, second_id])
+
+    # CHANNEL FUNCTIONS
+
+    def set_channel_value(self, channel, key, value):
+        channel = Nick(channel).lower()
+        self.execute('INSERT OR REPLACE INTO channel_values VALUES (?, ?, ?)',
+                     [channel, key, value])
+
+    def get_channel_value(self, channel, key):
+        """Retrieves the value for a given key associated with a channel."""
+        channel = Nick(channel).lower()
+        result = self.execute(
+            'SELECT value FROM channel WHERE channel = ? AND key = ?',
+            [channel, key]
+        ).fetchone()
+        if result is not None:
+            result = result[0]
+        return result
+
+    # NICK AND CHANNEL FUNCTIONS
+
+    def get_nick_or_channel_value(self, name, key):
+        """Gets the value `key` associated to the nick or channel  `name`.
+        """
+        name = Nick(name)
+        if name.is_nick():
+            return self.get_nick_value(trigger.nick, key)
+        else:
+            return self.get_channel_value(trigger.sender, key)
+
+    def get_preferred_value(self, names, key):
+        """Gets the value for the first name which has it set.
+
+        `names` is a list of channel and/or user names. Returns None if none of
+        the names have the key set."""
+        for name in names:
+            value = self.get_nick_or_channel_value(self, name, key)
+            if value:
+                return value
