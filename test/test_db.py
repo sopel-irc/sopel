74bbc5865778878956cd7bf57e43a7fb1956abed
Edward Powell <git@embolalia.com>
2014-09-07 17:44:08 -0400

    [db, tests] Add tests and fix bugs in new DB

--- test/test_db.py
+++ test/test_db.py
@@ -6,6 +6,7 @@ enough to get everything working (and is better than nothing), but best
 practice would probably be not to do that."""
 from __future__ import unicode_literals
 
+import json
 import os
 import sqlite3
 
@@ -18,7 +19,7 @@ db_filename = 'asdf'
 
 
 @pytest.fixture
-def db(monkeypatch):
+def db():
     db = WillieDB(db_filename)
     # TODO add tests to ensure this is working properly, too.
     db.create()
@@ -78,10 +79,13 @@ def test_alias_nick(db):
     for alias in aliases:
         assert db._get_nick_id(alias) == nick_id
 
+    db.alias_nick('both', 'arenew')  # Shouldn't fail.
+
     with pytest.raises(ValueError):
         db.alias_nick('Eve', nick)
-    # TODO what should happen with alias_nick(nick, nick), or other already-
-    # existing aliases
+
+    with pytest.raises(ValueError):
+        db.alias_nick(nick, nick)
 
 
 def test_set_nick_value(db):
@@ -105,7 +109,7 @@ def test_set_nick_value(db):
                 'SELECT value FROM nick_values WHERE nick_id = ? AND key = ?',
                 [nick_id, key]
             ).fetchone()[0]
-            assert found_value == value
+            assert json.loads(unicode(found_value)) == value
     check()
 
     # Test updates
@@ -128,10 +132,115 @@ def test_get_nick_value(db):
 
     for key, value in data.iteritems():
         cursor.execute('INSERT INTO nick_values VALUES (?, ?, ?)',
-                     [nick_id, key, value])
-        conn.commit()
+                       [nick_id, key, json.dumps(value, ensure_ascii=False)])
+    conn.commit()
 
     for key, value in data.iteritems():
-        print key, value
         found_value = db.get_nick_value(nick, key)
         assert found_value == value
+
+
+def test_unalias_nick(db):
+    conn = sqlite3.connect(db_filename)
+    nick = 'Embolalia'
+    nick_id = 42
+    conn.execute('INSERT INTO nicknames VALUES (?, ?, ?)',
+                 [nick_id, Nick(nick).lower(), nick])
+    aliases = ['EmbölaliÅ', 'Embo`work', 'Embo']
+    for alias in aliases:
+        conn.execute('INSERT INTO nicknames VALUES (?, ?, ?)',
+                     [nick_id, Nick(alias).lower(), alias])
+    conn.commit()
+
+    for alias in aliases:
+        db.unalias_nick(alias)
+
+    for alias in aliases:
+        found = conn.execute(
+            'SELECT * FROM nicknames WHERE nick_id = ?',
+            [nick_id]).fetchall()
+        assert len(found) == 1
+
+
+def test_delete_nick_group(db):
+    conn = sqlite3.connect(db_filename)
+    aliases = ['Embolalia', 'Embo']
+    nick_id = 42
+    for alias in aliases:
+        conn.execute('INSERT INTO nicknames VALUES (?, ?, ?)',
+                     [nick_id, Nick(alias).lower(), alias])
+    conn.commit()
+
+    db.set_nick_value(aliases[0], 'foo', 'bar')
+    db.set_nick_value(aliases[1], 'spam', 'eggs')
+
+    db.delete_nick_group(aliases[0])
+
+    for alias in aliases:
+        # Nothing else has created values, so we know the tables are empty
+        nicks = conn.execute('SELECT * FROM nicknames').fetchall()
+        # 1 because of the dummy record
+        assert len(nicks) == 1
+        data = conn.execute('SELECT * FROM nick_values').fetchone()
+        assert data is None
+
+
+def test_merge_nick_groups(db):
+    conn = sqlite3.connect(db_filename)
+    aliases = ['Embolalia', 'Embo']
+    for nick_id, alias in enumerate(aliases):
+        conn.execute('INSERT INTO nicknames VALUES (?, ?, ?)',
+                     [nick_id, Nick(alias).lower(), alias])
+    conn.commit()
+
+    finals = (('foo', 'bar'), ('bar', 'blue'), ('spam', 'eggs'))
+
+    db.set_nick_value(aliases[0], finals[0][0], finals[0][1])
+    db.set_nick_value(aliases[0], finals[1][0], finals[1][1])
+    db.set_nick_value(aliases[1], 'foo', 'baz')
+    db.set_nick_value(aliases[1], finals[2][0], finals[2][1])
+
+    db.merge_nick_groups(aliases[0], aliases[1])
+
+    nick_id = conn.execute('SELECT nick_id FROM nicknames').fetchone()[0]
+    alias_id = conn.execute('SELECT nick_id FROM nicknames').fetchone()[0]
+    assert nick_id == alias_id
+
+    for key, value in finals:
+        found = conn.execute(
+            'SELECT value FROM nick_values WHERE nick_id = ? AND key = ?',
+            [nick_id, key]).fetchone()[0]
+        assert json.loads(unicode(found)) == value
+
+
+def test_set_channel_value(db):
+    conn = sqlite3.connect(db_filename)
+    db.set_channel_value('#asdf', 'qwer', 'zxcv')
+    result = conn.execute(
+        'SELECT value FROM channel_values WHERE channel = ? and key = ?',
+        ['#asdf', 'qwer']).fetchone()[0]
+    assert result == '"zxcv"'
+
+
+def test_get_channel_value(db):
+    conn = sqlite3.connect(db_filename)
+    conn.execute("INSERT INTO channel_values VALUES ('#asdf', 'qwer', '\"zxcv\"')")
+    conn.commit()
+    result = db.get_channel_value('#asdf', 'qwer')
+    assert result == 'zxcv'
+
+
+def test_get_nick_or_channel_value(db):
+    db.set_nick_value('asdf', 'qwer', 'poiu')
+    db.set_channel_value('#asdf', 'qwer', '/.,m')
+    assert db.get_nick_or_channel_value('asdf', 'qwer', 'poiu')
+    assert db.get_nick_or_channel_value('#asdf', 'qwer', '/.,m')
+
+
+def test_get_nick_or_channel_value(db):
+    db.set_nick_value('asdf', 'qwer', 'poiu')
+    db.set_channel_value('#asdf', 'qwer', '/.,m')
+    db.set_channel_value('#asdf', 'lkjh', '1234')
+    names = ['asdf', '#asdf']
+    assert db.get_preferred_value(names, 'qwer') == 'poiu'
+    assert db.get_preferred_value(names, 'lkjh') == '1234'
--- willie/new_db.py
+++ willie/new_db.py
@@ -1,5 +1,20 @@
+import json
 import sqlite3
 from willie.tools import Nick
+
+def _deserialize(value):
+    if value is None:
+        return None
+    # sqlite likes to return ints for strings that look like ints, even though
+    # the column type is string. That's how you do dynamic typing wrong.
+    value = unicode(value)
+    # Just in case someone's mucking with the DB in a way we can't account for,
+    # ignore json parsing errors
+    try:
+        value = json.loads(value)
+    except:
+        pass
+    return value
 
 class WillieDB(object):
 
@@ -27,16 +42,17 @@ class WillieDB(object):
         )
         self.execute(
             'CREATE TABLE nick_values '
-            '(nick_id INTEGER REFERENCES nicknames(nick_id) '
-            '    ON UPDATE CASCADE ON DELETE CASCADE,'
-            'key STRING, value STRING,'
+            '(nick_id INTEGER REFERENCES nicknames(nick_id), '
+            'key STRING, value STRING, '
             'PRIMARY KEY (nick_id, key))'
         )
+        # A dummy record is needed for the method of grabbing an id number to
+        # work
         self.execute('INSERT INTO nicknames VALUES (?, ?, ?)', [0, '', ''])
         self.execute(
             'CREATE TABLE channel_values '
-            '(channel STRING, key STRING, value STRING) '
-            'PRIMARY KEY (channel, key)'
+            '(channel STRING, key STRING, value STRING, '
+            'PRIMARY KEY (channel, key))'
         )
 
     # NICK FUNCTIONS
@@ -73,12 +89,13 @@ class WillieDB(object):
         values = [nick_id, alias.lower(), alias]
         try:
             self.execute(sql, values)
-        except sqlite3.IntegrityError:  #TODO check that it's a unique violation
+        except sqlite3.IntegrityError as e:
             raise ValueError('Alias already exists.')
 
     def set_nick_value(self, nick, key, value):
         """Sets the value for a given key to be associated with the nick."""
         nick = Nick(nick)
+        value = json.dumps(value, ensure_ascii=False)
         nick_id = self._get_nick_id(nick)
         self.execute('INSERT OR REPLACE INTO nick_values VALUES (?, ?, ?)',
                      [nick_id, key, value])
@@ -92,16 +109,7 @@ class WillieDB(object):
         ).fetchone()
         if result is not None:
             result = result[0]
-        return result
-
-    def get_all_nick_values(self, nick):
-        """Returns a dict of all values associated with a nick."""
-        nick = Nick(nick)
-        results = self.execute(
-            'SELECT key, value from nicknames, nick_values WHERE slug = ?',
-            [nick.lower()]).fetchall()
-        results = {key: value for key, value in results}
-        return results
+        return _deserialize(result)
 
     def unalias_nick(self, alias):
         """Removes an alias.
@@ -122,7 +130,8 @@ class WillieDB(object):
         """
         nick = Nick(nick)
         nick_id = self._get_nick_id(nick, False)
-        self.execute('DELETE FROM nicknames WHERE id = ?', [nick_id])
+        self.execute('DELETE FROM nicknames WHERE nick_id = ?', [nick_id])
+        self.execute('DELETE FROM nick_values WHERE nick_id = ?', [nick_id])
 
     def merge_nick_groups(self, first_nick, second_nick):
         """Merges the nick groups for the specified nicks.
@@ -137,7 +146,7 @@ class WillieDB(object):
         will need to have their merging done separately."""
         first_id = self._get_nick_id(Nick(first_nick))
         second_id = self._get_nick_id(Nick(second_nick))
-        data = self.execute(
+        self.execute(
             'UPDATE OR IGNORE nick_values SET nick_id = ? WHERE nick_id = ?',
             [first_id, second_id])
         self.execute('DELETE FROM nick_values WHERE nick_id = ?', [second_id])
@@ -148,6 +157,7 @@ class WillieDB(object):
 
     def set_channel_value(self, channel, key, value):
         channel = Nick(channel).lower()
+        value = json.dumps(value, ensure_ascii=False)
         self.execute('INSERT OR REPLACE INTO channel_values VALUES (?, ?, ?)',
                      [channel, key, value])
 
@@ -155,12 +165,12 @@ class WillieDB(object):
         """Retrieves the value for a given key associated with a channel."""
         channel = Nick(channel).lower()
         result = self.execute(
-            'SELECT value FROM channel WHERE channel = ? AND key = ?',
+            'SELECT value FROM channel_values WHERE channel = ? AND key = ?',
             [channel, key]
         ).fetchone()
         if result is not None:
             result = result[0]
-        return result
+        return _deserialize(result)
 
     # NICK AND CHANNEL FUNCTIONS
 
@@ -169,9 +179,9 @@ class WillieDB(object):
         """
         name = Nick(name)
         if name.is_nick():
-            return self.get_nick_value(trigger.nick, key)
+            return self.get_nick_value(name, key)
         else:
-            return self.get_channel_value(trigger.sender, key)
+            return self.get_channel_value(name, key)
 
     def get_preferred_value(self, names, key):
         """Gets the value for the first name which has it set.
@@ -179,6 +189,6 @@ class WillieDB(object):
         `names` is a list of channel and/or user names. Returns None if none of
         the names have the key set."""
         for name in names:
-            value = self.get_nick_or_channel_value(self, name, key)
-            if value:
+            value = self.get_nick_or_channel_value(name, key)
+            if value is not None:
                 return value
