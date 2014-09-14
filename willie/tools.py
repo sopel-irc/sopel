cca8a3a46f105c4bed4c168b6d1427048628b37e
Edward Powell <git@embolalia.com>
2014-09-14 11:59:41 -0400

    [tools] Fix a leftover old db reference

--- willie/tools.py
+++ willie/tools.py
@@ -556,7 +556,7 @@ def format_time(db=None, config=None, zone=None, nick=None, channel=None,
     if db:
         if nick:
             tformat = db.get_nick_value(nick, 'time_format')
-        if not tformat and channel in db.preferences:
+        if not tformat:
             tformat = db.get_channel_value(channel, 'time_format')
     if not tformat and config and config.has_option('core',
                                                     'default_time_format'):
