1c6ff5eb397976b06325fe371841c19c02ae9b21
Edward Powell <git@embolalia.com>
2015-01-19 10:15:05 -0500

    db: Fix path creation problems

--- willie/db.py
+++ willie/db.py
@@ -39,11 +39,12 @@ class WillieDB(object):
     directory as the config."""
 
     def __init__(self, config):
-        path = os.path.expanduser(config.core.db_filename)
+        path = config.core.db_filename
         config_dir, config_file = os.path.split(config.filename)
         config_name, _ = os.path.splitext(config_file)
         if path is None:
-            os.path.join(config_dir, config_name, '.db')
+            path = os.path.join(config_dir, config_name + '.db')
+        path = os.path.expanduser(path)
         if not os.path.isabs(path):
             path = os.path.normpath(os.path.join(config_dir, path))
         self.filename = path
