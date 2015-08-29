5175660e61c7733131e9c4c90aa7bf15ecbe4dc9
Embolalia <git@embolalia.com>
2015-08-29 11:21:01 -0400

    Clear job queue when reloading
    Just a stop-gap to make #831 not a regression so 6.0 can get out the
    door. It's not the right solution, but it's slightly better than what
    was there before. Only reloading a module with a job actually in it will
    kill all the jobs now, rather than any module. So, progress?

--- sopel/bot.py
+++ sopel/bot.py
@@ -162,7 +162,9 @@ class Sopel(irc.Bot):
             for rule in obj.rule:
                 self._callables[obj.priority][rule].remove(obj)
         if hasattr(obj, 'interval'):
-            pass  # TODO
+            # TODO this should somehow find the right job to remove, rather than
+            # clearing the entire queue. Issue #831
+            self.scheduler.clear_jobs()
         if getattr(obj, '__name__', None) == 'shutdown':
             self.shutdown_methods.remove(obj)
 
