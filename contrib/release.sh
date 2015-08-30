2e9180618dd1bd80cde8834a380e6576ca27cf1d
Embolalia <git@embolalia.com>
2015-08-30 10:04:02 -0400

    Update release script for sopel

--- contrib/release.sh
+++ contrib/release.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-# Runs through the steps to release a Willie version. This is only useful to
+# Runs through the steps to release a Sopel version. This is only useful to
 # the people with permissions to do so, of course.
 set -e
 cd $(dirname $0)/..
 
-version=$(python -c "import willie; print(willie.__version__)")
-echo "Releasing Willie version $version."
+version=$(python -c "import sopel; print(sopel.__version__)")
+echo "Releasing Sopel version $version."
 
 echo "PyPI username:"
 read pypi_user
 echo "PyPI password:"
 read pypi_pass
-echo "willie.dftba.net username:"
+echo "sopel.chat username:"
 read server_user
 
 cat <<EOF > ~/.pypirc
@@ -32,10 +32,10 @@ echo "Building docs..."
 cd docs
 make html
 
-echo "Setting up folders on willie.dftba.net..."
-ssh $server_user@willie.dftba.net "mkdir /var/www/willie/$version; rm /var/www/willie/docs; ln -s /var/www/willie/$version/docs /var/www/willie/docs"
+echo "Setting up folders on sopel.chat..."
+ssh $server_user@sopel.chat "mkdir /var/www/sopel/$version; rm /var/www/sopel/docs; ln -s /var/www/sopel/$version/docs /var/www/sopel/docs"
 
 echo "Uploading docs..."
-scp -r build/html $server_user@willie.dftba.net:/var/www/willie/$version/docs
+scp -r build/html $server_user@sopel.chat:/var/www/sopel/$version/docs
 
 echo "Done!"
