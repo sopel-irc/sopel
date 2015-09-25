d48910f9e0119e70a6cc682f2eb63e3963af9e45
James McDonald <james@jamesmcdonald.com>
2015-09-25 20:36:49 +0200

    Update the other instances of the help URL.

--- sopel/modules/meetbot.py
+++ sopel/modules/meetbot.py
@@ -201,7 +201,7 @@ def startmeeting(bot, trigger):
 def meetingsubject(bot, trigger):
     """
     Change the meeting subject.
-    https://github.com/embolalia/sopel/wiki/Using-the-meetbot-module
+    https://github.com/sopel-irc/sopel/wiki/Using-the-meetbot-module
     """
     if not ismeetingrunning(trigger.sender):
         bot.say('Can\'t do that, start meeting first')
@@ -226,7 +226,7 @@ def meetingsubject(bot, trigger):
 def endmeeting(bot, trigger):
     """
     End a meeting.
-    https://github.com/embolalia/sopel/wiki/Using-the-meetbot-module
+    https://github.com/sopel-irc/sopel/wiki/Using-the-meetbot-module
     """
     if not ismeetingrunning(trigger.sender):
         bot.say('Can\'t do that, start meeting first')
@@ -251,7 +251,7 @@ def endmeeting(bot, trigger):
 def chairs(bot, trigger):
     """
     Set the meeting chairs.
-    https://github.com/embolalia/sopel/wiki/Using-the-meetbot-module
+    https://github.com/sopel-irc/sopel/wiki/Using-the-meetbot-module
     """
     if not ismeetingrunning(trigger.sender):
         bot.say('Can\'t do that, start meeting first')
@@ -275,7 +275,7 @@ def chairs(bot, trigger):
 def meetingaction(bot, trigger):
     """
     Log an action in the meeting log
-    https://github.com/embolalia/sopel/wiki/Using-the-meetbot-module
+    https://github.com/sopel-irc/sopel/wiki/Using-the-meetbot-module
     """
     if not ismeetingrunning(trigger.sender):
         bot.say('Can\'t do that, start meeting first')
@@ -308,7 +308,7 @@ def listactions(bot, trigger):
 def meetingagreed(bot, trigger):
     """
     Log an agreement in the meeting log.
-    https://github.com/embolalia/sopel/wiki/Using-the-meetbot-module
+    https://github.com/sopel-irc/sopel/wiki/Using-the-meetbot-module
     """
     if not ismeetingrunning(trigger.sender):
         bot.say('Can\'t do that, start meeting first')
@@ -330,7 +330,7 @@ def meetingagreed(bot, trigger):
 def meetinglink(bot, trigger):
     """
     Log a link in the meeing log.
-    https://github.com/embolalia/sopel/wiki/Using-the-meetbot-module
+    https://github.com/sopel-irc/sopel/wiki/Using-the-meetbot-module
     """
     if not ismeetingrunning(trigger.sender):
         bot.say('Can\'t do that, start meeting first')
@@ -359,7 +359,7 @@ def meetinglink(bot, trigger):
 def meetinginfo(bot, trigger):
     """
     Log an informational item in the meeting log
-    https://github.com/embolalia/sopel/wiki/Using-the-meetbot-module
+    https://github.com/sopel-irc/sopel/wiki/Using-the-meetbot-module
     """
     if not ismeetingrunning(trigger.sender):
         bot.say('Can\'t do that, start meeting first')
@@ -395,7 +395,7 @@ def take_comment(bot, trigger):
     in the meeting.
 
     Used in private message only, as `.comment <#channel> <comment to add>`
-    https://github.com/embolalia/sopel/wiki/Using-the-meetbot-module
+    https://github.com/sopel-irc/sopel/wiki/Using-the-meetbot-module
     """
     if not trigger.sender.is_nick():
         return
@@ -418,7 +418,7 @@ def take_comment(bot, trigger):
 def show_comments(bot, trigger):
     """
     Show the comments that have been logged for this meeting with .comment.
-    https://github.com/embolalia/sopel/wiki/Using-the-meetbot-module
+    https://github.com/sopel-irc/sopel/wiki/Using-the-meetbot-module
     """
     if not ismeetingrunning(trigger.sender):
         return
