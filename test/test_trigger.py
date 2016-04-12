7ae34a6301004582b957dd7b7ea3ab2a82acb1d1
Max Gurela <maxpowa1@gmail.com>
2016-04-12 10:26:42 -0700

    Add IRCv3 extended-join tests

--- test/test_trigger.py
+++ test/test_trigger.py
@@ -126,7 +126,48 @@ def test_ctcp_data_pretrigger(nick):
     assert pretrigger.sender == Identifier('Foo')
 
 
-def test_intents_trigger(nick):
+def test_ircv3_extended_join_pretrigger(nick):
+    line = ':Foo!foo@example.com JOIN #Sopel bar :Real Name'
+    pretrigger = PreTrigger(nick, line)
+    assert pretrigger.tags == {}
+    assert pretrigger.hostmask == 'Foo!foo@example.com'
+    assert pretrigger.line == line
+    assert pretrigger.args == ['#Sopel', 'bar', 'Real Name']
+    assert pretrigger.event == 'JOIN'
+    assert pretrigger.nick == Identifier('Foo')
+    assert pretrigger.user == 'foo'
+    assert pretrigger.host == 'example.com'
+    assert pretrigger.sender == Identifier('#Sopel')
+
+
+def test_ircv3_extended_join_trigger(nick):
+    line = ':Foo!foo@example.com JOIN #Sopel bar :Real Name'
+    pretrigger = PreTrigger(nick, line)
+
+    config = MockConfig()
+    config.core.owner_account = 'bar'
+
+    fakematch = re.match('.*', line)
+
+    trigger = Trigger(config, pretrigger, fakematch)
+    assert trigger.sender == '#Sopel'
+    assert trigger.raw == line
+    assert trigger.is_privmsg is False
+    assert trigger.hostmask == 'Foo!foo@example.com'
+    assert trigger.user == 'foo'
+    assert trigger.nick == Identifier('Foo')
+    assert trigger.host == 'example.com'
+    assert trigger.event == 'JOIN'
+    assert trigger.match == fakematch
+    assert trigger.group == fakematch.group
+    assert trigger.groups == fakematch.groups
+    assert trigger.args == ['#Sopel', 'bar', 'Real Name']
+    assert trigger.tags == {}
+    assert trigger.owner is True
+    assert trigger.admin is True
+
+
+def test_ircv3_intents_trigger(nick):
     line = '@intent=ACTION :Foo!foo@example.com PRIVMSG #Sopel :Hello, world'
     pretrigger = PreTrigger(nick, line)
 
@@ -154,6 +195,7 @@ def test_intents_trigger(nick):
     assert trigger.admin is True
     assert trigger.owner is True
 
+
 def test_ircv3_account_tag_trigger(nick):
     line = '@account=Foo :Nick_Is_Not_Foo!foo@example.com PRIVMSG #Sopel :Hello, world'
     pretrigger = PreTrigger(nick, line)
@@ -167,6 +209,7 @@ def test_ircv3_account_tag_trigger(nick):
     trigger = Trigger(config, pretrigger, fakematch)
     assert trigger.admin is True
     assert trigger.owner is True
+
 
 def test_ircv3_server_time_trigger(nick):
     line = '@time=2016-01-09T03:15:42.000Z :Foo!foo@example.com PRIVMSG #Sopel :Hello, world'
