6477fd10605e2af0d4798dff9013cb92f2db51a1
Max Gurela <maxpowa1@gmail.com>
2016-05-01 22:44:16 -0700

    Removed default so the code is less confusing

--- sopel/module.py
+++ sopel/module.py
@@ -115,7 +115,7 @@ def thread(value):
     return add_attribute
 
 
-def echo(value=True):
+def echo(value):
     """Decorate a function to specify if it should receive echo messages.
 
     This decorator can be used to listen in on the messages that Sopel is
