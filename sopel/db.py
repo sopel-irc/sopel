4355aae2eb3ba620c5441f72c783dbb4a5ef6d67
dgw <dgw@technobabbl.es>
2016-07-19 18:19:53 -0500

    db: raise ValueError in unalias_nick to match documentation (#1102)
    Documentation says that a ValueError should be raised if there is not at least one other nick in the group.
    
    Resolves #1101

--- sopel/db.py
+++ sopel/db.py
@@ -174,7 +174,7 @@ class SopelDB(object):
         nick_id = self.get_nick_id(alias, False)
         count = self.execute('SELECT COUNT(*) FROM nicknames WHERE nick_id = ?',
                              [nick_id]).fetchone()[0]
-        if count == 0:
+        if count <= 1:
             raise ValueError('Given alias is the only entry in its group.')
         self.execute('DELETE FROM nicknames WHERE slug = ?', [alias.lower()])
 
