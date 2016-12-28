e8f6d7255ea7cbbf9e30e052eb3327e0fad7ecae
Alan Huang <Alan.Huang@utdallas.edu>
2016-12-28 01:27:00 -0600

    Rewrite spellcheck to use aspell, now with 50% less fail

--- sopel/modules/spellcheck.py
+++ sopel/modules/spellcheck.py
@@ -1,55 +1,46 @@
 # coding=utf-8
-"""
-spellcheck.py - Sopel spell check Module
-Copyright © 2012, Elad Alfassa, <elad@fedoraproject.org>
-Copyright © 2012, Lior Ramati
-Licensed under the Eiffel Forum License 2.
 
-https://sopel.chat
+import aspell
+from sopel.module import commands
 
-This module relies on pyenchant, on Fedora and Red Hat based system, it can be found in the package python-enchant
-"""
-from __future__ import unicode_literals, absolute_import, print_function, division
-
-try:
-    import enchant
-except ImportError:
-    enchant = None
-from sopel.module import commands, example
-
-
-@commands('spellcheck', 'spell')
-@example('.spellcheck stuff')
-def spellcheck(bot, trigger):
-    """
-    Says whether the given word is spelled correctly, and gives suggestions if
-    it's not.
-    """
-    if not enchant:
-        bot.say("Missing pyenchant module.")
-    if not trigger.group(2):
-        return
-    word = trigger.group(2).rstrip()
-    if " " in word:
-        bot.say("One word at a time, please")
-        return
-    dictionary = enchant.Dict("en_US")
-    dictionary_uk = enchant.Dict("en_GB")
-    # I don't want to make anyone angry, so I check both American and British English.
-    if dictionary_uk.check(word):
-        if dictionary.check(word):
-            bot.say(word + " is spelled correctly")
-        else:
-            bot.say(word + " is spelled correctly (British)")
-    elif dictionary.check(word):
-        bot.say(word + " is spelled correctly (American)")
+@commands('add')
+def add_command(bot, trigger):
+    if(trigger.owner):
+        c = aspell.Speller('lang', 'en')
+        c.addtoPersonal(trigger.group(2))
+        c.saveAllwords()
+        bot.say('Added {0}.'.format(trigger.group(2)))
     else:
-        msg = word + " is not spelled correctly. Maybe you want one of these spellings:"
-        sugWords = []
-        for suggested_word in dictionary.suggest(word):
-                sugWords.append(suggested_word)
-        for suggested_word in dictionary_uk.suggest(word):
-                sugWords.append(suggested_word)
-        for suggested_word in sorted(set(sugWords)):  # removes duplicates
-            msg = msg + " '" + suggested_word + "',"
-        bot.say(msg)
+        bot.say('I only trust {0} to add words >:c'.format(bot.config.core.owner))
+
+@commands('spell')
+def spellchecker(bot, trigger):
+    if not trigger.group(2):
+        bot.say('What word am I checking?')
+        return
+
+    if trigger.group(2) == bot.nick:
+        bot.say('Hey, that\'s my name! Nothing wrong with it.')
+        return
+
+    if len(trigger.group(2).split(' ')) > 1:
+        bot.say('One word at a time, please.')
+        return
+
+    c = aspell.Speller('lang', 'en')
+    if c.check(trigger.group(2)):
+        bot.say("I don't see any problems with that word.")
+        return
+
+    suggestions = c.suggest(trigger.group(2))
+    count = 0
+    suggestion = ''
+    for word in suggestions:
+        if count < 5:
+            suggestion += '"{0}", '.format(word)
+            count += 1
+    
+    if len(suggestion) == 0:
+        bot.say("That doesn't seem to be correct.")
+    else:
+        bot.say("That doesn't seem to be correct. Try {0}.".format(suggestion[:-2]))
