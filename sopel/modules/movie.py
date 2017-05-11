e0d58e3e5620142f339f8d4c851c15a741353db0
Sujeet Akula <sujeet@freeboson.org>
2017-05-11 11:54:42 +1000

    make .imdb more useful

--- sopel/modules/movie.py
+++ sopel/modules/movie.py
@@ -11,9 +11,20 @@ from __future__ import unicode_literals, absolute_import, print_function, divisi
 import requests
 import sopel.module
 from sopel.logger import get_logger
+import re
 
 LOGGER = get_logger(__name__)
 
+yearfmt = re.compile('\((\d{4})\)')
+
+def setup(bot):
+    if not bot.memory.contains('url_callbacks'):
+        bot.memory['url_callbacks'] = tools.SopelMemory()
+    imdb_re = re.compile(r'(imdb\.com\/title\/)(tt[0-9]+)')
+    bot.memory['url_callbacks'][imdb_re] = imdb_url
+
+def shutdown(bot):
+    del bot.memory['url_callbacks'][imdb_url]
 
 @sopel.module.commands('movie', 'imdb')
 @sopel.module.example('.movie ThisTitleDoesNotExist', '[MOVIE] Movie not found!')
@@ -25,9 +36,24 @@ def movie(bot, trigger):
     if not trigger.group(2):
         return
     word = trigger.group(2).rstrip()
+    params={}
+
+    # check to see if there is a year e.g. (2017) at the end
+    last = word.split()[-1]
+    yrm = yearfmt.match(last)
+    if yrm is not None:
+        params['y'] = yrm.group(1)
+        word = ' '.join(word.split()[:-1])
+
+    params['t'] = word
+    bot.say(run_omdb_query(params, bot.config.core.verify_ssl, True))
+
+def run_omdb_query(params, verify_ssl, add_url=True):
     uri = "http://www.omdbapi.com/"
-    data = requests.get(uri, params={'t': word}, timeout=30,
-                        verify=bot.config.core.verify_ssl).json()
+#    data = requests.get(uri, params={'t': word}, timeout=30,
+#                        verify=bot.config.core.verify_ssl).json()
+    data = requests.get(uri, params=params, timeout=30,
+                        verify=verify_ssl).json()
     if data['Response'] == 'False':
         if 'Error' in data:
             message = '[MOVIE] %s' % data['Error']
@@ -41,9 +67,22 @@ def movie(bot, trigger):
                   ' | Year: ' + data['Year'] + \
                   ' | Rating: ' + data['imdbRating'] + \
                   ' | Genre: ' + data['Genre'] + \
-                  ' | IMDB Link: http://imdb.com/title/' + data['imdbID']
-    bot.say(message)
+                  ' | Plot: {}'
+        if add_url:
+            message += ' | IMDB Link: http://imdb.com/title/' + data['imdbID']
 
+        plot = data['Plot']
+        if len(message.format(plot)) > 300:
+            cliplen = 300 - (len(message) - 2 + 3) # remove {} add […]
+            plot = plot[:cliplen] + '[…]'
+
+    return message.format(plot)
+
+@sopel.module.rule('.*(imdb\.com\/title\/)(tt[0-9]+).*')
+def imdb_url(bot, trigger, found_match=None):
+    match = found_match or trigger
+    bot.say(run_omdb_query({'i': match.group(2)},
+                            bot.config.core.verify_ssl, False))
 
 if __name__ == "__main__":
     from sopel.test_tools import run_example_tests
