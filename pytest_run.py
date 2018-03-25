c1e291367f1245ba94dca2dd39514c1292b96c1f
Natalie Fearnley <nfearnley@gmail.com>
2018-03-25 16:33:37 -0400

    updated old references to willie

--- pytest_run.py
+++ pytest_run.py
@@ -3,7 +3,7 @@
 """This is a script for running pytest from the command line.
 
 This script exists so that the project directory gets added to sys.path, which
-prevents us from accidentally testing the globally installed willie version.
+prevents us from accidentally testing the globally installed sopel version.
 
 pytest_run.py
 Copyright 2013, Ari Koivula, <ari@koivu.la>
