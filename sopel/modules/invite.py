d6eeee9195988a15f7e26cb3a7d30a2f14db654f
dgw <dgw@technobabbl.es>
2018-03-26 15:59:03 -0500

    [invite] fix `trigger.groups(3)`
    Regex match object's `groups()` method is not an alias for `group()`, it
    does something else (and not what's expected here).

--- sopel/modules/invite.py
+++ sopel/modules/invite.py
@@ -7,6 +7,6 @@ from sopel.module import commands, require_owner, OP
 def invite(bot, trigger):
     if bot.privileges[trigger.sender][bot.nick] < OP:
         return bot.reply("I'm not a channel operator!")
-    if not trigger.groups(3) or not trigger.group(4):
+    if not trigger.group(3) or not trigger.group(4):
         return bot.reply("Usage: .invite user #channel (the bot must be operator and in the invited channel)")
     bot.invite(trigger.group(3), trigger.group(4))
