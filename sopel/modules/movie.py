15253a5c15a0491b45bc64af90bf7712cb598f93
dgw <dgw@technobabbl.es>
2018-11-05 00:36:45 -0600

    movie: style & missing import (fixup)

--- sopel/modules/movie.py
+++ sopel/modules/movie.py
@@ -11,12 +11,14 @@ from __future__ import unicode_literals, absolute_import, print_function, divisi
 import requests
 import sopel.module
 from sopel.logger import get_logger
+from sopel import tools
 import re
-from urllib.parse import quote 
+from urllib.parse import quote
 
 LOGGER = get_logger(__name__)
 
-yearfmt = re.compile('\((\d{4})\)')
+yearfmt = re.compile(r'\((\d{4})\)')
+
 
 def setup(bot):
     if not bot.memory.contains('url_callbacks'):
@@ -24,8 +26,10 @@ def setup(bot):
     imdb_re = re.compile(r'(imdb\.com\/title\/)(tt[0-9]+)')
     bot.memory['url_callbacks'][imdb_re] = imdb_url
 
+
 def shutdown(bot):
     del bot.memory['url_callbacks'][imdb_url]
+
 
 @sopel.module.commands('movie', 'imdb')
 @sopel.module.example('.movie ThisTitleDoesNotExist', '[MOVIE] Movie not found!')
@@ -37,7 +41,7 @@ def movie(bot, trigger):
     if not trigger.group(2):
         return
     word = trigger.group(2).rstrip()
-    params={}
+    params = {}
 
     # check to see if there is a year e.g. (2017) at the end
     last = word.split()[-1]
@@ -49,10 +53,11 @@ def movie(bot, trigger):
     params['t'] = word
     bot.say(run_omdb_query(params, bot.config.core.verify_ssl, True))
 
+
 def run_omdb_query(params, verify_ssl, add_url=True):
     uri = "https://theimdbapi.org/api/find/movie?"
     if 'i' in params:
-        uri = uri.replace("/find","")
+        uri = uri.replace("/find", "")
         data = requests.get(uri + "movie_id={}".format(params['i']), timeout=30,
                         verify=verify_ssl)
     elif 'y' in params:
@@ -78,17 +83,19 @@ def run_omdb_query(params, verify_ssl, add_url=True):
 
         plot = data['description']
         if len(message.format(plot)) > 300:
-            cliplen = 300 - (len(message) - 2 + 3) # remove {} add […]
+            cliplen = 300 - (len(message) - 2 + 3)  # remove {} add […]
             plot = plot[:cliplen] + '[…]'
         message = message.format(plot)
 
     return message
 
-@sopel.module.rule('.*(imdb\.com\/title\/)(tt[0-9]+).*')
+
+@sopel.module.rule(r'.*(imdb\.com\/title\/)(tt[0-9]+).*')
 def imdb_url(bot, trigger, found_match=None):
     match = found_match or trigger
     bot.say(run_omdb_query({'i': match.group(2)},
                             bot.config.core.verify_ssl, False))
+
 
 if __name__ == "__main__":
     from sopel.test_tools import run_example_tests
