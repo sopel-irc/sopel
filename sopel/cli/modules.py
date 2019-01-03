1b66fc77c94abdd7d7cbb12c8d48bb71a00ffd0a
Florian Strzelecki <florian.strzelecki@gmail.com>
2019-01-03 19:47:48 +0100

    cli: sopel-module list option --path

--- sopel/cli/modules.py
+++ sopel/cli/modules.py
@@ -12,12 +12,20 @@ def main():
     parser = argparse.ArgumentParser(
         description='Experimental Sopel Module tool')
     subparsers = parser.add_subparsers(
-        help='Actions to perform, default to list',
+        help='Actions to perform (default to list)',
         dest='action')
 
     # Configure LIST action
-    subparsers.add_parser(
-        'list', help='List availables sopel modules')
+    list_parser = subparsers.add_parser(
+        'list',
+        help='List availables sopel modules',
+        description='List availables sopel modules')
+    list_parser.add_argument(
+        '-p', '--path',
+        action='store_true',
+        dest='show_path',
+        default=False,
+        help='Show the path to the module file')
 
     options = parser.parse_args()
     action = options.action or 'list'
@@ -25,11 +33,27 @@ def main():
     if action == 'list':
         config_filename = run_script.find_config('default')
         settings = config.Config(config_filename)
+        modules = loader.enumerate_modules(settings)
         modules = sorted(
-            tools.iteritems(loader.enumerate_modules(settings)),
+            modules.items(),
             key=lambda arg: arg[0])
 
+        template = '{name}'
+        if options.show_path:
+            # Get the maximum length of module names for display purpose
+            max_length = max(len(info[0]) for info in modules)
+
+            template = '\t'.join([
+                '{name:<' + str(max_length) +'}',
+                '{path}',
+            ])
+
         for name, info in modules:
-            print(name)
+            path, module_type = info
+            print(template.format(
+                name=name,
+                path=path,
+                module_type=module_type,
+            ))
 
         return
