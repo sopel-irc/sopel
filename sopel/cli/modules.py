c685418fbaedc3f7da8d218c403217ce6d0b9f82
Florian Strzelecki <florian.strzelecki@gmail.com>
2019-01-03 20:42:07 +0100

    cli: sopel-module list option --type

--- sopel/cli/modules.py
+++ sopel/cli/modules.py
@@ -3,8 +3,15 @@
 from __future__ import unicode_literals, absolute_import, print_function, division
 
 import argparse
+import imp
 
-from sopel import loader, run_script, config, tools
+from sopel import loader, run_script, config
+
+
+DISPLAY_TYPE = {
+    imp.PKG_DIRECTORY: 'p',
+    imp.PY_SOURCE: 'm'
+}
 
 
 def main():
@@ -26,6 +33,15 @@ def main():
         dest='show_path',
         default=False,
         help='Show the path to the module file')
+    list_parser.add_argument(
+        '-t', '--type',
+        action='store_true',
+        dest='show_type',
+        default=False,
+        help=('Show the type to the module file: '
+              '`p` for package directory, '
+              '`m` for module file, '
+              '`?` for unknown'))
 
     options = parser.parse_args()
     action = options.action or 'list'
@@ -38,22 +54,26 @@ def main():
             modules.items(),
             key=lambda arg: arg[0])
 
+        col_sep = '\t'
         template = '{name}'
         if options.show_path:
             # Get the maximum length of module names for display purpose
             max_length = max(len(info[0]) for info in modules)
 
-            template = '\t'.join([
-                '{name:<' + str(max_length) +'}',
+            template = col_sep.join([
+                '{name:<' + str(max_length) + '}',
                 '{path}',
             ])
+
+        if options.show_type:
+            template = col_sep.join(['{module_type}', template])
 
         for name, info in modules:
             path, module_type = info
             print(template.format(
                 name=name,
                 path=path,
-                module_type=module_type,
+                module_type=DISPLAY_TYPE.get(module_type, '?'),
             ))
 
         return
