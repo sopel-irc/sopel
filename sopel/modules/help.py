77817242f85a8d3761b0021631bf7d2f92b690c0
Tomasz Kurcz <tom@uint>
2019-01-15 18:40:04 +0000

    help: add 0x0, hastebin, and termbin as help outputs
    (Rebased, default changed from ptpb->clbin, 0x0 switched to HTTPS, and
    whitespace fixed by dgw)
    
    Co-Authored-By: dgw <dgw@technobabbl.es>

--- sopel/modules/help.py
+++ sopel/modules/help.py
@@ -12,6 +12,7 @@ from __future__ import unicode_literals, absolute_import, print_function, divisi
 
 import collections
 import requests
+import socket
 import textwrap
 
 from sopel.logger import get_logger
@@ -22,7 +23,7 @@ from sopel.config.types import (
 
 
 class HelpSection(StaticSection):
-    output = ChoiceAttribute('output', ["clbin"], default='clbin')
+    output = ChoiceAttribute('output', ["clbin", "0x0", "hastebin", "termbin"], default='clbin')
 
 
 def configure(config):
@@ -116,6 +117,36 @@ def post_to_clbin(bot, msg):
     else:
         LOGGER.error("Invalid result %s", result)
         raise PostingException()
+
+
+def post_to_0x0(bot, msg):
+    payload = {'file': msg}
+    result = requests.post('https://0x0.st', files=payload)
+    return result.text
+
+
+def post_to_hastebin(bot, msg):
+    result = requests.post('https://hastebin.com/documents', data=msg)
+    result = result.json()
+    if 'key' not in result:
+        LOGGER.error("Invalid result %s", result)
+        raise PostingException()
+    return "https://hastebin.com/" + result['key']
+
+
+def post_to_termbin(bot, msg):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('termbin.com', 9999))
+    s.sendall(msg)
+    s.shutdown(socket.SHUT_WR)
+    response = ""
+    while 1:
+        data = s.recv(1024)
+        if data == "":
+            break
+        response += data
+    s.close()
+    return response.strip(u'\x00\n')
 
 
 class PostingException(Exception):
