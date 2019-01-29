a19916851d38a1905078262eeff52af0f53c3da4
dgw <dgw@technobabbl.es>
2019-01-29 06:04:41 -0600

    setup.py: Add Python 3.3 Trove classifier
    Technically we still advertise support for this version, too.

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ classifiers = [
     'License :: OSI Approved :: Eiffel Forum License',
     'Operating System :: POSIX :: Linux',
     'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
