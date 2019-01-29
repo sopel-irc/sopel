59acc737cba2d55a51ae7b26f7d2b32200aa1e33
Humorous Baby <44451911+HumorBaby@users.noreply.github.com>
2019-01-29 08:06:23 -0500

    version: include python version
    Sometimes I forget which version of python I am using :( Really though, this stemmed from testing issues/code across multiple versions of Python. I could _quickly_ check the Sopel version from the bot, but not the python version it was running on. This commit simply appends the Python version to the `.version` response. This will still be useful even after Sopel drops Python 2 compatibility.

--- sopel/modules/version.py
+++ sopel/modules/version.py
@@ -14,6 +14,8 @@ from sopel import __version__ as release
 from sopel.module import commands, intent, rate
 import re
 from os import path
+import platform
+
 
 log_line = re.compile(r'\S+ (\S+) (.*? <.*?>) (\d+) (\S+)\tcommit[^:]*: (.+)')
 
@@ -37,13 +39,13 @@ def version(bot, trigger):
     """Display the latest commit version, if Sopel is running in a git repo."""
     sha = git_info()
     if not sha:
-        msg = 'Sopel v. ' + release
+        msg = 'Sopel v. ' + release + ' (Python {})'.format(platform.python_version())
         if release[-4:] == '-git':
             msg += ' at unknown commit.'
         bot.reply(msg)
         return
 
-    bot.reply("Sopel v. {} at commit: {}".format(release, sha))
+    bot.reply("Sopel v. {} (Python {}) at commit: {}".format(release, platform.python_version(), sha))
 
 
 @intent('VERSION')
