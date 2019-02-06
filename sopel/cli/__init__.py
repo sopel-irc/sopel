d2b88f43d645ab4986ff82d1ff1ee551317f209f
Florian Strzelecki <florian.strzelecki@gmail.com>
2019-02-06 17:29:14 +0100

    cli: shortcuts to sopel.cli.utils in sopel.cli

--- sopel/cli/__init__.py
+++ sopel/cli/__init__.py
@@ -2,4 +2,9 @@
 from __future__ import unicode_literals, absolute_import, print_function, division
 
 # Shortcut imports
-from .utils import enumerate_configs, find_config, load_settings  # noqa
+from .utils import (  # noqa
+    add_config_arguments,
+    enumerate_configs,
+    find_config,
+    load_settings
+)
