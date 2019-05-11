af41fc748327f9c6d9aab43cc9506e17bda46784
dgw <dgw@technobabbl.es>
2019-05-10 22:32:14 -0500

    invite: privilege checking, the sensible way
    As promised almost two months ago, I did finally "revisit the whole
    privilege-checking thing" and make it so the `.invite` command is at
    least a bit sensible.
    
    Tested cases, in PM:
    
    * Neither caller nor bot in target channel (error)
    * Caller in target channel; bot not (error)
    * Bot in target channel; caller not (error)
    * Caller has op in target channel; bot does not (error)
    * Bot has op in target channel; caller does not (error)
    * Bot and caller both have op in target channel (sends INVITE)
    
    Tested cases, in a channel (without optional channel argument):
    
    * Neither caller nor bot has op in current channel (error)
    * Caller has op in current channel; bot does not (error)
    * Bot has op in current channel; caller does not (error)
    * Bot and caller both have op in current channel (sends INVITE)
    
    Tested cases, in a channel (with optional channel argument)
    
    * Neither caller nor bot in target channel (error)
    * Caller in target channel; bot not (error)
    * Bot in target channel; caller not (error)
    
    (This last set of tests is admittedly sparse, but I think my manual test
    coverage here is Good Enough™ all the same.)

--- sopel/modules/invite.py
+++ sopel/modules/invite.py
@@ -9,12 +9,52 @@ https://sopel.chat
 """
 from __future__ import unicode_literals, absolute_import, print_function, division
 
-from sopel.module import commands, example, OP
+from sopel import module, tools
 
 
-@commands('invite')
-@example('.invite jenny')
-@example('.invite converge #sopel')
+MIN_PRIV = module.HALFOP
+
+
+def invite_handler(bot, sender, user, channel):
+    """Common control logic for invite commands received from anywhere."""
+    sender = tools.Identifier(sender)
+    user = tools.Identifier(user)
+    channel = tools.Identifier(channel)
+
+    # Sanity checks, in case someone reuses this function from outside the plugin
+    if not sender.is_nick():
+        raise ValueError("Invite sender must be a nick, not a channel.")
+    if not user.is_nick():
+        raise ValueError("User to invite must be a nick, not a channel.")
+    if channel.is_nick():
+        raise ValueError("Target channel name must not be a nick.")
+
+    # Sopel must be in the target channel
+    if channel not in bot.channels or bot.nick not in bot.channels[channel].privileges:
+        return bot.reply("I'm not in {}!".format(channel))
+
+    privs = bot.channels[channel].privileges
+
+    # Sopel must have sufficient privileges in the target channel to send invites
+    if privs[bot.nick] < MIN_PRIV:
+        return bot.reply("I don't have permission to invite anyone into {}.".format(channel))
+
+    # The sender must be in the target channel
+    if sender not in privs:
+        return bot.reply("You're not in {}.".format(channel))
+
+    # The sender must have sufficient privileges in the target channel to send invites
+    if privs[sender] < MIN_PRIV:
+        return bot.reply("You don't have permission to invite anyone into {}.".format(channel))
+
+    # Sopel and the sender both passed permission checks.
+    # DDDDOOOO IIIITTTT
+    bot.write(['INVITE', user, channel])
+
+
+@module.commands('invite')
+@module.example('.invite jenny')
+@module.example('.invite converge #sopel')
 def invite(bot, trigger):
     """
     Invite the given user to the current channel, or (with optional
@@ -22,7 +62,8 @@ def invite(bot, trigger):
     """
     if not trigger.group(3):
         return bot.reply("Whom should I invite?")
-    nick = trigger.group(3)
+    user = trigger.group(3)
+
     if trigger.group(4):
         channel = trigger.group(4)
     else:
@@ -31,9 +72,5 @@ def invite(bot, trigger):
                 "Channel is required ({}invite user #channel) when inviting from queries."
                 .format(bot.config.core.help_prefix))
         channel = trigger.sender
-    try:
-        if bot.channels[channel].privileges[bot.nick] < OP:
-            return bot.reply("I'm not a channel operator!")
-    except KeyError:
-        return bot.reply("I'm not in {}!".format(channel))
-    bot.write(['INVITE', nick, channel])
+
+    invite_handler(bot, trigger.nick, user, channel)
