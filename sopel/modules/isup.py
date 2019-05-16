7129f83671f050811dec0493dcf21548a1227860
Florian Strzelecki <florian.strzelecki@gmail.com>
2019-05-16 23:47:43 +0200

    isup: extract site URL validation - with tests

--- sopel/modules/isup.py
+++ sopel/modules/isup.py
@@ -14,6 +14,36 @@ from requests.exceptions import SSLError
 from sopel.module import commands
 
 
+def get_site_url(site):
+    """Get a ``site`` URL
+
+    :param str site: the site to get URL for
+    :return: a valid site URL
+    :raise ValueError: when site is empty, or isn't well formatted
+
+    The ``site`` argument is checked: its scheme must be ``http`` or ``https``,
+    or a :exc:`ValueError` is raised.
+
+    If the ``site`` does not have a scheme, ``http`` is used. If it doesn't
+    have a TLD, ``.com`` is used.
+    """
+    site = site.strip() if site else ''
+    if not site:
+        raise ValueError('What site do you want to check?')
+
+    if site[:7] != 'http://' and site[:8] != 'https://':
+        if '://' in site:
+            protocol = site.split('://')[0] + '://'
+            raise ValueError('Try it again without the %s' % protocol)
+        else:
+            site = 'http://' + site
+
+    if '.' not in site:
+        site += ".com"
+
+    return site
+
+
 def handle_isup(bot, trigger, secure=True):
     """Handle the ``bot`` command from ``trigger``
 
@@ -24,18 +54,11 @@ def handle_isup(bot, trigger, secure=True):
     :param bool secure: Check SSL error if ``True`` (the default)
     """
     site = trigger.group(2)
-    if not site:
-        return bot.reply("What site do you want to check?")
 
-    if site[:7] != 'http://' and site[:8] != 'https://':
-        if '://' in site:
-            protocol = site.split('://')[0] + '://'
-            return bot.reply("Try it again without the %s" % protocol)
-        else:
-            site = 'http://' + site
-
-    if '.' not in site:
-        site += ".com"
+    try:
+        site = get_site_url(trigger.group(2))
+    except ValueError as error:
+        return bot.reply(str(error))
 
     try:
         response = requests.head(site, verify=secure).headers
