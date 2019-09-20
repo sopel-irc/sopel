90299d3a4b67ec27ccbca9fe4291983e87505d06
Florian Strzelecki <florian.strzelecki@gmail.com>
2019-09-20 13:59:13 +0200

    core: log MODE issue as DEBUG instead of INFO

--- sopel/coretasks.py
+++ sopel/coretasks.py
@@ -216,8 +216,8 @@ def track_modes(bot, trigger):
     if len(trigger.args) < 3:
         # We need at least [channel, mode, nickname] to do anything useful
         # MODE messages with fewer args won't help us
-        LOGGER.info("Received an apparently useless MODE message: {}"
-                    .format(trigger.raw))
+        LOGGER.debug("Received an apparently useless MODE message: {}"
+                     .format(trigger.raw))
         return
     # Our old MODE parsing code checked if any of the args was empty.
     # Somewhere around here would be a good place to re-implement that if it's
