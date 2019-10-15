c836d2847d3c85f604e83106c00ec7d284a94199
dgw <dgw@technobabbl.es>
2019-10-15 05:51:14 -0500

    py: make Oblique endpoint configurable

--- sopel/modules/py.py
+++ sopel/modules/py.py
@@ -11,10 +11,40 @@ from __future__ import unicode_literals, absolute_import, print_function, divisi
 from requests import get
 
 from sopel import module
+from sopel.config.types import StaticSection, ValidatedAttribute
 from sopel.tools.web import quote
 
 
-BASE_TUMBOLIA_URI = 'https://oblique.sopel.chat/'
+class PySection(StaticSection):
+    oblique_instance = ValidatedAttribute('oblique_instance',
+                                          default='https://oblique.sopel.chat/')
+    """The Oblique instance to use when evaluating Python expressions"""
+
+
+def configure(config):
+    """
+    | name | example | purpose |
+    | ---- | ------- | ------- |
+    | oblique_instance | https://oblique.sopel.chat/ | The Oblique instance to use when evaluating Python expressions (see <https://github.com/sopel-irc/oblique>) |
+    """
+    config.define_section('py', PySection)
+    config.py.configure_setting(
+        'oblique_instance',
+        'Enter the base URL of a custom Oblique instance (optional): '
+    )
+
+
+def setup(bot):
+    bot.config.define_section('py', PySection)
+
+    if not any(
+        bot.config.py.oblique_instance.startswith(prot)
+        for prot in ['http://', 'https://']
+    ):
+        raise ValueError('Oblique instance URL must start with a protocol.')
+
+    if not bot.config.py.oblique_instance.endswith('/'):
+        bot.config.py.oblique_instance += '/'
 
 
 @module.commands('py')
@@ -26,7 +56,7 @@ def py(bot, trigger):
         return bot.reply('I need an expression to evaluate.')
 
     query = trigger.group(2)
-    uri = BASE_TUMBOLIA_URI + 'py/'
+    uri = bot.config.py.oblique_instance + 'py/'
     answer = get(uri + quote(query)).content.decode('utf-8')
     if answer:
         bot.say(answer)
