874c6eae6d310e4e0d91bc6930094f3d6da36905
Florian Strzelecki <florian.strzelecki@gmail.com>
2019-11-14 23:26:58 +0100

    isort: sopel.plugins

--- sopel/plugins/__init__.py
+++ sopel/plugins/__init__.py
@@ -27,7 +27,12 @@ exist for each type of plugin.
 # Copyright 2019, Florian Strzelecki <florian.strzelecki@gmail.com>
 #
 # Licensed under the Eiffel Forum License 2.
-from __future__ import unicode_literals, absolute_import, print_function, division
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals
+)
 
 import collections
 import imp
--- sopel/plugins/handlers.py
+++ sopel/plugins/handlers.py
@@ -31,14 +31,20 @@ away from the rest of the application.
 # Copyright 2019, Florian Strzelecki <florian.strzelecki@gmail.com>
 #
 # Licensed under the Eiffel Forum License 2.
-from __future__ import unicode_literals, absolute_import, print_function, division
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals
+)
 
-import inspect
 import imp
 import importlib
+import inspect
 import os
 
 from sopel import loader
+
 from . import exceptions
 
 try:
