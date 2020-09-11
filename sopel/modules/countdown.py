48e3f30c6c088fbcc24b99a744ba533af48c9902
Florian Strzelecki <florian.strzelecki@gmail.com>
2020-09-11 18:49:50 +0200

    countdown: check output consistency
    * replaced `sopel.module` by `sopel.plugin`
    * used output prefix with `bot.say`

--- sopel/modules/countdown.py
+++ sopel/modules/countdown.py
@@ -10,17 +10,20 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import datetime
 
-from sopel.module import commands, NOLIMIT
+from sopel import plugin
 
 
-@commands('countdown')
+@plugin.command('countdown')
+@plugin.example('.countdown 2078 09 14')
+@plugin.output_prefix('[countdown] ')
 def generic_countdown(bot, trigger):
-    """.countdown <year> <month> <day> - displays a countdown to a given date."""
+    """Displays a countdown to a given date."""
     text = trigger.group(2)
     if not text:
-        bot.say("Please use correct format: {}countdown 2012 12 21"
-                .format(bot.config.core.help_prefix))
-        return NOLIMIT
+        bot.reply("Please use correct format: {}countdown 2012 12 21"
+                  .format(bot.config.core.help_prefix))
+        return plugin.NOLIMIT
+
     text = trigger.group(2).split()
     if text and (len(text) == 3 and text[0].isdigit() and
                  text[1].isdigit() and text[2].isdigit()):
@@ -28,13 +31,13 @@ def generic_countdown(bot, trigger):
             diff = (datetime.datetime(int(text[0]), int(text[1]),
                     int(text[2])) - datetime.datetime.today())
         except Exception:  # TODO: Be specific
-            bot.say("Please use correct format: {}countdown 2012 12 21"
-                    .format(bot.config.core.help_prefix))
-            return NOLIMIT
+            bot.reply("Please use correct format: {}countdown 2012 12 21"
+                      .format(bot.config.core.help_prefix))
+            return plugin.NOLIMIT
         bot.say(str(diff.days) + " days, " + str(diff.seconds // 3600) +
                 " hours and " + str(diff.seconds % 3600 // 60) +
                 " minutes until " + text[0] + " " + text[1] + " " + text[2])
     else:
-        bot.say("Please use correct format: {}countdown 2012 12 21"
-                .format(bot.config.core.help_prefix))
-        return NOLIMIT
+        bot.reply("Please use correct format: {}countdown 2012 12 21"
+                  .format(bot.config.core.help_prefix))
+        return plugin.NOLIMIT
