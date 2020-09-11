927938123693395f85fc3f99f0de472dff953ab0
Florian Strzelecki <florian.strzelecki@gmail.com>
2020-09-11 17:19:58 +0200

    calc: check output consistency
    * replaced `sopel.module` by `sopel.plugin`
    * used output prefix with `bot.say`
    * removed obsolete "if main" block

--- sopel/modules/calc.py
+++ sopel/modules/calc.py
@@ -8,34 +8,34 @@ https://sopel.chat
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from sopel.module import commands, example
+from sopel import plugin
 from sopel.tools.calculation import eval_equation
 
 
-@commands('c', 'calc')
-@example('.c 5 + 3', '8')
-@example('.c 0.9*10', '9')
-@example('.c 10*0.9', '9')
-@example('.c 2*(1+2)*3', '18')
-@example('.c 2**10', '1024')
-@example('.c 5 // 2', '2')
-@example('.c 5 / 2', '2.5')
+@plugin.command('c', 'calc')
+@plugin.example('.c 5 + 3', '8')
+@plugin.example('.c 0.9*10', '9')
+@plugin.example('.c 10*0.9', '9')
+@plugin.example('.c 2*(1+2)*3', '18')
+@plugin.example('.c 2**10', '1024')
+@plugin.example('.c 5 // 2', '2')
+@plugin.example('.c 5 / 2', '2.5')
+@plugin.output_prefix('[calc] ')
 def c(bot, trigger):
     """Evaluate some calculation."""
     if not trigger.group(2):
-        return bot.reply("Nothing to calculate.")
+        bot.reply('Nothing to calculate.')
+        return
     # Account for the silly non-Anglophones and their silly radix point.
     eqn = trigger.group(2).replace(',', '.')
     try:
         result = eval_equation(eqn)
         result = "{:.10g}".format(result)
     except ZeroDivisionError:
-        result = "Division by zero is not supported in this universe."
-    except Exception as e:
-        result = "{error}: {msg}".format(error=type(e), msg=e)
-    bot.reply(result)
+        bot.reply('Division by zero is not supported in this universe.')
+        return
+    except SyntaxError:
+        bot.reply('Invalid syntax')
+        return
 
-
-if __name__ == "__main__":
-    from sopel.test_tools import run_example_tests
-    run_example_tests(__file__)
+    bot.say(result)
