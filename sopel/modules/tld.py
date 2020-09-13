1a0b4ee0b1b63acf41fdf5f0116b4353d3d5ca5b
dgw <dgw@technobabbl.es>
2020-09-13 01:35:49 -0500

    tld: use TLD list from IANA to validate queried items' existence
    List is cached in `bot.memory` at runtime, and persisted to a plugin
    value in `bot.db` at shutdown. If a cached list exists, it is loaded
    into `bot.memory` at startup.
    
    The list is updated automatically once the data is 7 days old.

--- sopel/modules/tld.py
+++ sopel/modules/tld.py
@@ -2,12 +2,16 @@
 """
 tld.py - Sopel TLD Plugin
 Copyright 2009-10, Michael Yanovich, yanovich.net
+Copyright 2020, dgw, technobabbl.es
 Licensed under the Eiffel Forum License 2.
 
 https://sopel.chat
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from datetime import datetime
+from encodings import idna
+import logging
 import re
 import sys
 
@@ -20,8 +24,70 @@ if sys.version_info.major >= 3:
     unicode = str
 
 
-uri = 'https://en.wikipedia.org/wiki/List_of_Internet_top-level_domains'
+LOGGER = logging.getLogger(__name__)
+
+
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+IANA_LIST_URI = 'https://data.iana.org/TLD/tlds-alpha-by-domain.txt'
+WIKI_PAGE_URI = 'https://en.wikipedia.org/wiki/List_of_Internet_top-level_domains'
 r_tag = re.compile(r'<(?!!)[^>]+>')
+
+
+def setup(bot):
+    bot.memory['tld_list_cache'] = bot.db.get_plugin_value(
+        'tld', 'tld_list_cache', [])
+    bot.memory['tld_list_cache_updated'] = bot.db.get_plugin_value(
+        'tld', 'tld_list_cache_updated', '2000-01-01 00:00:00')
+    bot.memory['tld_list_cache_updated'] = datetime.strptime(
+        bot.memory['tld_list_cache_updated'], DATE_FORMAT)
+
+
+
+def shutdown(bot):
+    if bot.memory['tld_list_cache']:
+        bot.db.set_plugin_value(
+            'tld', 'tld_list_cache', bot.memory['tld_list_cache'])
+        bot.db.set_plugin_value(
+            'tld', 'tld_list_cache_updated',
+            bot.memory['tld_list_cache_updated'].strftime(DATE_FORMAT))
+
+    for key in ['tld_list_cache', 'tld_list_cache_updated']:
+        try:
+            del bot.memory[key]
+        except KeyError:
+            pass
+
+
+@plugin.interval(60 * 60)
+def update_tld_list(bot):
+    now = datetime.now()
+    then = bot.memory['tld_list_cache_updated']
+    since = now - then
+    if since.days < 7:
+        LOGGER.debug(
+            "Skipping TLD list cache update; the cached list is only %d days old.",
+            since.days,
+        )
+        return
+
+    try:
+        tld_list = requests.get(IANA_LIST_URI).text
+    except requests.exceptions.RequestException:
+        # Probably a transient error; log it and continue life
+        LOGGER.warning(
+            "Error fetching IANA TLD list; will try again later.",
+            exc_info=True)
+        return
+
+    tld_list = [
+        line.lower()
+        for line in tld_list.splitlines()
+        if not line.startswith('#')
+    ]
+
+    bot.memory['tld_list_cache'] = tld_list
+    bot.memory['tld_list_cache_updated'] = now
+    LOGGER.debug("Updated TLD list cache.")
 
 
 @plugin.command('tld')
@@ -29,13 +95,28 @@ r_tag = re.compile(r'<(?!!)[^>]+>')
 @plugin.output_prefix('[tld] ')
 def gettld(bot, trigger):
     """Show information about the given Top Level Domain."""
-    page = requests.get(uri).text
     tld = trigger.group(2)
     if not tld:
         bot.reply("You must provide a top-level domain to search.")
         return  # Stop if no tld argument is provided
     if tld[0] == '.':
         tld = tld[1:]
+
+    if not bot.memory['tld_list_cache']:
+        update_tld_list(bot)
+    tld_list = bot.memory['tld_list_cache']
+
+    if not any([
+        name in tld_list
+        for name
+        in [tld.lower(), idna.ToASCII(tld).decode('utf-8')]
+    ]):
+        bot.reply(
+            "The top-level domain '{}' is not in IANA's list of valid TLDs."
+            .format(tld))
+        return
+
+    page = requests.get(WIKI_PAGE_URI).text
     search = r'(?i)<td><a href="\S+" title="\S+">\.{0}</a></td>\n(<td><a href=".*</a></td>\n)?<td>([A-Za-z0-9].*?)</td>\n<td>(.*)</td>\n<td[^>]*>(.*?)</td>\n<td[^>]*>(.*?)</td>\n'
     search = search.format(tld)
     re_country = re.compile(search)
@@ -74,7 +155,7 @@ def gettld(bot, trigger):
                 dict_val["notes"] = dict_val["notes"][:400] + "..."
             reply = "%s (%s, %s). IDN: %s, DNSSEC: %s, SLD: %s" % (dict_val["country"], dict_val["expl"], dict_val["notes"], dict_val["idn"], dict_val["dnssec"], dict_val["sld"])
         else:
-            reply = "No matches found for TLD: {0}".format(unicode(tld))
+            reply = "The top-level domain '{}' exists, but no details about it could be found.".format(tld)
     # Final touches + output
     reply = web.decode(reply)
     bot.say(reply)
