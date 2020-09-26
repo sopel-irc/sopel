b7b6b46a84e29e26a6a6b921debf57735661a4c0
dgw <dgw@technobabbl.es>
2020-09-26 15:04:58 -0500

    announce: send announces to TARGMAX channels at once, if advertised
    Reduces the bot's network overhead, and might make servers happier.

--- sopel/modules/announce.py
+++ sopel/modules/announce.py
@@ -12,12 +12,26 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from sopel import plugin
 
 
+def _chunks(items, size):
+    """Break a list of items into groups.
+
+    :param items: the collection of items to chunk
+    :type items: :term:`iterable`
+    :param int size: the size of each chunk
+    """
+    # from https://stackoverflow.com/a/312464/5991 with modified names for readability
+    for delim in range(0, len(items), size):
+        yield items[delim:delim + size]
+
+
 @plugin.command('announce')
 @plugin.example('.announce Some important message here')
 @plugin.require_admin('Sorry, I can\'t let you do that', reply=True)
 @plugin.output_prefix('[ANNOUNCEMENT] ')
 def announce(bot, trigger):
     """Send an announcement to all channels the bot is in"""
-    for channel in bot.channels:
-        bot.say(trigger.group(2), channel)
+    size = bot.isupport.get('TARGMAX', {}).get('PRIVMSG', 1)
+    channels = _chunks(bot.channels.keys(), size)
+    for cgroup in channels:
+        bot.say(trigger.group(2), ','.join(cgroup))
     bot.reply('Announce complete.')
