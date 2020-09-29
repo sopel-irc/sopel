d465f332b50982597c7977c2b78401bd14b27e9d
dgw <dgw@technobabbl.es>
2020-09-29 13:43:55 -0500

    ip: add note about relative `GeoIP_db_path` value behavior

--- sopel/modules/ip.py
+++ sopel/modules/ip.py
@@ -39,7 +39,11 @@ LOGGER = logging.getLogger(__name__)
 
 class GeoipSection(types.StaticSection):
     GeoIP_db_path = types.FilenameAttribute('GeoIP_db_path', directory=True)
-    """Path of the directory containing the GeoIP database files."""
+    """Path of the directory containing the GeoIP database files.
+
+    If the given value is not an absolute path, it will be interpreted relative
+    to the directory containing the config file with which Sopel was started.
+    """
 
 
 def configure(config):
