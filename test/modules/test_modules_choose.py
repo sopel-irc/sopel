078c4ac8d4b1428b51a8b336cd0ede3dc642fe46
dgw <dgw@technobabbl.es>
2020-10-18 22:03:45 -0500

    test_modules_choose: we'll aspire to fix these XFAIL tests someday

--- test/modules/test_modules_choose.py
+++ test/modules/test_modules_choose.py
@@ -63,3 +63,36 @@ SAFE_PAIRS = (
 def test_format_safe(text, cleaned):
     """Test expected formatting-safe string sanitization."""
     assert choose._format_safe(text) == cleaned
+
+
+# --- Insert reset only when necessary | Expected to fail --- #
+
+EFFICIENT_PAIRS = (
+    # whitespace inside formatting (kept)
+    ('\x02  leading space inside formatting\x02',
+     '\x02  leading space inside formatting\x02'),
+    ('\x02trailing space inside formatting  \x02',
+     '\x02trailing space inside formatting  \x02'),
+    ('\x02  leading AND trailing inside formatting  \x02',
+     '\x02  leading AND trailing inside formatting  \x02'),
+    # whitespace outside formatting (dropped)
+    ('  \x02leading space outside formatting\x02',
+     '\x02leading space outside formatting\x02'),
+    ('\x02trailing space outside formatting\x02  ',
+     '\x02trailing space outside formatting\x02'),
+    # whitespace both inside and outside formatting
+    # (respectively kept and dropped)
+    ('  \x02  leading space inside AND outside\x02',
+     '\x02  leading space inside AND outside\x02'),
+    ('\x02trailing space inside AND outside  \x02  ',
+     '\x02trailing space inside AND outside  \x02'),
+    ('  \x02  leading AND trailing inside AND outside  \x02  ',
+     '\x02  leading AND trailing inside AND outside  \x02'),
+)
+
+
+@pytest.mark.parametrize('text, cleaned', EFFICIENT_PAIRS)
+@pytest.mark.xfail(strict=True)
+def test_format_safe_future(text, cleaned):
+    """Test future aspirations of efficiency."""
+    assert choose._format_safe(text) == cleaned
