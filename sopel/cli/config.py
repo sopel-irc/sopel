63797af9d8dbfb80923b6aeda61f25b670468d3d
dgw <dgw@technobabbl.es>
2020-11-08 23:12:18 -0600

    cli.config: implement `set` and `unset` subcommands

--- sopel/cli/config.py
+++ sopel/cli/config.py
@@ -62,6 +62,27 @@ def build_parser():
     get_parser.add_argument('option')
     utils.add_common_arguments(get_parser)
 
+    # sopel-config set <section> <key> <value>
+    set_parser = subparsers.add_parser(
+        'set',
+        help="Set a configuration option's value",
+        description="Set a configuration option's value",
+    )
+    set_parser.add_argument('section')
+    set_parser.add_argument('option')
+    set_parser.add_argument('value')
+    utils.add_common_arguments(set_parser)
+
+    # sopel-config unset <section> <key>
+    unset_parser = subparsers.add_parser(
+        'unset',
+        help="Unset a configuration option",
+        description="Unset a configuration option",
+    )
+    unset_parser.add_argument('section')
+    unset_parser.add_argument('option')
+    utils.add_common_arguments(unset_parser)
+
     return parser
 
 
@@ -167,6 +188,78 @@ def handle_get(options):
     return 0  # successful operation
 
 
+def handle_set(options):
+    """Set the ``<section> <key>`` setting to ``<value>``.
+
+    :param options: parsed arguments
+    :type options: :class:`argparse.Namespace`
+    :return: 0 if everything went fine;
+             1 if the section and/or key does not exist;
+             2 if the settings can't be loaded
+    """
+    try:
+        settings = utils.load_settings(options)
+    except Exception as error:
+        tools.stderr(error)
+        return 2
+
+    section = options.section
+    option = options.option
+    value = options.value
+
+    # Make sure the section exists
+    if not settings.parser.has_section(section):
+        try:
+            settings.parser.add_section(section)
+        except (TypeError, ValueError):
+            tools.stderr('Invalid section name "%s"' % section)
+            return 1
+
+    # Update the option & save config file
+    settings.parser.set(section, option, value)
+    settings.save()
+    print('Updated option "{}.{}"'.format(section, option))
+    return 0  # successful operation
+
+
+def handle_unset(options):
+    """Unset the setting value of ``<section> <key>``.
+
+    :param options: parsed arguments
+    :type options: :class:`argparse.Namespace`
+    :return: 0 if everything went fine;
+             1 if the section and/or key does not exist;
+             2 if the settings can't be loaded
+    """
+    try:
+        settings = utils.load_settings(options)
+    except Exception as error:
+        tools.stderr(error)
+        return 2
+
+    section = options.section
+    option = options.option
+
+    # Make sure the section exists
+    if not settings.parser.has_section(section):
+        tools.stderr('Section "%s" does not exist' % section)
+        return 1
+
+    # Unset the option
+    removed = settings.parser.remove_option(section, option)
+    # Remove section completely if empty
+    if len(settings.parser.options(section)) == 0:
+        settings.parser.remove_section(section)
+    # Save config file
+    settings.save()
+    # Report result
+    if removed:
+        print('Unset option "{}.{}"'.format(section, option))
+    else:
+        print('Option "{}.{}" was not set'.format(section, option))
+    return 0  # successful operation
+
+
 def main():
     """Console entry point for ``sopel-config``."""
     parser = build_parser()
@@ -184,3 +277,7 @@ def main():
         return handle_init(options)
     elif action == 'get':
         return handle_get(options)
+    elif action == 'set':
+        return handle_set(options)
+    elif action == 'unset':
+        return handle_unset(options)
