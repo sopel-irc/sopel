sopel-irc/sopel

commit 5af0eea249238485d70c8e14743358a50115b0f4

isup: refactor obsolete if/else
Simplifies handler function (flattens the logic a bit).

sopel/modules/isup.py
@@ -62,7 +62,6 @@ def handle_isup(bot, trigger, secure=True):
         site = get_site_url(trigger.group(2))
         response = requests.head(site, verify=secure, timeout=(10.0, 5.0))
         response.raise_for_status()
-        response = response.headers
     except ValueError as error:
         bot.reply(str(error))
     except requests.exceptions.SSLError:
@@ -85,11 +84,9 @@ def handle_isup(bot, trigger, secure=True):
         bot.say(
             '{} looks down to me (connection error).'
             .format(site))
-    else:
-        if response:
-            bot.say(site + ' looks fine to me.')
-        else:  # TODO: Is it even possible to get here any more?
-            bot.say(site + ' looks down to me.')
+
+    # If no exception happened, the request succeeded.
+    bot.say(site + ' looks fine to me.')
 
 
 @plugin.command('isupinsecure')

commit 9f20e23212329014f7a12fc05f9325395f696fbd

isup: fix "looks fine" being sent after "looks down" messages
Regression introduced in 5af0eea249238485d70c8e14743358a50115b0f4.

sopel/modules/isup.py
@@ -64,28 +64,34 @@ def handle_isup(bot, trigger, secure=True):
         response.raise_for_status()
     except ValueError as error:
         bot.reply(str(error))
+        return
     except requests.exceptions.SSLError:
         bot.say(
             '{} looks down to me (SSL error). Try using `{}isupinsecure`.'
             .format(site, bot.config.core.help_prefix))
+        return
     except requests.HTTPError:
         bot.say(
             '{} looks down to me (HTTP {} "{}").'
             .format(site, response.status_code, response.reason))
+        return
     except requests.ConnectTimeout:
         bot.say(
             '{} looks down to me (timed out while connecting).'
             .format(site))
+        return
     except requests.ReadTimeout:
         bot.say(
             '{} looks down to me (timed out waiting for reply).'
             .format(site))
+        return
     except requests.ConnectionError:
         bot.say(
             '{} looks down to me (connection error).'
             .format(site))
+        return
 
-    # If no exception happened, the request succeeded.
+    # If no exception happened, the request must have succeeded.
     bot.say(site + ' looks fine to me.')
 
 