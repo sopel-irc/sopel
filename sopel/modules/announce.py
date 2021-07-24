aeb4bb638290e40c9e19d3bf1b24421613c1487a
dgw <dgw@technobabbl.es>
2021-07-24 01:48:43 -0500

    announce: simplified approach to `_chunks()`
    Co-authored-by: Exirel <florian.strzelecki@gmail.com>

--- sopel/modules/announce.py
+++ sopel/modules/announce.py
@@ -9,6 +9,8 @@ https://sopel.chat
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import itertools
+
 from sopel import plugin
 
 
@@ -21,15 +23,15 @@ def _chunks(items, size):
     :return: a :term:`generator` of chunks
     :rtype: :term:`generator` of :class:`tuple`
     """
-    # Need to convert non-subscriptable types like `dict_keys` objects
-    try:
-        items[0]
-    except TypeError:
-        items = tuple(items)
-
-    # from https://stackoverflow.com/a/312464/5991 with modified names for readability
-    for delim in range(0, len(items), size):
-        yield tuple(items[delim:delim + size])
+    # This approach is safer than slicing with non-subscriptable types,
+    # for example `dict_keys` objects
+    iterator = iter(items)
+    # TODO: Simplify to assignment expression (`while cond := expr`)
+    # when dropping Python 3.7
+    chunk = tuple(itertools.islice(iterator, size))
+    while chunk:
+        yield chunk
+        chunk = tuple(itertools.islice(iterator, size))
 
 
 @plugin.command('announce')
