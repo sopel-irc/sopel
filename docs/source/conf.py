8b9ac907d05474b411b6df9a8be28c51b6630bdb
dgw <dgw@technobabbl.es>
2021-08-27 21:31:38 -0500

    docs: explicitly select both light and dark Pygments styles
    In the dark color scheme particularly, the default Pygments style
    ("native") has some weird choices in places like imports (underlined
    names), and bland colors.
    
    Having the styles selected explicitly in the docs' `conf.py` also means
    we aren't subject to surprise default-value changes from Furo.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -105,7 +105,8 @@ exclude_patterns = []
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'friendly'
+pygments_dark_style = 'monokai'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
