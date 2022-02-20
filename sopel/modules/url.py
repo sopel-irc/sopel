d814d84108aa18dd3ce89bf92943d597cfa28266
dgw <dgw@technobabbl.es>
2022-02-19 20:49:08 -0600

    Backport pull request #2218
    url: bump user-agent

--- sopel/modules/url.py
+++ sopel/modules/url.py
@@ -33,7 +33,7 @@ LOGGER = logging.getLogger(__name__)
 USER_AGENT = (
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
     'AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/78.0.3904.108 Safari/537.36'
+    'Chrome/98.0.4758.102 Safari/537.36'
 )
 DEFAULT_HEADERS = {
     'User-Agent': USER_AGENT,
