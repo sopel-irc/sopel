523a1e189f7c0f07b95cf70a8a41d228eec76ec0
xnaas <me@xnaas.info>
2022-02-19 19:26:58 -0600

    bump user-agent again

--- sopel/modules/url.py
+++ sopel/modules/url.py
@@ -28,7 +28,7 @@ LOGGER = logging.getLogger(__name__)
 USER_AGENT = (
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
     'AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/96.0.4664.45 Safari/537.36'
+    'Chrome/98.0.4758.102 Safari/537.36'
 )
 DEFAULT_HEADERS = {
     'User-Agent': USER_AGENT,
