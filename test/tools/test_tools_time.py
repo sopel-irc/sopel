a10a75c09d8d317b42561eea824226e094a20695
Florian Strzelecki <florian.strzelecki@gmail.com>
2023-04-22 20:52:01 +0200

    test: fully cover sopel.tools.time

--- test/tools/test_tools_time.py
+++ test/tools/test_tools_time.py
@@ -2,11 +2,36 @@
 from __future__ import annotations
 
 import datetime
+from typing import TYPE_CHECKING
 
 import pytest
 import pytz
 
+from sopel.db import SopelDB
 from sopel.tools import time
+
+
+if TYPE_CHECKING:
+    from sopel.config import Config
+
+
+TMP_CONFIG = """
+[core]
+owner = Pepperpots
+db_filename = {db_filename}
+"""
+
+
+@pytest.fixture
+def tmpconfig(configfactory, tmpdir):
+    content = TMP_CONFIG.format(db_filename=tmpdir.join('test.sqlite'))
+    return configfactory('default.cfg', content)
+
+
+@pytest.fixture
+def db(tmpconfig):
+    db = SopelDB(tmpconfig)
+    return db
 
 
 def test_validate_timezone():
@@ -47,6 +72,96 @@ def test_validate_format_none():
         time.validate_format(None)
 
 
+def test_get_nick_timezone(db: SopelDB):
+    nick = 'IronMan'
+    assert time.get_nick_timezone(db, nick) is None
+
+    db.set_nick_value(nick, 'timezone', 'Europe/Paris')
+    assert time.get_nick_timezone(db, nick) == 'Europe/Paris'
+
+    db.set_nick_value(nick, 'timezone', 'Invalid_TZ')
+    assert time.get_nick_timezone(db, nick) is None
+
+
+def test_get_channel_timezone(db: SopelDB):
+    channel = '#test'
+    assert time.get_channel_timezone(db, channel) is None
+
+    db.set_channel_value(channel, 'timezone', 'Europe/Paris')
+    assert time.get_channel_timezone(db, channel) == 'Europe/Paris'
+
+    db.set_channel_value(channel, 'timezone', 'Invalid_TZ')
+    assert time.get_channel_timezone(db, channel) is None
+
+
+def test_get_timezone(db: SopelDB, tmpconfig: Config):
+    zone = 'Europe/Paris'
+    zone_nick = 'America/New_York'
+    zone_channel = 'Asia/Tokyo'
+    nick = 'IronMan'
+    channel = '#test'
+
+    assert time.get_timezone(db) is None
+    assert time.get_timezone(db, tmpconfig) == 'UTC', (
+        'Must use default timezone from config'
+    )
+    assert time.get_timezone(db, tmpconfig, zone) == zone
+    assert time.get_timezone(db, tmpconfig, zone, nick, channel) == zone
+
+    # channel tz when no tz and no nick tz (or tz is invalid)
+    db.set_channel_value(channel, 'timezone', zone_channel)
+    assert time.get_timezone(
+        db, tmpconfig, None, nick, channel
+    ) == zone_channel, 'Channel '
+    assert time.get_timezone(
+        db, tmpconfig, 'Invalid/Tz', nick, channel
+    ) == zone_channel
+
+    # nick tz has priority
+    db.set_nick_value(nick, 'timezone', zone_nick)
+    assert time.get_timezone(db, tmpconfig, None, nick, channel) == zone_nick
+
+
+def test_get_timezone_zone_nick_or_channel(db: SopelDB, tmpconfig):
+    zone_nick = 'America/New_York'
+    zone_channel = 'Asia/Tokyo'
+    nick = 'IronMan'
+    channel = '#test'
+
+    db.set_nick_value(nick, 'timezone', zone_nick)
+    db.set_channel_value(channel, 'timezone', zone_channel)
+
+    # when zone is actually a nick
+    assert time.get_timezone(db, tmpconfig, zone=nick) == zone_nick
+    assert time.get_timezone(
+        db, tmpconfig, zone=nick, channel=channel
+    ) == zone_nick, 'zone argument must have priority over channel arguments'
+    assert time.get_timezone(
+        db, tmpconfig, zone=nick, nick=nick, channel=channel
+    ) == zone_nick, 'zone argument must have priority over other arguments'
+
+    # when zone is actually a channel
+    assert time.get_timezone(db, tmpconfig, zone=channel) == zone_channel
+    assert time.get_timezone(
+        db, tmpconfig, zone=channel, nick=nick
+    ) == zone_channel, 'zone argument must have priority over nick argument'
+    assert time.get_timezone(
+        db, tmpconfig, zone=channel, nick=nick, channel=channel
+    ) == zone_channel, 'zone argument must have priority over other arguments'
+
+
+def test_get_timezone_zone_has_priority(db: SopelDB, tmpconfig: Config):
+    zone = 'Europe/Paris'
+    nick = 'IronMan'
+    channel = '#test'
+
+    db.set_nick_value(nick, 'timezone', 'America/New_York')
+    db.set_channel_value(channel, 'timezone', 'Asia/Tokyo')
+    assert time.get_timezone(db, tmpconfig, zone, nick, channel) == zone, (
+        'zone argument must have priority over other arguments'
+    )
+
+
 UTC = pytz.timezone('UTC')
 PARIS = pytz.timezone('Europe/Paris')
 
@@ -79,8 +194,67 @@ TIME_FORMAT_PAIRS = [
 
 
 @pytest.mark.parametrize('time_arg, result', TIME_FORMAT_PAIRS)
-def test_format_time(time_arg, result):
+def test_format_time(time_arg: datetime.datetime, result: str):
     assert time.format_time(time=time_arg) == result
+
+
+def test_format_time_config_default_format(tmpconfig: Config):
+    time_arg = datetime.datetime(2023, 4, 22, 19, 38, 26)
+    tmpconfig.core.default_time_format = '%Y-%m-%d at %H:%M'
+    assert time.format_time(
+        config=tmpconfig, time=time_arg
+    ) == '2023-04-22 at 19:38'
+
+    # with timezone
+    assert time.format_time(
+        config=tmpconfig, time=time_arg, zone='Europe/Paris',
+    ) == '2023-04-22 at 21:38'
+
+
+def test_format_time_nick_format(tmpconfig: Config, db: SopelDB):
+    nick = 'IronMan'
+    time_arg = datetime.datetime(2023, 4, 22, 19, 38, 26)
+    tmpconfig.core.default_time_format = '%Y-%m-%d at %H:%M'
+    db.set_nick_value(nick, 'time_format', '%H:%M on %h %d of %Y')
+    assert time.format_time(
+        db, config=tmpconfig, nick=nick, time=time_arg
+    ) == '19:38 on Apr 22 of 2023'
+
+    # with timezone
+    assert time.format_time(
+        db, config=tmpconfig, nick=nick, time=time_arg, zone='Europe/Paris',
+    ) == '21:38 on Apr 22 of 2023'
+
+
+def test_format_time_channel_format(tmpconfig: Config, db: SopelDB):
+    nick = 'IronMan'
+    channel = '#test'
+    time_arg = datetime.datetime(2023, 4, 22, 19, 38, 26)
+    tmpconfig.core.default_time_format = '%Y-%m-%d at %H:%M'
+    db.set_channel_value(channel, 'time_format', '%H:%M on %h %d of %Y')
+    assert time.format_time(
+        db, config=tmpconfig, channel=channel, time=time_arg
+    ) == '19:38 on Apr 22 of 2023'
+
+    # with timezone
+    assert time.format_time(
+        db, config=tmpconfig,
+        channel=channel,
+        time=time_arg,
+        zone='Europe/Paris',
+    ) == '21:38 on Apr 22 of 2023'
+
+    # nick has priority
+    db.set_nick_value(nick, 'time_format', '%h %d of %Y at %H:%M')
+    assert time.format_time(
+        db, config=tmpconfig, nick=nick, channel=channel, time=time_arg
+    ) == 'Apr 22 of 2023 at 19:38'
+
+
+def test_format_time_utcnow():
+    result = time.format_time()
+    parsed = datetime.datetime.strptime(result, '%Y-%m-%d - %H:%M:%S +0000')
+    assert result == time.format_time(time=parsed)
 
 
 def test_seconds_to_human():
