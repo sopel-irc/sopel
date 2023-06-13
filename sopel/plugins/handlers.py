3b948e580107eb5cf50e25af08db411db5cd19d0
James Gerity <snoopjedi@gmail.com>
2023-06-12 22:47:03 -0400

    plugins.handlers: handle & test case when module `__doc__` is `None`

--- sopel/plugins/handlers.py
+++ sopel/plugins/handlers.py
@@ -280,7 +280,7 @@ class PyModulePlugin(AbstractPluginHandler):
         if not self.is_loaded() or not hasattr(self.module, '__doc__'):
             return default_label
 
-        module_doc = getattr(self.module, '__doc__', "")
+        module_doc = self.module.__doc__ or ""
         lines = inspect.cleandoc(module_doc).splitlines()
         return default_label if not lines else lines[0]
 
