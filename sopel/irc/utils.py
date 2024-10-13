db4b2962a97987c6338a8991623c4669a2a32a7e
dgw <dgw@technobabbl.es>
2024-10-13 12:12:04 -0500

    irc.utils: improve `safe()` docstring
    Link to RFC explaining why CR, LF, and NUL are prohibited, and annotate
    the version in which NUL stripping was added.

--- sopel/irc/utils.py
+++ sopel/irc/utils.py
@@ -17,27 +17,31 @@ from typing import NamedTuple
 from sopel.lifecycle import deprecated
 
 
-def safe(string):
-    """Remove newlines from a string.
+def safe(string: str) -> str:
+    """Remove disallowed bytes from a string, and ensure Unicode.
 
-    :param str string: input text to process
-    :return: the string without newlines
-    :rtype: str
+    :param string: input text to process
+    :return: the string as Unicode without characters prohibited in IRC messages
     :raises TypeError: when ``string`` is ``None``
 
-    This function removes newlines and null-bytes from a string. It will always
+    This function removes newlines and null bytes from a string. It will always
     return a Unicode ``str``, even if given non-Unicode input, but doesn't strip
     or alter the string in any other way::
 
-        >>> safe('some \x00text\\r\\n')
+        >>> safe('some \\x00text\\r\\n')
         'some text'
 
-    This is useful to ensure a string can be used in a IRC message.
+    This is useful to ensure a string can be used in a IRC message. Parameters
+    can **never** contain NUL, CR, or LF octets, per :rfc:`2812#section-2.3.1`.
 
     .. versionchanged:: 7.1
 
         This function now raises a :exc:`TypeError` instead of an unpredictable
         behaviour when given ``None``.
+
+    .. versionchanged:: 8.0.1
+
+        Also remove NUL (``\\x00``) in addition to CR/LF.
 
     """
     if string is None:
